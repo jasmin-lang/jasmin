1221ce6f97ac7903eeb999c9a5d31a3e8b7ba2aa
Pierre-Yves Strub <pierre-yves@strub.nu>
2015-06-12 11:35:53 +0200

    Non-executable translation to Coq of x86_64_monad.ec

---
+++ proofs/src/x86_64.v
@@ -0,0 +1,227 @@
+(* -------------------------------------------------------------------- *)
+Require Import ssreflect ssrfun ssrbool eqtype ssrnat fintype finfun.
+Require Import choice seq tuple.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Set Printing Projections.
+
+(* -------------------------------------------------------------------- *)
+Local Notation efst := (@fst _ _).
+Local Notation esnd := (@snd _ _).
+
+Local Notation "f [@ b <- v ]" :=
+  ([ffun x => if x == b then v else f x])
+  (at level 20, format "f [@ b  <-  v ]").
+
+(* -------------------------------------------------------------------- *)
+Section GenFin.
+Variable T : eqType.
+Variable e : seq T.
+
+Hypothesis total : forall x, x \in e.
+
+Definition tonatr (x : T   ) := tag (seq_tnthP (total x)).
+Definition ofnatr (i : 'I__) := tnth (in_tuple e) i.
+
+Lemma tonatrK : cancel tonatr ofnatr.
+Proof. by move=> x; rewrite /tonatr; case: seq_tnthP. Qed.
+End GenFin.
+
+(* -------------------------------------------------------------------- *)
+Parameter w64 : Type.
+
+Parameter addc : w64 -> w64 -> bool -> (bool * w64).
+Parameter mulc : w64 -> w64 -> (w64 * w64).
+
+(* -------------------------------------------------------------------- *)
+Inductive reg :=
+  | RAX | RBX | RCX | RDX | RDI | RSI | RBP | RSP
+  | R8  | R9  | R10 | R11 | R12 | R13 | R14 | R15.
+
+Scheme Equality for reg.
+
+Lemma reg_eqP r1 r2 : reflect (r1 = r2) (reg_beq r1 r2).
+Proof. by apply/(iffP idP)=> [/internal_reg_dec_bl|/internal_reg_dec_lb]. Qed.
+
+Definition reg_eqMixin := EqMixin reg_eqP.
+Canonical  reg_eqType  := Eval hnf in EqType reg reg_eqMixin.
+
+Definition regs := 
+  [:: RAX; RBX; RCX; RDX; RDI; RSI; RBP; RSP;
+      R8 ; R9 ; R10; R11; R12; R13; R14; R15].
+
+Lemma total_regs x : x \in regs.
+Proof. by case: x. Qed.
+
+Definition reg_choiceMixin := CanChoiceMixin (tonatrK total_regs).
+Canonical  reg_choiceType  := Eval hnf in ChoiceType reg reg_choiceMixin.
+Definition reg_countMixin  := CanCountMixin (tonatrK total_regs).
+Canonical  reg_countType   := Eval hnf in CountType reg reg_countMixin.
+Definition reg_finMixin    := CanFinMixin (tonatrK total_regs).
+Canonical  reg_finType     := Eval hnf in FinType reg reg_finMixin.
+
+(* -------------------------------------------------------------------- *)
+Inductive flagid := CF | ZF.
+
+Scheme Equality for flagid.
+
+Lemma flagid_eqP f1 f2 : reflect (f1 = f2) (flagid_beq f1 f2).
+Proof. by apply/(iffP idP)=> [/internal_flagid_dec_bl|/internal_flagid_dec_lb]. Qed.
+
+Definition flagid_eqMixin := EqMixin flagid_eqP.
+Canonical  flagid_eqType  := Eval hnf in EqType flagid flagid_eqMixin.
+
+Definition flagids := [:: CF; ZF].
+
+Lemma total_flagids x : x \in flagids.
+Proof. by case: x. Qed.
+
+Definition flagid_choiceMixin := CanChoiceMixin (tonatrK total_flagids).
+Canonical  flagid_choiceType  := Eval hnf in ChoiceType flagid flagid_choiceMixin.
+Definition flagid_countMixin  := CanCountMixin (tonatrK total_flagids).
+Canonical  flagid_countType   := Eval hnf in CountType flagid flagid_countMixin.
+Definition flagid_finMixin    := CanFinMixin (tonatrK total_flagids).
+Canonical  flagid_finType     := Eval hnf in FinType flagid flagid_finMixin.
+
+(* -------------------------------------------------------------------- *)
+Inductive  rvalue   := Imm of w64 | Reg of reg.
+Definition cmovcond := (flagid * bool)%type.
+
+Inductive instr :=
+| ADC   of rvalue   & reg
+| ADD   of rvalue   & reg
+| MOV   of rvalue   & reg
+| CMOV  of cmovcond & rvalue & reg
+| MUL   of rvalue.
+
+(* -------------------------------------------------------------------- *)
+Record rstate := MkState {
+  s_regs  : {ffun reg -> w64};
+  s_flags : {ffun flagid -> option bool}
+}.
+
+Implicit Types f : flagid.
+Implicit Types r : reg.
+
+Definition get_flag ps f := ps.(s_flags) f.
+Definition get_reg  ps r := ps.(s_regs ) r.
+
+Definition set_flag ps f b :=
+  {| s_regs  := ps.(s_regs);
+     s_flags := ps.(s_flags)[@ f <- b] |}.
+
+Definition set_reg ps r w :=
+  {| s_flags := ps.(s_flags);
+     s_regs  := ps.(s_regs)[@ r <- w] |}.
+
+(* -------------------------------------------------------------------- *)
+Notation "''C_' T" := (rstate -> option (rstate * T))
+  (at level 8, T at level 2, format "''C_' T").
+
+(* -------------------------------------------------------------------- *)
+Definition munit {T : Type} (x : T) : 'C_T :=
+  fun state => Some (state, x).
+
+Definition mfail {T : Type} : 'C_T :=
+  fun _ => None.
+
+Definition mlet {T U : Type} (m1 : 'C_T) (m2 : T -> 'C_U) : 'C_U :=
+  fun state =>
+    if   m1 state is Some (state, x)
+    then m2 x state
+    else None.
+
+(* -------------------------------------------------------------------- *)
+Notation "'mlet' x := m1 'in' m2" := (mlet m1 (fun x => m2))
+  (at level 50, m1, m2 at level 40, x ident,
+     format "'mlet'  x  :=  m1  'in'  m2").
+
+Notation "'mreturn' e" := (munit e)
+  (at level 50, e at level 40, format "'mreturn'  e").
+
+(* -------------------------------------------------------------------- *)
+Definition mread : 'C_rstate :=
+  fun state => Some (state, state).
+
+Definition mwrite (new : rstate) : 'C_unit :=
+  fun _ => Some (new, tt).
+
+Definition mcomp {T : Type} (m : 'C_T) (state : rstate) :=
+  m state.
+
+Definition mexec {T : Type} (m : 'C_T) (state : rstate) :=
+  omap efst (mcomp m state).
+
+(* -------------------------------------------------------------------- *)
+Definition mreadF (f : flagid) :=
+  mlet state := mread in
+    if get_flag state f is Some b then mreturn b else mfail.
+
+Definition mreadR (r : reg) :=
+  mlet state := mread in mreturn (get_reg state r).
+
+(* -------------------------------------------------------------------- *)
+Definition mwriteF (f : flagid) b :=
+  mlet state := mread in mwrite (set_flag state f b).
+
+Definition mwriteR (r : reg) w :=
+  mlet state := mread in mwrite (set_reg state r w).
+
+(* -------------------------------------------------------------------- *)
+Definition mreadir ir :=
+  match ir with
+  | Imm w => mreturn w
+  | Reg r => mreadR r
+  end.
+
+(* -------------------------------------------------------------------- *)
+Definition exec_ADD ir r cf :=
+  mlet x := mreadir ir in
+  mlet y := mreadR  r  in
+
+  let (c, z) := addc x y cf in
+
+  mlet _ := mwriteR r z in
+  mlet _ := mwriteF CF (Some c) in
+
+  mreturn tt.
+
+Definition exec_ADC ir r :=
+  mlet cf := mreadF CF in exec_ADD ir r cf.
+
+Definition exec_MUL ir :=
+  mlet x := mreadir ir in
+  mlet y := mreadR RDX in
+
+  let (z1, z2) := mulc x y in
+
+  mlet _ := mwriteR RDX z1 in
+  mlet _ := mwriteR RAX z2 in
+
+  mreturn tt.
+
+Definition exec_MOV ir r :=
+  mlet x := mreadir ir in mwriteR r x.
+
+Definition exec_CMOV c ir r :=
+  mlet v := mreadF c.1 in
+  if v == c.2 then exec_MOV ir r else mreturn tt.
+
+(* -------------------------------------------------------------------- *)
+Definition step i :=
+  match i with
+  | ADC  ir r   => exec_ADC  ir r
+  | ADD  ir r   => exec_ADD  ir r false
+  | MUL  ir     => exec_MUL  ir
+  | CMOV c ir r => exec_CMOV c ir r
+  | MOV  ir r   => exec_MOV  ir r
+  end.
+
+Fixpoint steps (s : seq instr) :=
+  if s is i :: s then
+    mlet _ := step i in steps s
+  else
+    mreturn tt.
