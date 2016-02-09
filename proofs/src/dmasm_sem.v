1ad982a437fe02b9e197c2cb99c7fe9f22561e8d
Benedikt Schmidt <beschmi@gmail.com>
2016-02-09 17:54:35 +0100

    another pass on dmasm_sem

--- proofs/src/dmasm_sem.v
+++ proofs/src/dmasm_sem.v
@@ -1,7 +1,7 @@
 (* * Syntax and semantics of the dmasm source language *)
 
 (* ** Imports and settings *)
-Require Import ssreflect ssrfun ssrbool ssrnat ssrint ssralg.
+Require Import ssreflect ssrfun ssrbool ssrnat ssrint ssralg tuple.
 Require Import choice fintype eqtype div seq finmap strings zmodp.
 Require Import dmasm_utils.
 
@@ -21,11 +21,13 @@ Section Sem.
 (* ** Types for idents and values
  * -------------------------------------------------------------------- *)
 
-Variable word : Set.
-Variable w2n : word -> nat.
-Variable n2w : nat -> word.
+Definition wsize : nat := nosimpl 64.
 
-Lemma codeK_word : cancel w2n n2w. Admitted.
+Definition word := 'Z_(2^wsize).
+Definition w2n (w : word) : nat := w.
+Definition n2w (n : nat) : word :=  n%:R.
+
+Lemma codeK_word : cancel w2n n2w. Proof. rewrite /cancel /w2n /n2w => x. by rewrite natr_Zp. Qed.
 Definition word_eqMixin     := comparableClass (@LEM word).
 Canonical  word_eqType      := Eval hnf in EqType word word_eqMixin.
 Definition word_choiceMixin := CanChoiceMixin codeK_word.
@@ -39,7 +41,7 @@ Canonical  ident_eqType      := Eval hnf in EqType ident ident_eqMixin.
 Definition ident_choiceMixin := CanChoiceMixin codeK_ident.
 Canonical  ident_choiceType  := ChoiceType ident ident_choiceMixin.
 
-(* ** Source language types
+(* ** Syntax
  * -------------------------------------------------------------------- *)
 
 (* more expressive than required, but leads to simpler definitions *)
@@ -47,32 +49,81 @@ Inductive stype : Set :=
 | sword : stype
 | sbool : stype
 | sprod : stype -> stype -> stype
-| sarr  : stype -> stype.
+| sarr  : forall (n : nat), stype -> stype.
 
-Definition stype_eqMixin := comparableClass (@LEM stype).
-Canonical  stype_eqType  := Eval hnf in EqType stype stype_eqMixin.
+Inductive var (st : stype) : Type :=
+| Var : ident -> var st.
 
-Fixpoint type_of_stype (t : stype) : Type :=
+Inductive sop : stype -> stype -> Set :=
+| Move : forall st, sop st st
+| Fst  : forall st1 st2, sop (sprod st1 st2) st1
+| Snd  : forall st1 st2, sop (sprod st1 st2) st2
+| Add  : sop (sprod sword sword)               (sprod sbool sword)
+| Addc : sop (sprod sword (sprod sword sbool)) (sprod sbool sword).
+
+Fixpoint st2ty (t : stype) : Type :=
   match t with
   | sword         => word
   | sbool         => bool
-  | sprod st1 st2 => ((type_of_stype st1) * (type_of_stype st2))%type
-  | sarr st       => {fmap word -> (type_of_stype st)}
+  | sprod st1 st2 => ((st2ty st1) * (st2ty st2))%type
+  | sarr n st     => (n.+1).-tuple (st2ty st) (* do not allow zero-dim arrays *)
   end.
 
-(* ** Typed finite maps
+Inductive pexpr (st : stype) :=
+| Pvar   : var st -> pexpr st
+| Pconst : st2ty st -> pexpr st
+| Papp   : forall starg : stype, sop starg st -> pexpr starg -> pexpr st.
+
+Inductive loc (st : stype) :=
+| Lvar : var st -> loc st
+| Aget : forall n : nat, var (sarr n st) -> pexpr sword -> loc st.
+
+Inductive src (st : stype) : Type :=
+| Imm : pexpr st -> src st
+| Loc : loc   st -> src st.
+
+Inductive bcmd :=
+| Assgn : forall starg stres, loc stres -> sop starg stres -> src starg -> bcmd
+| Load  : loc sword -> pexpr sword -> bcmd
+| Store : pexpr sword -> src sword -> bcmd.
+
+Inductive dir := UpTo | DownTo.
+
+Definition range := (dir * pexpr sword * pexpr sword)%type.
+
+Inductive cmd :=
+| Sskip  : cmd
+| Sbcmd  : bcmd -> cmd
+| Sseq   : cmd -> cmd -> cmd
+| Sif    : pexpr sbool -> cmd -> cmd -> cmd
+| Sfor   : var sword -> range -> cmd -> cmd
+| Scall  : forall starg stres,
+             var starg -> src stres -> cmd (* function def: (args, ret, body) *)
+             -> var stres
+             -> src starg
+             -> cmd.
+
+(* ** Check if we really need this
  * -------------------------------------------------------------------- *)
 
-Definition amap (st : stype) := {fmap word -> type_of_stype st}.
-Definition amap0 (st : stype) : amap st := fmap0.
+(*
+Definition stype_eqMixin := comparableClass (@LEM stype).
+Canonical  stype_eqType  := Eval hnf in EqType stype stype_eqMixin.
+*)
 
-Definition vmap := forall (st : stype), {fmap ident -> type_of_stype st}.
-Definition vmap_set st (tm : vmap) k (v : type_of_stype st) :=
+(* ** Variables and typed finite maps
+ * -------------------------------------------------------------------- *)
+
+Definition vname st (v : var st) :=
+  let: Var s := v in s.
+
+Definition vmap := forall (st : stype), {fmap ident -> st2ty st}.
+Definition vmap_set st (tm : vmap) k (v : st2ty st) :=
   fun st' =>
      match LEM st st' with
      | left p_eq =>
          eq_rect st
-           (fun st => {fmap ident -> type_of_stype st})
+           (fun st => {fmap ident -> st2ty st})
            (tm st).[k <- v]
            st'
            p_eq
@@ -80,49 +131,29 @@ Definition vmap_set st (tm : vmap) k (v : type_of_stype st) :=
      end.
 Definition vmap0 : vmap := fun st => fmap0.
 
-(* ** Variables
- * -------------------------------------------------------------------- *)
-
-Inductive var (st : stype) : Type :=
-| Var : ident -> var st.
-
-Definition vname st (v : var st) :=
-  let: Var s := v in s.
-
 (* ** Parameter expressions
  * -------------------------------------------------------------------- *)
 
-Variable pexpr : stype -> Set.
-
-Definition pconst st (v : type_of_stype st) : pexpr st := admit.
-Definition sem_pexpr st (vm : vmap) (pe : pexpr st) : result (type_of_stype st) :=
+Definition sem_pexpr st (vm : vmap) (pe : pexpr st) : result (st2ty st) :=
   admit.
-
-(* ** Locations and sources
- * -------------------------------------------------------------------- *)
-
-Inductive loc (st : stype) :=
-| Lvar : var st -> loc st
-| Aget : var (sarr st) -> pexpr sword -> loc st.
-
-Inductive src (st : stype) : Type :=
-| Imm : pexpr st -> src st
-| Loc : loc st   -> src st.
 
 (* ** Reading local variables
  * -------------------------------------------------------------------- *)
 
-Definition read_loc st (vm : vmap) (l : loc st) : result (type_of_stype st) :=
+Definition read_loc st (vm : vmap) (l : loc st) : result (st2ty st) :=
   match l with
   | Lvar (Var vid) =>
       o2r "IdUndef" ((vm st).[? vid])
-  | Aget (Var vid) pe =>
-      o2r "IdUndef" ((vm (sarr st)).[? vid]) >>= fun (a : amap st) =>
+  | Aget n (Var vid) pe =>
+      o2r "IdUndef" ((vm (sarr n st)).[? vid]) >>= fun (a : (n.+1).-tuple (st2ty st)) =>
       sem_pexpr vm pe >>= fun w =>
-      o2r "IdxUndef" (a.[? w])
+      let nw := w2n w in
+      if nw > n
+      then Error "OutOfBounds"
+      else Ok (tnth a (@inZp n nw))
   end.
 
-Definition read_src st (vm : vmap) (s : src st) : result (type_of_stype st) :=
+Definition read_src st (vm : vmap) (s : src st) : result (st2ty st) :=
   match s with
   | Imm pe => sem_pexpr vm pe >>= fun w => Ok w
   | Loc d  => read_loc vm d
@@ -131,16 +162,21 @@ Definition read_src st (vm : vmap) (s : src st) : result (type_of_stype st) :=
 (* ** Writing local variables
  * -------------------------------------------------------------------- *)
 
-Definition write_loc st (vm : vmap) (l : loc st) (v : type_of_stype st)
+Definition write_loc st (vm : vmap) (l : loc st) (v : st2ty st)
     : result vmap :=
   match l with
   | Lvar (Var vid) => Ok (vmap_set vm vid v)
-  | Aget (Var vid) pe =>
+  | Aget n (Var vid) pe =>
       sem_pexpr vm pe >>= fun w =>
-      let a := odflt (amap0 st) ((vm (sarr st)).[? vid]) in
-      Ok (@vmap_set (sarr st) vm vid (a.[w <- v]))
+      o2r "IdUndef" (vm (sarr n st)).[? vid] >>= fun a =>
+      let nw := w2n w in
+      if nw > n
+      then Error "OutOfBounds"
+      else (
+        let a' := [tuple (if i == inZp nw then v else tnth a i) | i < n.+1] in
+        Ok (@vmap_set (sarr n st) vm vid a')
+      )
   end.
-
 
 (* ** Memory
  * -------------------------------------------------------------------- *)
@@ -166,14 +202,7 @@ Definition write_mem (m : mem) (p w : word) : result mem :=
 (* ** Instructions
  * -------------------------------------------------------------------- *)
 
-Definition sop st_in st_out := type_of_stype st_in -> type_of_stype st_out.
-
-Inductive dir := UpTo | DownTo.
-
-Inductive bcmd :=
-| Assgn : forall starg stres, loc stres -> sop starg stres -> src starg -> bcmd
-| Load  : loc sword -> pexpr sword -> bcmd
-| Store : pexpr sword -> src sword -> bcmd.
+Definition sem_sop (sarg sret : stype) : st2ty sarg -> st2ty sret := admit.
 
 Record estate := Estate {
   emem : mem;
@@ -182,9 +211,9 @@ Record estate := Estate {
 
 Definition sem_bcmd (es : estate) (bc : bcmd) : result estate :=
   match bc with
-  | Assgn sto sti d op s =>
+  | Assgn starg stres d op s =>
       read_src es.(evm) s >>= fun args =>
-      let: res := op args in
+      let: res := sem_sop stres args in
       write_loc es.(evm) d res >>= fun vm =>
       Ok (Estate es.(emem) vm)
 
@@ -201,23 +230,9 @@ Definition sem_bcmd (es : estate) (bc : bcmd) : result estate :=
       Ok (Estate m es.(evm))
   end.
 
-Definition range := (dir * pexpr sword * pexpr sword)%type.
-
 Definition sem_range (vm : vmap) (r : range) :=
   let: (d,pe1,pe2) := r in
   Ok (map n2w (iota 0 10)). (* FIXME *)
-
-Inductive cmd :=
-| Sskip  : cmd
-| Sbcmd  : bcmd -> cmd
-| Sseq   : cmd -> cmd -> cmd
-| Sif    : pexpr sbool -> cmd -> cmd -> cmd
-| Sfor   : var sword -> range -> cmd -> cmd
-| Scall  : forall starg stres,
-             var starg -> src stres -> cmd (* function def: (args, ret, body) *)
-             -> var stres
-             -> src starg
-             -> cmd.
 
 Inductive sem : estate -> cmd -> estate -> Prop :=
 | Eskip s :
@@ -259,7 +274,7 @@ with sem_for : var sword -> seq word -> estate -> cmd -> estate -> Prop :=
     sem_for iv [::] s c s
 
 | EForOne s1 s2 s3 c w ws iv :
-    let ac := Sseq (Sbcmd (Assgn (Lvar iv) id (Imm (pconst w)))) c in
+    let ac := Sseq (Sbcmd (Assgn (Lvar iv) (Move sword) (Imm (Pconst w)))) c in
     sem                s1 ac s2 ->
     sem_for iv (ws)    s2 c  s3 ->
     sem_for iv (w::ws) s1 c  s3.
