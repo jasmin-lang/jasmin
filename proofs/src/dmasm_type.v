9306dd65c5804c6a8f10cecb9f90809319d52def
Benjamin Gregoire <Benjamin.Gregoire@inria.fr>
2016-03-07 07:51:32 +0100

    Dependent map from stype.

--- proofs/src/dmasm_type.v
+++ proofs/src/dmasm_type.v
@@ -26,6 +26,25 @@ Inductive stype : Set :=
 
 Notation "st1 ** st2" := (sprod st1 st2) (at level 40, left associativity).
 
+
+(* -------------------------------------------------------------------- *)
+Scheme Equality for stype. 
+(* Definition stype_beq : stype -> stype -> bool *)
+
+Definition eq_stype (st1 st2 : stype) : {st1 = st2} + {st1<>st2}.
+Proof. apply stype_eq_dec. Qed.
+
+Lemma steq_axiom : Equality.axiom stype_beq. 
+Proof. 
+  move=> x y;apply:(iffP idP).
+  + by apply: internal_stype_dec_bl.
+  by apply: internal_stype_dec_lb.
+Qed.
+
+Definition stype_eqMixin     := Equality.Mixin steq_axiom.
+Canonical  stype_eqType      := Eval hnf in EqType stype stype_eqMixin.
+
+(* -------------------------------------------------------------------- *)
 Fixpoint n2P_app (n:nat) p := 
   match n with 
   | 0 => xI p
@@ -131,62 +150,97 @@ Proof.
   by rewrite heq=> -> [] ->.
 Qed.
 
-(*
+Module DInjSType.
 
-Lemma P2n_lt p1 p2 n : P2n p1 = Some (n, p2) -> plog p2 < plog p1.
-Proof.
-  elim:p1 n p2=> [ p1 Hp1 | p1 Hp1 | ] p2 n //=.
-  + by move=> [] _ ->;apply ltnSn.
-  case: P2n Hp1 => [ [n' p'] H [] _ <-| //]. 
-  apply: (@ltn_trans (plog p1));first by apply: (H _).
-  by apply ltnSn.
-Qed.
+  Definition t := [eqType of stype].
 
-Require Import Recdef.
+  Definition t2P := st2P.
  
-Lemma elt_SS n : n < S (S n).
-Proof. by apply: (@ltn_trans (S n));apply: ltnSn. Qed.
-
-Definition ex_log p1 p2 (H:plog p1 < (plog p2).+2) : {p1 | plog p1 < (plog p2).+2 } :=
-  exist (fun p => plog p < (plog p2).+2) p1 H. 
-
-Function P2st (p:positive){measure (plog p)} : 
-   option (stype * {p1 | plog p1 < plog p}) := 
-  match p as p0 return option (stype * {p1 | plog p1 < plog p0}) with
-  | xO (xO p2) => 
-    Some (sword, ex_log (elt_SS (plog p2))) 
-
-  | xO (xI p2) => 
-    Some (sbool, ex_log (elt_SS (plog p2))) 
-
-  | xI (xO p') => 
-    match P2st p' with
-    | Some (t1, exist p1' Hlt1) => 
-      match P2st p1' with
-      | Some (t2, exist p2' Hlt2) => 
-        Some (t1 ** t2, ex_log (ltn_trans (ltn_trans Hlt2 Hlt1) (elt_SS (plog p'))))
-      | None => None
+  Definition t2P_inj := st2P_inj.
+  
+  Fixpoint n_dec (n1 n2:nat) : {n1 = n2} + {True} :=
+    match n1 as n return {n = n2} + {True} with
+    | O => 
+      match n2 as n0 return {O = n0} + {True} with
+      | O => left (erefl O)
+      | _ => right I
       end
-    | None => None
-    end
+    | S n1 =>
+      match n2 as n0 return {S n1 = n0} + {True} with
+      | S n2 => 
+        match n_dec n1 n2 with
+        | left eq =>
+          left (eq_rect n1 (fun n => S n1 = S n) (erefl (S n1)) n2 eq)
+        | right _ => 
+          right I
+        end
+      | _ => right I
+      end
+    end.
+          
+  Fixpoint eq_dec (t1 t2:t) : {t1 = t2} + {True} :=
+    match t1 as t return {t = t2} + {True} with 
+    | sword =>
+      match t2 as t0 return {sword = t0} + {True} with
+      | sword => left (erefl sword)
+      | _     => right I
+      end
+    | sbool =>
+      match t2 as t0 return {sbool = t0} + {True} with
+      | sbool => left (erefl sbool)
+      | _     => right I
+      end
+    | sprod t1 t1' =>
+      match t2 as t0 return {t1 ** t1' = t0} + {True} with
+      | sprod t2 t2' =>
+        match eq_dec t1 t2 with
+        | left eq1 =>
+          match eq_dec t1' t2' with
+          | left eq2 => 
+            let aux  := eq_rect t1 (fun t => t1 ** t1' = t ** t1') (erefl (t1 ** t1')) t2 eq1 in
+            let aux' := eq_rect t1' (fun t => t1 ** t1' = t2 ** t) aux t2' eq2 in
+            left aux'
+          | right _  => right I
+          end
+        | right _  => right I
+        end
+      | _            => right I
+      end
+    | sarr n1 t1 =>
+      match t2 as t0 return {sarr n1 t1 = t0} + {True} with
+      | sarr n2 t2 =>
+        match n_dec n1 n2 with
+        | left eqn =>
+          match eq_dec t1 t2 with
+          | left eqt =>
+            let auxn  := 
+                eq_rect n1 (fun n => sarr n1 t1 = sarr n t1) (erefl (sarr n1 t1)) n2 eqn in
+            let auxt := eq_rect t1 (fun t => sarr n1 t1 = sarr n2 t) auxn t2 eqt in
+            left auxt
+          | right _ => right I
+          end
+        | right _  => right I
+        end
+      | _          => right I
+      end
+    end.
 
-  | xI (xI p') =>
-     match P2n p' as o return (forall p2 n, o = Some (n, p2) -> plog p2 < plog p') ->
-                               option (stype * {p2 | plog p2 < (plog p').+2}) with
-    | Some (n', p1') =>
-      fun (H: forall p2 n, Some(n',p1') = Some (n,p2) -> plog p2 < plog p') =>
-        match P2st p1' with
-        | Some (st, exist p2' Hlt2) =>
-          let Hlt1 := H p1' n' (erefl (Some(n',p1'))) in
-          Some (sarr n' st, ex_log (ltn_trans (ltn_trans Hlt2 Hlt1) (elt_SS (plog p'))))
-        | _ => None
-        end 
-    | _ => fun _ => None
-    end (@P2n_lt p')
+  Lemma n_dec_r n1 n2 tt: n_dec n1 n2 = right tt -> n1 <> n2.
+  Proof.
+    case: tt;elim: n1 n2 => [|n1 Hn1] [|n2] //=.
+    by case: n_dec (Hn1 n2) => // -[] neq _ [] *;apply neq.
+  Qed.
 
-  | _ => None
-  end. 
+  Lemma eq_dec_r t1 t2 tt: eq_dec t1 t2 = right tt -> t1 <> t2.
+  Proof.
+    case: tt;elim:t1 t2=> [|| t1 Ht1 t2 Ht2 | n t Ht] [|| t1' t2' | n' t'] //=.
+    + case: eq_dec (Ht1 t1') => [? _ | [] neq _ [] *];last by apply neq.
+      by case: eq_dec (Ht2 t2') => // -[] neq _ [] *;apply neq.
+    + case: n_dec (@n_dec_r n n' I) => [? _ | [] neq _ [] *];last by apply neq.
+      by case: eq_dec (Ht t') => // -[] neq _ [] *;apply neq.
+  Qed.
+    
+End DInjSType.
 
-*)
-
+Module DMst := DMmake DInjSType.
 
