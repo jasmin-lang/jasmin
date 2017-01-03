5f92b26d7a2e3b1a28ccfdd9ecad1d5ab7f7382a
Benedikt Schmidt <beschmi@gmail.com>
2017-01-03 18:24:06 -0500

    initial version: jasmin library for rust

---
+++ rust-lib/src/lib.rs
@@ -0,0 +1,361 @@
+#![feature(const_fn)]
+
+extern crate extprim;
+
+
+pub mod var;
+
+#[macro_use]
+pub mod jasmin {
+
+    #[derive(Clone,Copy,Debug,PartialEq,Eq)]
+    pub struct Jval<T> {
+        pub val: T
+    }
+    
+    pub const fn jv<T>(x: T) -> Jval<T> {
+        Jval {val: x}
+    }
+
+    #[allow(non_camel_case_types)]
+    pub type b1 = Jval<bool>;
+
+    #[allow(non_camel_case_types)]
+    pub type b8 = Jval<u8>;
+
+    #[allow(non_camel_case_types)]
+    pub type b16 = Jval<u16>;
+
+    #[allow(non_camel_case_types)]
+    pub type b32 = Jval<u32>;
+
+    #[allow(non_camel_case_types)]
+    pub type b64 = Jval<u64>;
+
+    pub trait ToJval<T> {
+        fn to_jval(self : Self) -> Jval<T>;
+    }
+
+    impl<T> ToJval<T> for Jval<T> {
+        fn to_jval(self : Self) -> Jval<T> { self }
+    }
+
+    // we define these explicitly, don't want to automatically lift e.g. arrays
+    impl ToJval<u64> for u64 {
+        fn to_jval(self) -> b64 { Jval { val: self } }
+    }
+    impl ToJval<u32> for u32 {
+        fn to_jval(self) -> b32 { Jval { val: self } }
+    }
+    impl ToJval<u16> for u16 {
+        fn to_jval(self) -> b16 { Jval { val: self } }
+    }
+    impl ToJval<u8> for u8 {
+        fn to_jval(self) -> b8 { Jval { val: self } }
+    }
+    impl ToJval<bool> for bool {
+        fn to_jval(self) -> b1 { Jval { val: self } }
+    }
+
+    #[macro_export]
+    macro_rules! code {
+        ( $($arg:tt)* ) => {
+            __j_internal!( $( $arg )* )
+        }
+    }
+
+    #[macro_export]
+    macro_rules! __j_internal {
+        () => {
+            ()
+        };
+        
+        // * Assignments with destructuring match
+
+        // = assignment with 5-tuple
+        ( ($v0: expr, $v1: expr, $v2: expr, $v3: expr, v4: expr) = $e: expr ; $($rest:tt)* ) => {
+            let t = $e; $v0 = t.0; $v1 = t.1; $v2 = t.2; $v3 = t.3; $v4 = t.4;
+            __j_internal!{ $($rest)* }
+        };
+        // = assignment with 4-tuple
+        ( ($v0: expr, $v1: expr, $v2: expr, $v3: expr) = $e: expr ; $($rest:tt)* ) => {
+            let t = $e; $v0 = t.0; $v1 = t.1; $v2 = t.2; $v3 = t.3;
+            __j_internal!{ $($rest)* }
+        };
+        // = assignment with triple
+        ( ($v0: expr, $v1: expr, $v2: expr) = $e: expr ; $($rest:tt)* ) => {
+            let t = $e; $v0 = t.0; $v1 = t.1; $v2 = t.2;
+            __j_internal!{ $($rest)* }
+        };        
+        // = assignment with pair
+        ( ($v0: expr, $v1: expr) = $e: expr ; $($rest:tt)* ) => {
+            let t = $e; $v0 = t.0; $v1 = t.1;
+            __j_internal!{ $($rest)* }
+        };
+
+        // * Assignment
+
+        // array assignment: v = [ a1, .., ak ]
+        ( $v0: ident = [ $( $e: expr),+ ] ; $($rest:tt)* ) => {
+            $v0 = [ $( $e.to_jval()),+ ];
+            __j_internal!{ $($rest)* }
+        };
+        // array assignment: v = [ v; n ]
+        ( $v0: ident = [ $e: expr; $n: expr ] ; $($rest:tt)* ) => {
+            $v0 = [ $e.to_jval(); n ];
+            __j_internal!{ $($rest)* }
+        };
+        // standard assignment: v = ..
+        ( $v0: ident = c $v1: expr ; $($rest:tt)* ) => {
+            $v0 = $v1.to_jval();
+            __j_internal!{ $($rest)* }
+        };
+        // standard assignment: v = ..
+        ( $v0: ident = $e: expr ; $($rest:tt)* ) => {
+            $v0 = $e;
+            __j_internal!{ $($rest)* }
+        };
+        // standard assignment: a[..] = ..
+        ( $v0: ident [ $e0: expr ] = $v1: expr ; $($rest:tt)* ) => {
+            $v0[$e0] = $v1;
+            __j_internal!{ $($rest)* }
+        };
+
+        // * Conditional move
+
+        // = conditional move negated: ident = ...
+        ( when ! $c: ident { $v0: ident = $v1: expr } ; $($rest: tt)* ) => {
+            if ! $c.val { $v0 = $v1; }
+            __j_internal!{ $($rest)* }
+        };
+        // = conditional move: ident = ...
+        ( when $c : ident { $v0: ident = $v1: expr }; $($rest: tt)* ) => {
+            if $c.val { $v0 = $v1; }
+            __j_internal!{ $($rest)* }
+        };
+        // = conditional move negated
+        ( when ! $c: ident { $v0: ident [ $idx: expr ] = $v1: expr } ; $($rest: tt)* ) => {
+            if ! $c.val { $v0[$idx] = $v1; }
+            __j_internal!{ $($rest)* }
+        };
+        // = conditional move
+        ( when $c : ident { $v0: ident [ $idx: expr ] = $v1: expr }; $($rest: tt)* ) => {
+            if $c.val { $v0[$idx] = $v1; }
+            __j_internal!{ $($rest)* }
+        };
+
+        // 
+        // * Control flow
+
+        // for i in .. { .. }
+        ( for $v0: ident in ( $rng: expr ) { $( $body: tt )* } $($rest:tt)* ) => {
+            for $v0 in $rng { __j_internal!{ $( $body)* } };
+            __j_internal!{ $($rest)* }
+        };
+
+        // do { .. } while !c;
+        ( do { $( $body: tt )* } while ! $c: ident; $($rest:tt)* ) => {
+            while { __j_internal!{ $( $body)* }; ! $c.val } { };
+            __j_internal!{ $($rest)* }
+        };
+
+        // do { .. } while c;
+        ( do { $( $body: tt )* } while $c: ident; $($rest:tt)* ) => {
+            while { __j_internal!{ $( $body)* }; $c.val } { };
+            __j_internal!{ $($rest)* }
+        };
+
+        // if .. { .. } else { .. }
+        ( if ( $c: expr ) { $( $body: tt )* } else { $( $ebody: tt )* } $($rest:tt)* ) => {
+            if $c { __j_internal!{ $( $body )* } } else { __j_internal!{ $( $ebody )* } } ;
+            __j_internal!{ $($rest)* }
+        };
+
+        // if .. { .. }
+        ( if ( $c: expr ) { $( $body: tt )* } $($rest:tt)* ) => {
+            if $c { __j_internal!{ $( $body)* } };
+            __j_internal!{ $($rest)* }
+        };
+
+        // embed rust only instructions
+
+        // everything else
+        ( rust! { $( $body: tt )* } $($rest:tt)* ) => {
+            { $( $body)* };
+            __j_internal!{ $($rest)* }
+        };
+
+    }
+
+    #[macro_export]
+    macro_rules! rust {
+        ( $( $d: tt )* ) => {
+            $( $d )*
+        }
+    }
+
+    #[macro_export]
+    macro_rules! reg {
+        ( $( $d: tt )* ) => {
+            $( $d )*
+        }
+    }
+
+    #[macro_export]
+    macro_rules! stack {
+        ( $( $d: tt )* ) => {
+            $( $d )*
+        }
+    }
+}
+
+// 
+#[allow(non_snake_case)]
+pub mod U64 {
+    use extprim::traits::ToExtraPrimitive;
+
+    use jasmin::*;
+
+    // we use ToJval<u64> to allow for u64 constants
+    pub fn add_v<T1,T2>(x: T1, y: T2) -> b64
+      where T1: ToJval<u64>,T2: ToJval<u64> {
+        let (x,y) = (x.to_jval(), y.to_jval());
+        jv(x.val.wrapping_add(y.val))
+    }
+
+    pub fn add<T1,T2>(x: T1, y: T2) -> (b1,b64) 
+      where T1: ToJval<u64>,T2: ToJval<u64> {
+        let (x,y) = (x.to_jval(), y.to_jval());
+        let (r,cf) = x.val.overflowing_add(y.val);
+        (jv(cf),jv(r))
+    }
+
+    pub fn adc_v<T1,T2,T3>(x: T1, y: T2, cf: T3) -> b64
+      where T1: ToJval<u64>,T2: ToJval<u64>, T3: ToJval<bool> {
+        let (x,y,cf) = (x.to_jval(), y.to_jval(),cf.to_jval());
+        jv(x.val.wrapping_add(y.val).wrapping_add(cf.val as u64))
+    }
+
+    pub fn adc<T1,T2,T3>(x: T1, y: T2, cf: T3) -> (b1,b64)
+      where T1: ToJval<u64>,T2: ToJval<u64>, T3: ToJval<bool> {
+        let (x,y,cf) = (x.to_jval(), y.to_jval(),cf.to_jval());
+        let (r,cf1) = x.val.overflowing_add(y.val);
+        let (r,cf2) = r.overflowing_add(cf.val as u64);
+        (jv(cf1 || cf2),jv(r))
+    }
+
+
+    pub fn sub_v<T1,T2>(x: T1, y: T2) -> b64
+      where T1: ToJval<u64>,T2: ToJval<u64> {
+        let (x,y) = (x.to_jval(), y.to_jval());
+        jv(x.val.wrapping_sub(y.val))
+    }
+
+    pub fn sub<T1,T2>(x: T1, y: T2) -> (b1,b64)
+      where T1: ToJval<u64>,T2: ToJval<u64> {
+        let (x,y) = (x.to_jval(), y.to_jval());
+        let (r,cf) = x.val.overflowing_sub(y.val);
+        (jv(cf),jv(r))
+    }
+
+
+    pub fn sbb_v<T1,T2,T3>(x: T1, y: T2, cf: T3) -> b64
+      where T1: ToJval<u64>,T2: ToJval<u64>, T3: ToJval<bool> {
+        let (x,y,cf) = (x.to_jval(), y.to_jval(),cf.to_jval());
+        jv(x.val.wrapping_sub(y.val).wrapping_sub(cf.val as u64))
+    }
+
+    pub fn sbb<T1,T2,T3>(x: T1, y: T2, cf: T3) -> (b1,b64)
+      where T1: ToJval<u64>,T2: ToJval<u64>, T3: ToJval<bool> {
+        let (x,y,cf) = (x.to_jval(),y.to_jval(),cf.to_jval());
+        let (r,cf1) = x.val.overflowing_sub(y.val);
+        let (r,cf2) = r.overflowing_sub(cf.val as u64);
+        (jv(cf1 || cf2),jv(r))
+    }
+
+    pub fn mul<T1,T2>(x: T1, y: T2) -> (b64, b64)
+      where T1: ToJval<u64>,T2: ToJval<u64> {
+        let (x,y) = (x.to_jval(), y.to_jval());
+        let z = x.val.to_u128().unwrap() * y.val.to_u128().unwrap();
+        (jv(z.high64()), jv(z.low64()))
+    }
+
+    pub fn imul<T1,T2>(x: T1, y: T2) -> b64
+      where T1: ToJval<u64>,T2: ToJval<u64> {
+        let (x,y) = (x.to_jval(), y.to_jval());
+        let z = x.val.to_u128().unwrap() * y.val.to_u128().unwrap();
+        jv(z.low64())
+    }
+
+    pub fn xor<T1,T2>(x: T1, y: T2) -> b64
+      where T1: ToJval<u64>,T2: ToJval<u64> {
+        let (x,y) = (x.to_jval(), y.to_jval());
+        let z = x.val ^ y.val;
+        jv(z)
+    }
+
+    pub fn land<T1,T2>(x: T1, y: T2) -> b64
+      where T1: ToJval<u64>,T2: ToJval<u64> {
+        let (x,y) = (x.to_jval(), y.to_jval());
+        let z = x.val & y.val;
+        jv(z)
+    }
+
+    pub fn shr<T1,T2>(x: T1, y: T2) -> b64
+      where T1: ToJval<u64>,T2: ToJval<u64> {
+        let (x,y) = (x.to_jval(), y.to_jval());
+        let z = x.val >> y.val;
+        jv(z)
+    }
+
+    pub fn shl<T1,T2>(x: T1, y: T2) -> b64
+      where T1: ToJval<u64>,T2: ToJval<u64> {
+        let (x,y) = (x.to_jval(), y.to_jval());
+        let z = x.val << y.val;
+        jv(z)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use jasmin::*;
+    use jasmin::ToJval;
+    use U64::*;
+
+    #[test]
+    fn test_syntax() {
+        let mut x  : stack![b64];
+        let mut y  : reg![b64];
+        let mut cf : reg![b1];
+        let mut _cf : reg![b1];
+
+        code!{
+            do {
+                cf = c false;
+                x  = c 0;
+                y  = x;
+                cf = jv(cf.val);
+                (cf,x)  = adc(x,y,cf);
+                when cf { x = adc_v(x,y,cf) };
+                when !cf { x = adc_v(x,y,cf) };
+                (_cf,x) = adc(x,y,cf);
+                (cf,x)  = add(x,0);
+                (cf,y)  = adc(x,y,cf);
+                (x,y,cf,_cf) = (x,y,cf,_cf);
+                _cf  = cf;
+                cf = c false;
+            } while cf;
+            do {
+                (cf,x)  = add(x,y);
+                (_cf,x) = adc(x,y,cf);
+                (cf,x)  = add(x,y);
+                (_cf,y)  = adc(x,y,cf);
+                cf = c true;
+            } while !cf;
+            
+        };
+        
+        println!("x: {}, y: {}, cf: {}", x.val, y.val, cf.val);
+    }
+}
