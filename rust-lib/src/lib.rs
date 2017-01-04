155e473e33e3c9d885e0faa8a27141174d74ee86
Benedikt Schmidt <beschmi@gmail.com>
2017-01-04 03:35:14 -0500

    add support for MEM and improve syntax for constants

--- rust-lib/src/lib.rs
+++ rust-lib/src/lib.rs
@@ -2,21 +2,48 @@
 
 extern crate extprim;
 
-
 pub mod var;
+
+use std::collections::HashMap;
+use std::cell::RefCell;
+
+// simulate heap in rust for testing
+thread_local! {
+    pub static MEM_ARRAY: RefCell<HashMap<usize,u64>> = RefCell::new(HashMap::new());
+}
 
 #[macro_use]
 pub mod jasmin {
+    use MEM_ARRAY;
+    use std::ops::{Index,IndexMut};
+
+    pub fn addr_to_idx(p: u64,off: usize) -> usize {
+        ((p as usize) + off) / 8
+    }
+
+    pub fn store_mem(p: u64,off: usize,x: u64) {
+        let i = addr_to_idx(p,off);
+        MEM_ARRAY.with(|m| {
+            m.borrow_mut().insert(i,x);
+        });
+    }
+
+    pub fn load_mem(p: u64,off: usize) -> u64 {
+        let i = addr_to_idx(p,off);
+        let mut x = None;
+        MEM_ARRAY.with(|m| {
+            let m = m.borrow();
+            let r = m.get(&i);
+            x = Some(*r.unwrap());
+        });
+        x.unwrap()
+    }
 
     #[derive(Clone,Copy,Debug,PartialEq,Eq)]
     pub struct Jval<T> {
         pub val: T
     }
     
-    pub const fn jv<T>(x: T) -> Jval<T> {
-        Jval {val: x}
-    }
-
     #[allow(non_camel_case_types)]
     pub type b1 = Jval<bool>;
 
@@ -57,6 +84,22 @@ pub mod jasmin {
         fn to_jval(self) -> b1 { Jval { val: self } }
     }
 
+    impl Index<Jval<u64>> for [b64] {
+        type Output = b64;
+
+        fn index(&self, i: b64) -> &b64 {
+            &self[i.val as usize]
+        }
+    }
+
+    impl IndexMut<Jval<u64>> for [b64] {
+
+        fn index_mut(&mut self, i: b64) -> &mut b64 {
+            &mut self[i.val as usize]
+        }
+    }
+
+
     #[macro_export]
     macro_rules! code {
         ( $($arg:tt)* ) => {
@@ -93,6 +136,24 @@ pub mod jasmin {
             __j_internal!{ $($rest)* }
         };
 
+        // * Memory load and store
+
+        // load to ident
+        ( $v0: ident              = MEM [ $vp: ident + $e:expr ] ; $($rest:tt)* ) => {
+            $v0 = load_mem($vp.val,$e).to_jval();
+            __j_internal!{ $($rest)* }
+        };
+        // load to array-index
+        ( $v0: ident [ $ie: expr ] = MEM [ $vp: ident + $e:expr ] ; $($rest:tt)* ) => {
+            $v0[$ie] = load_mem($vp.val,$e).to_jval();
+            __j_internal!{ $($rest)* }
+        };
+        // store
+        ( MEM [ $vp: ident + $e:expr ] = $x: expr; $($rest:tt)* ) => {
+            store_mem($vp.val,$e,$x.val);
+            __j_internal!{ $($rest)* }
+        };
+     
         // * Assignment
 
         // array assignment: v = [ a1, .., ak ]
@@ -102,11 +163,11 @@ pub mod jasmin {
         };
         // array assignment: v = [ v; n ]
         ( $v0: ident = [ $e: expr; $n: expr ] ; $($rest:tt)* ) => {
-            $v0 = [ $e.to_jval(); n ];
+            $v0 = [ $e.to_jval(); $n ];
             __j_internal!{ $($rest)* }
         };
         // standard assignment: v = ..
-        ( $v0: ident = c $v1: expr ; $($rest:tt)* ) => {
+        ( $v0: ident = # $v1: expr ; $($rest:tt)* ) => {
             $v0 = $v1.to_jval();
             __j_internal!{ $($rest)* }
         };
@@ -115,6 +176,12 @@ pub mod jasmin {
             $v0 = $e;
             __j_internal!{ $($rest)* }
         };
+        // standard assignment: v = ..
+        ( $v0: ident [ $e0: expr ] = # $v1: expr ; $($rest:tt)* ) => {
+            $v0[$e0] = $v1.to_jval();
+            __j_internal!{ $($rest)* }
+        };
+
         // standard assignment: a[..] = ..
         ( $v0: ident [ $e0: expr ] = $v1: expr ; $($rest:tt)* ) => {
             $v0[$e0] = $v1;
@@ -179,9 +246,15 @@ pub mod jasmin {
 
         // embed rust only instructions
 
-        // everything else
+        // rust code ignored by compiler
         ( rust! { $( $body: tt )* } $($rest:tt)* ) => {
             { $( $body)* };
+            __j_internal!{ $($rest)* }
+        };
+
+        // everything else, e.g. function calls for void functions
+        ( $e: expr ; $($rest:tt)* ) => {
+            $e;
             __j_internal!{ $($rest)* }
         };
 
@@ -215,6 +288,10 @@ pub mod U64 {
     use extprim::traits::ToExtraPrimitive;
 
     use jasmin::*;
+
+    const fn jv<T>(x: T) -> Jval<T> {
+        Jval {val: x}
+    }
 
     // we use ToJval<u64> to allow for u64 constants
     pub fn add_v<T1,T2>(x: T1, y: T2) -> b64
@@ -290,29 +367,31 @@ pub mod U64 {
     pub fn xor<T1,T2>(x: T1, y: T2) -> b64
       where T1: ToJval<u64>,T2: ToJval<u64> {
         let (x,y) = (x.to_jval(), y.to_jval());
-        let z = x.val ^ y.val;
-        jv(z)
+        jv(x.val ^ y.val)
     }
 
     pub fn land<T1,T2>(x: T1, y: T2) -> b64
       where T1: ToJval<u64>,T2: ToJval<u64> {
         let (x,y) = (x.to_jval(), y.to_jval());
-        let z = x.val & y.val;
-        jv(z)
+        jv(x.val & y.val)
+    }
+
+    pub fn lor<T1,T2>(x: T1, y: T2) -> b64
+      where T1: ToJval<u64>,T2: ToJval<u64> {
+        let (x,y) = (x.to_jval(), y.to_jval());
+        jv(x.val | y.val)
     }
 
     pub fn shr<T1,T2>(x: T1, y: T2) -> b64
       where T1: ToJval<u64>,T2: ToJval<u64> {
         let (x,y) = (x.to_jval(), y.to_jval());
-        let z = x.val >> y.val;
-        jv(z)
+        jv(x.val >> y.val)
     }
 
     pub fn shl<T1,T2>(x: T1, y: T2) -> b64
       where T1: ToJval<u64>,T2: ToJval<u64> {
         let (x,y) = (x.to_jval(), y.to_jval());
-        let z = x.val << y.val;
-        jv(z)
+        jv(x.val << y.val)
     }
 }
 
@@ -323,39 +402,90 @@ mod tests {
     use jasmin::ToJval;
     use U64::*;
 
+
+    fn id_array(x: [b64; 10]) -> [b64; 10] {
+        x
+    }
+
+    fn clear_array(_x: [b64; 10]) { }
+
     #[test]
     fn test_syntax() {
-        let mut x  : stack![b64];
-        let mut y  : reg![b64];
-        let mut cf : reg![b1];
-        let mut _cf : reg![b1];
+        #![allow(unused_assignments)]
+        #![allow(unused_variables)]
+
+        let mut x    : stack! (b64);
+        let mut y    : reg! (b64);
+        let mut cf   : reg! (b1);
+        let mut _cf  : reg! (b1);
+        let mut arr1 : reg! ([b64; 10]);
+        let mut arr2 : reg! ([b64; 10]);
 
         code!{
             do {
-                cf = c false;
-                x  = c 0;
+                arr1 = [0; 10];
+                arr2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+                x  = #16;
+                MEM[x + 0] = x;
+                y = #0;
+                arr1[y] = #1;
+                y = arr1[y];
+                for i in (0..4) {
+                    MEM[x + i*8] = x;
+                }
+                for i in (0..4) {
+                    x = MEM[x + i*8];
+                }      
+                x = MEM[x + 0];
+                arr1 = id_array(arr1);
+                clear_array(arr1);
+                cf = #false;
+                arr1[0] = #0;
                 y  = x;
-                cf = jv(cf.val);
+                cf = cf.val.to_jval();
                 (cf,x)  = adc(x,y,cf);
                 when cf { x = adc_v(x,y,cf) };
-                when !cf { x = adc_v(x,y,cf) };
+                when !cf { x = adc_v(x,y,false) };
                 (_cf,x) = adc(x,y,cf);
                 (cf,x)  = add(x,0);
                 (cf,y)  = adc(x,y,cf);
                 (x,y,cf,_cf) = (x,y,cf,_cf);
                 _cf  = cf;
-                cf = c false;
+                cf = #false;
             } while cf;
             do {
                 (cf,x)  = add(x,y);
                 (_cf,x) = adc(x,y,cf);
                 (cf,x)  = add(x,y);
-                (_cf,y)  = adc(x,y,cf);
-                cf = c true;
+                (_cf,y) = adc(x,y,cf);
+                cf = #true;
             } while !cf;
             
         };
-        
         println!("x: {}, y: {}, cf: {}", x.val, y.val, cf.val);
+    }
+
+    #[test]
+    fn test_mem() {
+        let mut x    : stack! (b64);
+        let     p    : stack! (b64);
+
+        println!("starting test");
+        code!{
+            p = #8;
+            for i in (0..64) {
+                x = #(i as u64);
+                MEM[p + i*8] = x;
+                rust! { println!("writing {} with i={}: ",x.val,i) }
+            }
+            for i in (0..64) {
+                x = MEM[p + i*8];
+                rust! {
+                    println!("reading {} with i={}: ",x.val,i);
+                    assert_eq!(x.val, i as u64);
+                }
+            }
+
+        }
     }
 }
