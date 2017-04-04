5db87a439b361ff1ef44d3b09d1f2dda2d084b64
Pierre-Yves Strub <pierre-yves@strub.nu>
2017-04-04 16:56:44 +0200

    While rules

---
+++ proofs/src/dmasm_wp_while.v
@@ -0,0 +1,84 @@
+(* -------------------------------------------------------------------- *)
+From mathcomp Require Import all_ssreflect.
+
+Require Import Morphisms ZArith.
+
+Require Import dmasm_utils dmasm_type dmasm_var.
+Require Import dmasm_expr dmasm_sem dmasm_Ssem dmasm_wp.
+Require Import memory.
+
+Import UnsafeMemory.
+
+(* -------------------------------------------------------------------- *)
+Set   Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+Unset SsrOldRewriteGoalsOrder.
+
+Local Open Scope Z_scope.
+
+(* -------------------------------------------------------------------- *)
+Hint Resolve 0 SEskip.
+Hint Resolve SEseq : ssem.
+
+Notation strue  := (SVbool true).
+Notation sfalse := (SVbool false).
+
+(* -------------------------------------------------------------------- *)
+Inductive dupI (P : Prop) := Dup of P & P.
+
+Lemma dup (P : Prop) : P -> dupI P.
+Proof. by move=> hP; constructor. Qed.
+
+(* -------------------------------------------------------------------- *)
+Notation e2b s e := (ssem_pexpr s e = ok strue).
+
+(* -------------------------------------------------------------------- *)
+Lemma ssem_app_inv prg s c1 c2 s' :
+  ssem prg s (c1 ++ c2) s' ->
+    exists2 si : sestate, ssem prg s c1 si & ssem prg si c2 s'.
+Proof.
+elim: c1 s => /= [|i c1 ih] s c; first by exists s.
+by case/ssem_inv: c => si [?] /ih []; eauto with ssem.
+Qed.
+
+(* -------------------------------------------------------------------- *)
+Lemma ssem_inv1_r prg s i s' : ssem prg s [:: i] s' -> ssem_I prg s i s'.
+Proof. by case/ssem_inv=> si [? /ssem_inv ->]. Qed.
+
+(* -------------------------------------------------------------------- *)
+Lemma ssem_inv1 prg z s i s' : ssem prg s [:: MkI z i] s' -> ssem_i prg s i s'.
+Proof. by case/ssem_inv1_r/ssem_I_inv=> [ir] [ii] [] [_ ->]. Qed.
+
+(* -------------------------------------------------------------------- *)
+Lemma hoare_seq prg R P Q c1 c2 :
+  hoare prg P c1 R -> hoare prg R c2 Q -> hoare prg P (c1 ++ c2) Q.
+Proof. by move=> h1 h2 s s' /ssem_app_inv[si hc1 hc2]; eauto. Qed.
+
+(* -------------------------------------------------------------------- *)
+Lemma hoare_rcons prg R P Q c i :
+  hoare prg P c R -> hoare prg R [:: i] Q -> hoare prg P (rcons c i) Q.
+Proof. by move=> h1 h2; rewrite -cats1; apply: (@hoare_seq _ R). Qed.
+
+(* -------------------------------------------------------------------- *)
+Lemma hoare_while prg z I e c :
+   hoare prg (fun s => I s /\ ssem_pexpr s e = ok strue) c I
+-> hoare prg I [:: MkI z (Cwhile e c)]
+         (fun s => I s /\ ssem_pexpr s e = ok sfalse).
+Proof.
+move=> h s s' /ssem_inv1; move: {-2}(Cwhile _ _) (erefl (Cwhile e c)).
+move=> ir eq C; elim: C eq => // {s}; last first.
++ move=> s e' c' hlet [<- _] Is; split=> //.
+  by elim/rbindP: hlet=> v -> /sto_bool_inv ->.
+move=> s1 s2 s3 e' c' hlet hc' _ ih [eqe eqc] Is1; subst e' c'.
+apply/ih/(h s1) => //; split=> //; elim/rbindP: hlet.
+by move=> v -> /sto_bool_inv ->.
+Qed.
+
+(* -------------------------------------------------------------------- *)
+Lemma hoare_while_seq prg z P I c0 e c :
+   hoare prg P c0 I
+-> hoare prg (fun s => I s /\ ssem_pexpr s e = ok strue) c I
+-> hoare prg P (rcons c0 (MkI z (Cwhile e c)))
+         (fun s => I s /\ ssem_pexpr s e = ok sfalse).
+Proof. by move=> h0 h; apply: (hoare_rcons h0); apply: hoare_while. Qed.
