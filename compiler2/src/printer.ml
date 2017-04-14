5e3729b7f71d0d071e4a81c51e71aa1ed5bcbfc8
Benjamin Gregoire <Benjamin.Gregoire@inria.fr>
2017-04-14 16:17:25 +0200

    more on printing

--- compiler2/src/printer.ml
+++ compiler2/src/printer.ml
@@ -3,6 +3,13 @@ open Prog
 module F = Format
 module B = Bigint
 
+let rec pp_list sep pp fmt xs =
+  let pp_list = pp_list sep pp in
+    match xs with
+    | []      -> ()
+    | [x]     -> Format.fprintf fmt "%a" pp x
+    | x :: xs -> Format.fprintf fmt "%a%(%)%a" pp x sep pp_list xs
+
 let pp_btype fmt = function
   | Bool -> F.fprintf fmt "bool"
   | U i  -> F.fprintf fmt "U%i" (int_of_ws i)
@@ -12,7 +19,7 @@ let pp_gtype pp_size fmt = function
   | Bty ty -> pp_btype fmt ty
   | Arr(ws,e) -> F.fprintf fmt "%a[%a]" pp_btype (U ws) pp_size e
 
-let pp_var_i pp_var fmt v = pp_var fmt v.L.pl_desc 
+let pp_gvar_i pp_var fmt v = pp_var fmt (L.unloc v)
 
 let string_of_op2 = function
   | Oand  -> "&&"  
@@ -27,8 +34,8 @@ let string_of_op2 = function
   | Ogt   -> ">"
   | Oge   -> ">="
 
-let pp_expr pp_var = 
-  let pp_var_i = pp_var_i pp_var in
+let pp_ge pp_var = 
+  let pp_var_i = pp_gvar_i pp_var in
   let rec pp_expr fmt = function
   | Pconst i    -> B.pp_print fmt i
   | Pbool  b    -> F.fprintf fmt "%b" b
@@ -45,5 +52,72 @@ let pp_expr pp_var =
                 pp_expr e1 (string_of_op2 op) pp_expr e2 in
   pp_expr   
 
+let pp_glv pp_var fmt = function
+  | Lnone _ -> F.fprintf fmt "_"
+  | Lvar x  -> pp_gvar_i pp_var fmt x
+  | Lmem (ws, x, e) -> 
+    F.fprintf fmt "@[store %a@ %a@ %a@]" 
+     pp_btype (U ws) (pp_gvar_i pp_var) x (pp_ge pp_var) e
+  | Laset(x,e) -> 
+    F.fprintf fmt "%a[%a]" (pp_gvar_i pp_var) x (pp_ge pp_var) e
+
+let pp_ges pp_var fmt es = 
+  Format.fprintf fmt "@[%a@]" (pp_list ",@ " (pp_ge pp_var)) es
+
+let pp_glvs pp_var fmt lvs = 
+  match lvs with
+  | [] -> assert false
+  | [x] -> pp_glv pp_var fmt x
+  | _   -> F.fprintf fmt "(@[%a@])" (pp_list ",@ " (pp_glv pp_var)) lvs
+
+let pp_opn _o =
+  "FIXME"
+
+  
+let rec pp_gi pp_var fmt i = 
+  match i.i_desc with 
+  | Cblock c -> F.fprintf fmt "@[<v>%a@]" (pp_cblock pp_var) c
+  | Cassgn(x,_,e) -> 
+    F.fprintf fmt "@[<hov 2>%a =@ %a;@]" (pp_glv pp_var) x (pp_ge pp_var) e
+  | Copn(x,o,e) -> (* FIXME *)
+    F.fprintf fmt "@[<hov 2>%a =@ %s(%a);@]" 
+       (pp_glvs pp_var) x (pp_opn o) 
+       (pp_ges pp_var) e
+  | Cif(e, c, []) ->
+    F.fprintf fmt "@[<v>if %a %a@]"
+      (pp_ge pp_var) e (pp_cblock pp_var) c
+  | Cif(e, c1, c2) ->
+    F.fprintf fmt "@[<v>if %a %a else %a@]"
+      (pp_ge pp_var) e (pp_cblock pp_var) c1 (pp_cblock pp_var) c2
+  | Cfor(i,(dir,lo,hi), c) ->
+    let dir, e1, e2 = 
+      if dir = UpTo then "to", lo, hi else "downto", hi, lo in
+    F.fprintf fmt "@[<v>for %a = @[%a %s@ %a] %a@]"
+      (pp_gvar_i pp_var) i (pp_ge pp_var) e1 dir (pp_ge pp_var) e2
+      (pp_gc pp_var) c
+  | Cwhile(e, c) ->
+    F.fprintf fmt "@[<v>while (%a) {@   %a@ }@]"
+      (pp_ge pp_var) e (pp_gc pp_var) c          
+  | Ccall(_ii,x,f,e) -> (* FIXME ii *)
+    F.fprintf fmt "@[<hov 2> %a =@ %s(%a);@]"
+      (pp_glvs pp_var) x f.f_name (pp_ges pp_var) e
+
+and pp_gc pp_var fmt c = 
+  F.fprintf fmt "@[<v>%a@]" (pp_list "@ " (pp_gi pp_var)) c
+
+and pp_cblock pp_var fmt c =
+  F.fprintf fmt "{@   %a@ }@]" (pp_gc pp_var) c
+    
+(*        
+let pp_gfun pp_var fmt fc = 
+  F.fprintf fmt "@[proc %s (%a) = {@  @[<v>%a@ %a@]@ }@]"
+ *)
+      
+    
+
+
+
+
+ 
 
 
