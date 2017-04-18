b17229088b4a7ab2408cf848dda6028cd34aa9fa
Arthur Blot <arthur.blot78@gmail.com>
2017-04-18 14:44:45 +0200

    Progress on x86-64

--- proofs/compiler/x86.v
+++ proofs/compiler/x86.v
@@ -33,7 +33,7 @@ From mathcomp Require Import choice fintype eqtype div seq zmodp.
 Require Import ZArith.
 
 Require Import strings word utils type var expr.
-Require Import memory sem linear compiler_util.
+Require Import low_memory memory sem linear compiler_util.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -47,11 +47,33 @@ Definition port_number := nat.
 Definition interrupt_type := nat.
 Definition selector := nat.
 
-Inductive register : Set := EAX | ECX | EDX | EBX | ESP | EBP | ESI | EDI.
+(* TODO: this is x86-32 registers *)
+Inductive register : Set :=
+    RAX | RCX | RDX | RBX | RSP | RBP | RSI | RDI
+  | R8  | R9  | R10 | R11 | R12 | R13 | R14 | R15.
+
+Definition reg_of_string (s: string) :=
+  match s with
+  | "RAX"%string => Some RAX
+  | "RCX"%string => Some RCX
+  | "RDX"%string => Some RDX
+  | "RBX"%string => Some RBX
+  | "RSP"%string => Some RSP
+  | "RBP"%string => Some RBP
+  | "RSI"%string => Some RSI
+  | "RDI"%string => Some RDI
+  | "R8"%string => Some R8
+  | "R9"%string => Some R9
+  | "R10"%string => Some R10
+  | "R11"%string => Some R11
+  | "R12"%string => Some R12
+  | "R13"%string => Some R13
+  | "R14"%string => Some R14
+  | "R15"%string => Some R15
+  | _ => None
+  end.
+
 Inductive scale : Set := Scale1 | Scale2 | Scale4 | Scale8.
-Inductive segment_register : Set := ES | CS | SS | DS | FS | GS.
-Inductive control_register : Set := CR0 | CR2 | CR3 | CR4.
-Inductive debug_register   : Set := DR0 | DR1 | DR2 | DR3 | DR6 | DR7.
 Inductive rflag : Set := CF | PF | AF | ZF | SF | OF.
 
 Definition rflag_of_string (s: string) :=
@@ -74,8 +96,7 @@ Record address : Set := mkAddress {
 Inductive operand : Set := 
 | Imm_op : word -> operand
 | Reg_op : register -> operand
-| Address_op : address -> operand
-| Offset_op : word -> operand.
+| Address_op : address -> operand.
 
 Inductive reg_or_immed : Set := 
 | Reg_ri : register -> reg_or_immed
@@ -99,29 +120,7 @@ Inductive condition_type : Set :=
 | LE_ct (* less than or equal to, not greater than *)
 | NLE_ct (* not less than or equal to, greater than *).
 
-(* Floating point instruction syntax *)
-
-Inductive fp_debug_register : Set := eMF | eDB | eBP | eUD | eNM | eDF | eSS | eGP | ePF | eAC | eMC.
-
-Inductive fp_operand : Set := 
-| FPS_op : nat -> fp_operand 	    (*an index from 0 to 7 relative to stack top *)
-| FPM16_op : address -> fp_operand
-| FPM32_op : address -> fp_operand
-| FPM64_op : address -> fp_operand
-| FPM80_op : address -> fp_operand. 
-
-(* floating-point condition tpe *)
-Inductive fp_condition_type : Set :=
-| B_fct (* below *)
-| E_fct (* equal *)
-| BE_fct (* below or equal *)
-| U_fct (* unordered *)
-| NB_fct (* not below *)
-| NE_fct (* not equal *)
-| NBE_fct (* not below or equal *)
-| NU_fct (* not unordered *).
-
-(*MMX syntax *)
+(* MMX syntax *)
 
 (* Eight 64-bit mmx registers; mmx registers are syntactically
    different from fpu registers, but are semantically mapped
@@ -161,25 +160,9 @@ Inductive instr : Set :=
 (* "High-level" assembly-specific instructions *)
 | LABEL : label -> instr
 
-(* two parts:  1-byte opcode instructions, followed by 2-byte in alphabetical order,
-   following Table B-13 *) 
-| AAA 
-| AAD 
-| AAM 
-| AAS 
-(* w is the width bit; 
-   with no operand override prefix, 
-     when w is true, then it's a 32-bit operation;
-     when w is false, then it's a 8-bit operation;
-   with an operand override prefix,
-     when w is true, it's a 16-bit operation;
-     when w is false, it's a 8-bit operation;
-   See load_op/set-op in X86Semantics.v *)
-| ADC   : forall (w:bool)(op1 op2:operand), instr
-| ADD   : forall (w:bool)(op1 op2:operand), instr
-| AND   : forall (w:bool)(op1 op2:operand), instr
-| ARPL  : forall (op1 op2:operand), instr
-| BOUND : forall (op1 op2:operand), instr
+| ADC   : forall (s:wsize)(op1 op2:operand), instr
+| ADD   : forall (s:wsize)(op1 op2:operand), instr
+| AND   : forall (s:wsize)(op1 op2:operand), instr
 | BSF   : forall (op1 op2:operand), instr
 | BSR   : forall (op1 op2:operand), instr
 | BSWAP : forall (r:register), instr
@@ -187,129 +170,75 @@ Inductive instr : Set :=
 | BTC   : forall (op1 op2:operand), instr
 | BTR   : forall (op1 op2:operand), instr
 | BTS   : forall (op1 op2:operand), instr
-| CALL  : forall (near:bool)(absolute: bool)(op1:operand)(sel:option selector), instr
-| CDQ 
-| CLC 
-| CLD 
-| CLI 
-| CLTS 
+(*| CALL  : forall (near:bool)(absolute: bool)(op1:operand)(sel:option selector), instr*)
+| CLC
+| CLD
+| CLI
 | CMC
 | CMOVcc : forall (ct:condition_type)(op1 op2: operand), instr
-| CMP    : forall (w:bool)(op1 op2:operand), instr
-| CMPS   : forall (w:bool), instr
-| CMPXCHG: forall (w:bool)(op1 op2:operand), instr
-| CPUID 
-| CWDE 
-| DAA 
-| DAS
-| DEC   : forall (w:bool)(op1:operand), instr
-| DIV   : forall (w:bool)(op1:operand), instr
+| CMP    : forall (s:wsize)(op1 op2:operand), instr
+| CMPS   : forall (s:wsize), instr
+| CMPXCHG: forall (s:wsize)(op1 op2:operand), instr
+| CWD
+| CWDE
+| DEC   : forall (s:wsize)(op1:operand), instr
+| DIV   : forall (s:wsize)(op1:operand), instr
+| IDIV  : forall (s:wsize)(op1:operand), instr
+| IMUL  : forall (s:wsize)(op1:operand) (opopt: option operand) (iopt:option nat), instr
+| INC   : forall (s:wsize)(op1:operand), instr
+| INVD  : instr
+| INVLPG : forall (op1:operand), instr
+| Jcc   : condition_type -> label -> instr
+| JCXZ  : label -> instr
+| JMP   : label -> instr
+| LAHF
+| LEA   : forall (op1 op2:operand), instr
+| LODS  : forall (s:wsize), instr
+| LOOP  : forall (disp:nat), instr
+| LOOPZ : forall (disp:nat), instr
+| LOOPNZ: forall (disp:nat), instr
+| MOV   : forall (s:wsize)(op1 op2:operand), instr
+| MOVBE : forall (op1 op2:operand), instr
+| MOVS  : forall (s:wsize), instr
+| MOVSX : forall (s:wsize)(op1 op2:operand), instr
+| MOVZX : forall (s:wsize)(op1 op2:operand), instr
+| MUL   : forall (s:wsize)(op1:operand), instr
+| NEG   : forall (s:wsize)(op:operand), instr
+| NOT   : forall (s:wsize)(op:operand), instr
+| OR    : forall (s:wsize)(op1 op2:operand), instr
+| POP   : forall (op1:operand), instr
+| POPA
+| POPF
+| PUSH  : forall (s:wsize)(op1:operand), instr
+| PUSHA
+| PUSHF
+| RCL   : forall (s:wsize)(op1:operand)(ri:reg_or_immed), instr
+| RCR   : forall (s:wsize)(op1:operand)(ri:reg_or_immed), instr
+(*| RET   : instr *)
+| ROL   : forall (s:wsize)(op1:operand)(ri:reg_or_immed), instr
+| ROR   : forall (s:wsize)(op1:operand)(ri:reg_or_immed), instr
+| SAHF
+| SAR   : forall (s:wsize)(op1:operand)(ri:reg_or_immed), instr
+| SBB   : forall (s:wsize)(op1 op2:operand), instr
+| SCAS  : forall (s:wsize), instr
+| SETcc : forall (ct:condition_type)(op1:operand), instr
+| SHL   : forall (s:wsize)(op1:operand)(ri:reg_or_immed), instr
+| SHLD  : forall (op1:operand)(r:register)(ri:reg_or_immed), instr
+| SHR   : forall (s:wsize)(op1:operand)(ri:reg_or_immed), instr
+| SHRD  : forall (op1:operand)(r:register)(ri:reg_or_immed), instr
+| STC
+| STD
+| STI
+| STOS  : forall (s:wsize), instr
+| SUB   : forall (s:wsize)(op1 op2:operand), instr
+| TEST  : forall (s:wsize)(op1 op2:operand), instr
+| WBINVD
+| XADD  : forall (s:wsize)(op1 op2:operand), instr
+| XCHG  : forall (s:wsize)(op1 op2:operand), instr
+| XLAT
+| XOR   : forall (s:wsize)(op1 op2:operand), instr
 
-(*Floating-point syntax defined starting here. Table B-38 explains how to read instructions and B-39 has the
-actual instruction details. Instructions can be found here: 
-http://download.intel.com/products/processor/manual/325383.pdf*)
-| F2XM1 : instr
-| FABS : instr
-(* zerod is true iff st(0) is the destination; 
-   op1 is the destination when zerod=false *)
-| FADD : forall (zerod: bool) (op1: fp_operand), instr
-| FADDP : forall (op1: fp_operand), instr
-| FBLD : forall (op1: fp_operand), instr
-| FBSTP : forall (op1: fp_operand), instr
-| FCHS : instr
-(* FCLEX is the same as FWAIT followed FNCLEX
-   | FCLEX : instr *)
-| FCMOVcc : forall (ct:fp_condition_type)(op1: fp_operand), instr
-| FCOM : forall (op1: fp_operand), instr
-| FCOMP : forall (op1: fp_operand), instr
-| FCOMPP : instr
-| FCOMIP : forall (op1: fp_operand), instr
-| FCOS : instr
-| FDECSTP : instr
-| FDIV : forall (zerod: bool) (op: fp_operand), instr
-| FDIVP : forall (op1: fp_operand), instr
-| FDIVR : forall (zerod: bool) (op: fp_operand), instr
-| FDIVRP : forall (op1: fp_operand), instr
-| FFREE : forall (op1: fp_operand), instr
-| FIADD : forall (op1: fp_operand), instr
-| FICOM : forall (op1: fp_operand), instr
-| FICOMP : forall (op1: fp_operand), instr
-| FIDIV : forall (op1: fp_operand), instr
-| FIDIVR : forall (op1: fp_operand), instr
-| FILD : forall (op1: fp_operand), instr
-| FIMUL : forall (op1: fp_operand), instr
-| FINCSTP : instr
-(* FINIT is FWAIT followed by FNINIT;
-   | FINIT : instr *)
-| FIST : forall (op1: fp_operand), instr
-| FISTP : forall (op1: fp_operand), instr
-| FISUB : forall (op1: fp_operand), instr
-| FISUBR : forall (op1: fp_operand), instr
-| FLD : forall (op1: fp_operand), instr
-| FLD1 : instr
-| FLDCW : forall (op1: fp_operand), instr
-| FLDENV : forall (op1: fp_operand), instr
-| FLDL2E : instr
-| FLDL2T : instr
-| FLDLG2 : instr
-| FLDLN2 : instr
-| FLDPI : instr
-| FLDZ : instr
-| FMUL : forall (zerod: bool) (op1: fp_operand), instr
-| FMULP : forall (op1: fp_operand), instr
-| FNCLEX : instr
-| FNINIT : instr
-| FNOP : instr
-| FNSAVE : forall (op1: fp_operand), instr
-| FNSTCW : forall (op1 : fp_operand), instr
-(* FNSTSW None means that storing the status word to AX *)
-| FNSTSW : forall (op1 : option fp_operand), instr
-| FPATAN : instr
-| FPREM : instr
-| FPREM1 : instr
-| FPTAN : instr
-| FRNDINT : instr
-| FRSTOR : forall (op1: fp_operand), instr
-(* FSAVE's encoding the same as FWAIT followed FNSAVE
-   | FSAVE : forall (op1: fp_operand), instr *)
-| FSCALE : instr
-| FSIN : instr
-| FSINCOS : instr
-| FSQRT : instr
-| FST : forall (op1: fp_operand), instr
-
-(* FSTCW's is the same as FWAIT followed by FNSTCW
-   | FSTCW : forall (op1: fp_operand), instr *)
-
-| FSTENV : forall (op1: fp_operand), instr
-| FSTP : forall (op1: fp_operand), instr
-
-(* FSTSW is the same as FWAIT followed by FNSTSW 
-   | FSTSW : forall (op1: option fp_operand), instr *)
-
-(* st(0) <- st(0) - op, when zerod is true;
-   op <- op - st(0), when zerod is false and op can only be st(i) *)
-| FSUB : forall (zerod: bool) (op: fp_operand), instr
-| FSUBP : forall (op1: fp_operand), instr
-(* reverse subtraction
-   st(0) <- op - st(0), when zerod is true;
-   op <- st(0) - op, when zerod is false and op can only be st(i) *)
-| FSUBR : forall (zerod: bool) (op: fp_operand), instr
-| FSUBRP : forall (op1: fp_operand), instr
-| FTST : instr
-| FUCOM : forall (op1: fp_operand), instr
-| FUCOMP : forall (op1: fp_operand), instr
-| FUCOMPP : instr
-| FUCOMI : forall (op1: fp_operand), instr
-| FUCOMIP : forall (op1: fp_operand), instr
-| FXAM : instr
-| FXCH : forall (op1: fp_operand), instr
-| FXTRACT : instr
-| FYL2X : instr
-| FYL2XP1 : instr
-| FWAIT : instr
-(*Floating-Point syntax ends here for now*)
-(*MMX syntax starting here (from table B.5.3) *)
+(* MMX instructions *)
 | EMMS : instr
 | MOVD : forall (op1 op2: mmx_operand), instr
 | MOVQ : forall (op1 op2: mmx_operand), instr
@@ -324,7 +253,7 @@ http://download.intel.com/products/processor/manual/325383.pdf*)
 | PCMPEQ : forall  (gg:mmx_granularity) (op1 op2 : mmx_operand), instr
 | PCMPGT : forall  (gg:mmx_granularity) (op1 op2 : mmx_operand), instr
 | PMADDWD : forall  (op1 op2 : mmx_operand), instr
-| PMULHUW : forall  (op1 op2 : mmx_operand), instr
+(*| PMULHUW : forall  (op1 op2 : mmx_operand), instr*)
 | PMULHW : forall  (op1 op2 : mmx_operand), instr
 | PMULLW : forall  (op1 op2 : mmx_operand), instr
 | POR : forall  (op1 op2 : mmx_operand), instr
@@ -337,9 +266,8 @@ http://download.intel.com/products/processor/manual/325383.pdf*)
 | PUNPCKH : forall (gg:mmx_granularity) (op1 op2: mmx_operand), instr
 | PUNPCKL : forall (gg:mmx_granularity) (op1 op2: mmx_operand), instr
 | PXOR : forall  (op1 op2 : mmx_operand), instr
-(*End of MMX syntax *)
 
-(*SSE Syntax (Table B.8 in manual) *)
+(* SSE instructions *)
 | ADDPS : forall (op1 op2: sse_operand), instr
 | ADDSS : forall (op1 op2: sse_operand), instr
 | ANDNPS : forall (op1 op2: sse_operand), instr
@@ -393,7 +321,7 @@ http://download.intel.com/products/processor/manual/325383.pdf*)
 | PMINSW : forall (op1 op2: sse_operand), instr
 | PMINUB : forall (op1 op2: sse_operand), instr
 | PMOVMSKB : forall (op1 op2: sse_operand), instr
-(*| PMULHUW : forall (op1 op2: sse_operand), instr *)
+| PMULHUW : forall (op1 op2: sse_operand), instr
 | PSADBW : forall (op1 op2: sse_operand), instr
 | PSHUFW : forall (op1 op2: sse_operand) (imm:nat), instr
 | MASKMOVQ : forall (op1 op2: sse_operand), instr
@@ -403,119 +331,19 @@ http://download.intel.com/products/processor/manual/325383.pdf*)
 | PREFETCHT1 : forall (op1: sse_operand), instr
 | PREFETCHT2 : forall (op1: sse_operand), instr
 | PREFETCHNTA : forall (op1: sse_operand), instr
-| SFENCE: instr
-(*end SSE, start SSE2 *)
-
-(*End of SSE Syntax *)
-| HLT  
-| IDIV  : forall (w:bool)(op1:operand), instr
-(* This one is kind of funny -- there are three cases:
-   one operand, two operand, and three operand. The one operand
-   form is precise and implicitly uses EAX; the other two variants
-   can potentially lose some data due to overflow. *)
-| IMUL  : forall (w:bool)(op1:operand) (opopt: option operand) (iopt:option nat), instr
-| IN    : forall (w:bool)(p: option port_number), instr
-| INC   : forall (w:bool)(op1:operand), instr
-| INS   : forall (w:bool), instr
-| INTn  : forall (it:interrupt_type),instr
-| INT   : instr
-| INTO  : instr
-| INVD  : instr
-| INVLPG : forall (op1:operand), instr
-| IRET   : instr
-| Jcc   : condition_type -> label -> instr
-| JCXZ  : forall (b:word), instr
-| JMP   : label -> instr
-| LAHF
-| LAR   : forall (op1 op2:operand), instr
-| LDS   : forall (op1 op2:operand), instr
-| LEA   : forall (op1 op2:operand), instr
-| LEAVE
-| LES   : forall (op1 op2:operand), instr
-| LFS   : forall (op1 op2:operand), instr
-| LGDT  : forall (op1:operand), instr
-| LGS   : forall (op1 op2:operand), instr
-| LIDT  : forall (op1:operand), instr
-| LLDT  : forall (op1:operand), instr
-| LMSW  : forall (op1:operand), instr
-| LODS  : forall (w:bool), instr
-| LOOP  : forall (disp:nat), instr
-| LOOPZ : forall (disp:nat), instr
-| LOOPNZ: forall (disp:nat), instr
-| LSL   : forall (op1 op2:operand), instr
-| LSS   : forall (op1 op2:operand), instr
-| LTR   : forall (op1:operand), instr
-| MOV   : forall (w:bool)(op1 op2:operand), instr
-(* Note:  when direction is true, we move the first operand to the second.  
- * When the direction is false, we move the second operand to the first. *)
-| MOVCR : forall (direction:bool)(cr:control_register)(r:register), instr
-| MOVDR : forall (direction:bool)(dr:debug_register)(r:register), instr
-| MOVSR : forall (direction:bool)(sr:segment_register)(op1:operand), instr
-| MOVBE : forall (op1 op2:operand), instr
-| MOVS  : forall (w:bool), instr
-| MOVSX : forall (w:bool)(op1 op2:operand), instr
-| MOVZX : forall (w:bool)(op1 op2:operand), instr
-| MUL   : forall (w:bool)(op1:operand), instr
-| NEG   : forall (w:bool)(op:operand), instr
-| NOP   : forall (op:operand), instr
-| NOT   : forall (w:bool)(op:operand), instr
-| OR    : forall (w:bool)(op1 op2:operand), instr
-| OUT   : forall (w:bool)(p:option port_number), instr
-| OUTS  : forall (w:bool), instr
-| POP   : forall (op1:operand), instr
-| POPSR : forall (sr:segment_register), instr
-| POPA 
-| POPF
-| PUSH  : forall (w:bool)(op1:operand), instr
-| PUSHSR: forall (sr:segment_register), instr
-| PUSHA
-| PUSHF
-| RCL   : forall (w:bool)(op1:operand)(ri:reg_or_immed), instr
-| RCR   : forall (w:bool)(op1:operand)(ri:reg_or_immed), instr
-| RDMSR     : instr
-| RDPMC     : instr
-| RDTSC     : instr
-| RDTSCP    : instr
-| RET   : instr
-| ROL   : forall (w:bool)(op1:operand)(ri:reg_or_immed), instr
-| ROR   : forall (w:bool)(op1:operand)(ri:reg_or_immed), instr
-| RSM
-| SAHF
-| SAR   : forall (w:bool)(op1:operand)(ri:reg_or_immed), instr
-| SBB   : forall (w:bool)(op1 op2:operand), instr
-| SCAS  : forall (w:bool), instr
-| SETcc : forall (ct:condition_type)(op1:operand), instr
-| SGDT  : forall (op1:operand), instr
-| SHL   : forall (w:bool)(op1:operand)(ri:reg_or_immed), instr
-| SHLD  : forall (op1:operand)(r:register)(ri:reg_or_immed), instr
-| SHR   : forall (w:bool)(op1:operand)(ri:reg_or_immed), instr
-| SHRD  : forall (op1:operand)(r:register)(ri:reg_or_immed), instr
-| SIDT  : forall (op1:operand), instr
-| SLDT  : forall (op1:operand), instr
-| SMSW  : forall (op1:operand), instr
-| STC
-| STD
-| STI
-| STOS  : forall (w:bool), instr
-| STR   : forall (op1:operand), instr
-| SUB   : forall (w:bool)(op1 op2:operand), instr
-| TEST  : forall (w:bool)(op1 op2:operand), instr
-| UD2
-| VERR  : forall (op1:operand), instr
-| VERW  : forall (op1:operand), instr
-(* | WAIT  removed because it's the same as FWAIT*)
-| WBINVD
-| WRMSR
-| XADD  : forall (w:bool)(op1 op2:operand), instr
-| XCHG  : forall (w:bool)(op1 op2:operand), instr
-| XLAT 
-| XOR   : forall (w:bool)(op1 op2:operand), instr.
+| SFENCE: instr.
 
 Definition cmd := seq instr.
 
 Definition rflag_of_var_i (v: var_i) :=
   match v with
   | VarI (Var sbool s) _ => rflag_of_string s
+  | _ => None
+  end.
+
+Definition reg_of_var_i (v: var_i) :=
+  match v with
+  | VarI (Var sword s) _ => reg_of_string s
   | _ => None
   end.
 
@@ -542,17 +370,70 @@ Definition assemble_cond ii (e: pexpr) :=
   | _ => cierror ii (Cerr_assembler "invalid branching")
   end.
 
+Definition word_of_int ii (z: Z) :=
+  if (I64.signed (I64.repr z) == z) then
+    ciok (I64.repr z)
+  else
+    cierror ii (Cerr_assembler "Integer constant out of bounds").
+
+Definition word_of_pexpr ii e :=
+  match e with
+  | Pcast (Pconst z) => word_of_int ii z
+  | _ => cierror ii (Cerr_assembler "Invalid integer constant")
+  end.
+
+Definition operand_of_lval ii (l: lval) :=
+  match l with
+  | Lnone _ => cierror ii (Cerr_assembler "Lnone not implemented")
+  | Lvar v =>
+     match (reg_of_var_i v) with
+     | Some s => ciok (Reg_op s)
+     | None => cierror ii (Cerr_assembler "Invalid register in Lvar")
+     end
+  | Lmem v e =>
+     match (reg_of_var_i v) with
+     | Some s =>
+         Let w := word_of_pexpr ii e in
+         ciok (Address_op (mkAddress w (Some s) None))
+     | None => cierror ii (Cerr_assembler "Invalid register in Lvar")
+     end
+  | Laset v e => cierror ii (Cerr_assembler "Laset not handled in assembler")
+  end.
+
+Definition operand_of_pexpr ii (e: pexpr) :=
+  match e with
+  | Pcast (Pconst z) =>
+     Let w := word_of_int ii z in
+     ciok (Imm_op w)
+  | Pvar v =>
+     match (reg_of_var_i v) with
+     | Some s => ciok (Reg_op s)
+     | None => cierror ii (Cerr_assembler "Invalid register in pexpr")
+     end
+  | Pload v e =>
+     match (reg_of_var_i v) with
+     | Some s =>
+         Let w := word_of_pexpr ii e in
+         ciok (Address_op (mkAddress w (Some s) None))
+     | None => cierror ii (Cerr_assembler "Invalid register in Lvar")
+     end
+  | _ => cierror ii (Cerr_assembler "Invalid pexpr")
+  end.
+
 Definition assemble_i (li: linstr) : ciexec instr :=
   let (ii, i) := li in
   match i with
-  | Lassgn _ _ _ => cierror ii (Cerr_assembler "assgn")
+  | Lassgn l _ e =>
+     Let dst := operand_of_lval ii l in
+     Let src := operand_of_pexpr ii e in
+     ciok (MOV U64 dst src)
   | Lopn l o p => cierror ii (Cerr_assembler "opn")
   | Llabel l => ciok (LABEL l)
   | Lgoto l => ciok (JMP l)
   | Lcond e l =>
      Let cond := assemble_cond ii e in
      ciok (Jcc cond l)
-  | Lreturn => ciok RET
+  | Lreturn => ciok (LABEL xH)
   end.
 
 Definition assemble_c (lc: lcmd) : ciexec cmd :=
