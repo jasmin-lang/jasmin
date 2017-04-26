83ab313467b3c527f9060060efc7a7f1278430b4
Vincent Laporte <Vincent.Laporte@gmail.com>
2017-04-26 09:36:35 +0000

    whitespace

--- compiler2/src/conv.ml
+++ compiler2/src/conv.ml
@@ -6,11 +6,11 @@ module C = Expr
 let rec pos_of_bi bi =
   let open B.Notations in
   if bi <=^ B.one then BinNums.Coq_xH
-  else 
+  else
     let p = pos_of_bi (B.rshift bi 1) in
-    if (B.erem bi (B.of_int 2)) =^ B.one 
-    then BinNums.Coq_xI p 
-    else BinNums.Coq_xO p 
+    if (B.erem bi (B.of_int 2)) =^ B.one
+    then BinNums.Coq_xI p
+    else BinNums.Coq_xO p
 
 let rec bi_of_pos pos =
   let open B.Notations in
@@ -24,11 +24,11 @@ let z_of_bi bi =
   if bi =^ B.zero then BinNums.Z0
   else if bi <^ B.zero then BinNums.Zneg (pos_of_bi (B.abs bi))
   else BinNums.Zpos (pos_of_bi bi)
-  
+
 let bi_of_z z =
   match z with
   | BinNums.Zneg p -> B.neg (bi_of_pos p)
-  | BinNums.Z0     -> B.zero 
+  | BinNums.Z0     -> B.zero
   | BinNums.Zpos p -> bi_of_pos p
 
 let pos_of_int i = pos_of_bi (B.of_int i)
@@ -53,15 +53,15 @@ let string_of_string0 s0 =
 
 let cty_of_ty = function
   | Bty Bool      -> T.Coq_sbool
-  | Bty Int       -> T.Coq_sint 
+  | Bty Int       -> T.Coq_sint
   | Bty (U W64)   -> T.Coq_sword
   | Arr (W64, sz) -> T.Coq_sarr (pos_of_int sz)
   | _ -> assert false
 
 let ty_of_cty = function
-  | T.Coq_sbool  ->  Bty Bool     
-  | T.Coq_sint   ->  Bty Int      
-  | T.Coq_sword  ->  Bty (U W64)  
+  | T.Coq_sbool  ->  Bty Bool
+  | T.Coq_sint   ->  Bty Int
+  | T.Coq_sword  ->  Bty (U W64)
   | T.Coq_sarr p ->  Arr (W64, (int_of_pos p))
 
 (* ------------------------------------------------------------------------ *)
@@ -77,7 +77,7 @@ type 'info coq_tbl = {
      finfo         : (int, L.t * call_conv) Hashtbl.t;
   }
 
-let new_count tbl = 
+let new_count tbl =
   let n = tbl.count in
   tbl.count <- n + 1;
   n
@@ -95,29 +95,29 @@ let empty_tbl () = {
 
 (* ------------------------------------------------------------------------ *)
 
-let cvar_of_var tbl v = 
-  try Hv.find tbl.cvar v 
-  with Not_found -> 
+let cvar_of_var tbl v =
+  try Hv.find tbl.cvar v
+  with Not_found ->
     let s = v.v_name ^ "." ^ (string_of_int (int_of_uid v.v_id)) in
-    let cv = { 
+    let cv = {
       Var.vtype = cty_of_ty v.v_ty;
-      Var.vname = string0_of_string s 
+      Var.vname = string0_of_string s
     } in
     Hv.add tbl.cvar v cv;
     Hashtbl.add tbl.var cv v;
     cv
 
-let var_of_cvar tbl cv = 
+let var_of_cvar tbl cv =
   try Hashtbl.find tbl.var cv
   with Not_found -> assert false
 
 (* ------------------------------------------------------------------------ *)
 
-let get_loc tbl p = 
+let get_loc tbl p =
   try Hashtbl.find tbl.vari (int_of_pos p)
   with Not_found -> assert false
 
-let set_loc tbl loc = 
+let set_loc tbl loc =
   let n = new_count tbl in
   Hashtbl.add tbl.vari n loc;
   pos_of_int n
@@ -133,34 +133,34 @@ let vari_of_cvari tbl v =
 
 (* ------------------------------------------------------------------------ *)
 let cop_of_op = function
-  | Oand -> C.Oand   
-  | Oor  -> C.Oor    
-  | Oadd -> C.Oadd   
-  | Omul -> C.Omul   
-  | Osub -> C.Osub   
-  | Oeq  -> C.Oeq    
+  | Oand -> C.Oand
+  | Oor  -> C.Oor
+  | Oadd -> C.Oadd
+  | Omul -> C.Omul
+  | Osub -> C.Osub
+  | Oeq  -> C.Oeq
   | Oneq -> C.Oneq
-  | Olt  -> C.Olt 
-  | Ole  -> C.Ole 
-  | Ogt  -> C.Ogt 
-  | Oge  -> C.Oge 
+  | Olt  -> C.Olt
+  | Ole  -> C.Ole
+  | Ogt  -> C.Ogt
+  | Oge  -> C.Oge
 
 let op_of_cop = function
-  | C.Oand -> Oand   
-  | C.Oor  -> Oor    
-  | C.Oadd -> Oadd   
-  | C.Omul -> Omul   
-  | C.Osub -> Osub   
-  | C.Oeq  -> Oeq    
+  | C.Oand -> Oand
+  | C.Oor  -> Oor
+  | C.Oadd -> Oadd
+  | C.Omul -> Omul
+  | C.Osub -> Osub
+  | C.Oeq  -> Oeq
   | C.Oneq -> Oneq
-  | C.Olt  -> Olt 
-  | C.Ole  -> Ole 
-  | C.Ogt  -> Ogt 
-  | C.Oge  -> Oge 
+  | C.Olt  -> Olt
+  | C.Ole  -> Ole
+  | C.Ogt  -> Ogt
+  | C.Oge  -> Oge
 
 (* ------------------------------------------------------------------------ *)
 
-let rec cexpr_of_expr tbl = function 
+let rec cexpr_of_expr tbl = function
   | Pconst z          -> C.Pconst (z_of_bi z)
   | Pbool  b          -> C.Pbool  b
   | Pcast (W64, e)    -> C.Pcast (cexpr_of_expr tbl e)
@@ -168,7 +168,7 @@ let rec cexpr_of_expr tbl = function
   | Pvar x            -> C.Pvar (cvari_of_vari tbl x)
   | Pget (x,e)        -> C.Pget (cvari_of_vari tbl x, cexpr_of_expr tbl e)
   | Pload (W64, x, e) -> C.Pload(cvari_of_vari tbl x, cexpr_of_expr tbl e)
-  | Pload _           -> assert false  
+  | Pload _           -> assert false
   | Pnot e            -> C.Pnot (cexpr_of_expr tbl e)
   | Papp2 (o, e1, e2) -> C.Papp2(cop_of_op o, cexpr_of_expr tbl e1, cexpr_of_expr tbl e2)
 
@@ -185,53 +185,53 @@ let rec expr_of_cexpr tbl = function
 (* ------------------------------------------------------------------------ *)
 
 let copn_of_opn = function
-  | Olnot     -> C.Olnot      
-  | Oxor      -> C.Oxor     
-  | Oland     -> C.Oland    
-  | Olor      -> C.Olor     
-  | Olsr      -> C.Olsr     
-  | Olsl      -> C.Olsl     
-  | Oif       -> C.Oif      
-  | Omulu     -> C.Omulu    
-  | Omuli     -> C.Omuli    
+  | Olnot     -> C.Olnot
+  | Oxor      -> C.Oxor
+  | Oland     -> C.Oland
+  | Olor      -> C.Olor
+  | Olsr      -> C.Olsr
+  | Olsl      -> C.Olsl
+  | Oif       -> C.Oif
+  | Omulu     -> C.Omulu
+  | Omuli     -> C.Omuli
   | Oaddcarry -> C.Oaddcarry
   | Osubcarry -> C.Osubcarry
-  | Oleu      -> C.Oleu     
-  | Oltu      -> C.Oltu     
-  | Ogeu      -> C.Ogeu     
-  | Ogtu      -> C.Ogtu     
-  | Oles      -> C.Oles     
-  | Olts      -> C.Olts     
-  | Oges      -> C.Oges     
-  | Ogts      -> C.Ogts     
-  | Oeqw      -> C.Oeqw   
+  | Oleu      -> C.Oleu
+  | Oltu      -> C.Oltu
+  | Ogeu      -> C.Ogeu
+  | Ogtu      -> C.Ogtu
+  | Oles      -> C.Oles
+  | Olts      -> C.Olts
+  | Oges      -> C.Oges
+  | Ogts      -> C.Ogts
+  | Oeqw      -> C.Oeqw
 
 let opn_of_copn = function
-  | C.Olnot     -> Olnot      
-  | C.Oxor      -> Oxor     
-  | C.Oland     -> Oland    
-  | C.Olor      -> Olor     
-  | C.Olsr      -> Olsr     
-  | C.Olsl      -> Olsl     
-  | C.Oif       -> Oif      
-  | C.Omulu     -> Omulu    
-  | C.Omuli     -> Omuli    
+  | C.Olnot     -> Olnot
+  | C.Oxor      -> Oxor
+  | C.Oland     -> Oland
+  | C.Olor      -> Olor
+  | C.Olsr      -> Olsr
+  | C.Olsl      -> Olsl
+  | C.Oif       -> Oif
+  | C.Omulu     -> Omulu
+  | C.Omuli     -> Omuli
   | C.Oaddcarry -> Oaddcarry
   | C.Osubcarry -> Osubcarry
-  | C.Oleu      -> Oleu     
-  | C.Oltu      -> Oltu     
-  | C.Ogeu      -> Ogeu     
-  | C.Ogtu      -> Ogtu     
-  | C.Oles      -> Oles     
-  | C.Olts      -> Olts     
-  | C.Oges      -> Oges     
-  | C.Ogts      -> Ogts     
-  | C.Oeqw      -> Oeqw     
- 
+  | C.Oleu      -> Oleu
+  | C.Oltu      -> Oltu
+  | C.Ogeu      -> Ogeu
+  | C.Ogtu      -> Ogtu
+  | C.Oles      -> Oles
+  | C.Olts      -> Olts
+  | C.Oges      -> Oges
+  | C.Ogts      -> Ogts
+  | C.Oeqw      -> Oeqw
+
 (* ------------------------------------------------------------------------ *)
 
 let clval_of_lval tbl = function
-  | Lnone loc      -> C.Lnone (set_loc tbl loc) 
+  | Lnone loc      -> C.Lnone (set_loc tbl loc)
   | Lvar x         -> C.Lvar  (cvari_of_vari tbl x)
   | Lmem (W64,x,e) -> C.Lmem  (cvari_of_vari tbl x, cexpr_of_expr tbl e)
   | Lmem _         -> assert false
@@ -285,42 +285,42 @@ let ii_of_cii = function
 
 (* ------------------------------------------------------------------------ *)
 
-let cfun_of_fun tbl fn = 
+let cfun_of_fun tbl fn =
   try Hashtbl.find tbl.funname fn
-  with Not_found -> 
+  with Not_found ->
     let p = pos_of_int (new_count tbl) in
     Hashtbl.add tbl.funname fn p;
     Hashtbl.add tbl.cfunname p fn;
     p
 
 let fun_of_cfun tbl p =
-  try Hashtbl.find tbl.cfunname p 
+  try Hashtbl.find tbl.cfunname p
   with Not_found -> assert false
-       
+
 (* ------------------------------------------------------------------------ *)
 
-let set_iinfo tbl loc ii = 
+let set_iinfo tbl loc ii =
   let n = new_count tbl in
   Hashtbl.add tbl.iinfo n (loc, ii);
   pos_of_int n
-  
-let get_iinfo tbl n = 
+
+let get_iinfo tbl n =
   try Hashtbl.find tbl.iinfo (int_of_pos n)
   with Not_found -> assert false
 
-let rec cinstr_of_instr tbl i c = 
+let rec cinstr_of_instr tbl i c =
   let n = set_iinfo tbl i.i_loc i.i_info in
   cinstr_r_of_instr_r tbl n i.i_desc c
 
-and cinstr_r_of_instr_r tbl p i tl = 
-  match i with 
+and cinstr_r_of_instr_r tbl p i tl =
+  match i with
   | Cblock c -> cstmt_of_stmt tbl c tl
-  | Cassgn(x,t,e) -> 
-    let ir  = 
+  | Cassgn(x,t,e) ->
+    let ir  =
       C.Cassgn(clval_of_lval tbl x, cat_of_at t, cexpr_of_expr tbl e) in
     C.MkI(p, ir) :: tl
   | Copn(x,o,e) ->
-    let ir = 
+    let ir =
       C.Copn(clval_of_lvals tbl x, copn_of_opn o, cexpr_of_exprs tbl e) in
     C.MkI(p, ir) :: tl
 
@@ -338,15 +338,15 @@ and cinstr_r_of_instr_r tbl p i tl =
     C.MkI(p, ir) :: tl
   | Cwhile(e,c) ->
     let ir = C.Cwhile(cexpr_of_expr tbl e, cstmt_of_stmt tbl c []) in
-    C.MkI(p,ir) :: tl 
+    C.MkI(p,ir) :: tl
   | Ccall(ii, x, f, e) ->
-    let ii = cii_of_ii ii in 
-    let ir = 
+    let ii = cii_of_ii ii in
+    let ir =
       C.Ccall(ii, clval_of_lvals tbl x, cfun_of_fun tbl f, cexpr_of_exprs tbl e)
     in
-    C.MkI(p,ir) :: tl 
-    
-and cstmt_of_stmt tbl c tl = 
+    C.MkI(p,ir) :: tl
+
+and cstmt_of_stmt tbl c tl =
   List.fold_right (cinstr_of_instr tbl) c tl
 
 let rec instr_of_cinstr tbl i =
@@ -355,74 +355,74 @@ let rec instr_of_cinstr tbl i =
     let (i_loc, i_info) = get_iinfo tbl p in
     let i_desc = instr_r_of_cinstr_r tbl ir in
     { i_desc; i_loc; i_info }
-      
+
 and instr_r_of_cinstr_r tbl = function
-  | C.Cassgn(x,t,e) -> 
-    Cassgn(lval_of_clval tbl x, at_of_cat t, expr_of_cexpr tbl e) 
+  | C.Cassgn(x,t,e) ->
+    Cassgn(lval_of_clval tbl x, at_of_cat t, expr_of_cexpr tbl e)
 
   | C.Copn(x,o,e) ->
-    Copn(lval_of_clvals tbl x, opn_of_copn o, expr_of_cexprs tbl e) 
+    Copn(lval_of_clvals tbl x, opn_of_copn o, expr_of_cexprs tbl e)
 
   | C.Cif(e,c1,c2) ->
     let c1 = stmt_of_cstmt tbl c1 in
     let c2 = stmt_of_cstmt tbl c2 in
-    Cif(expr_of_cexpr tbl e, c1, c2) 
+    Cif(expr_of_cexpr tbl e, c1, c2)
 
   | Cfor(x, ((d,e1),e2), c) ->
     let d = (rdir_of_crdir d, expr_of_cexpr tbl e1, expr_of_cexpr tbl e2) in
     let x = vari_of_cvari tbl x in
     let c = stmt_of_cstmt tbl c in
-    Cfor(x,d,c) 
+    Cfor(x,d,c)
 
   | Cwhile(e,c) ->
     Cwhile(expr_of_cexpr tbl e, stmt_of_cstmt tbl c)
 
 
   | Ccall(ii, x, f, e) ->
-    let ii = ii_of_cii ii in 
+    let ii = ii_of_cii ii in
     Ccall(ii, lval_of_clvals tbl x, fun_of_cfun tbl f, expr_of_cexprs tbl e)
 
-and stmt_of_cstmt tbl c = 
-  List.map (instr_of_cinstr tbl) c 
+and stmt_of_cstmt tbl c =
+  List.map (instr_of_cinstr tbl) c
 
 
 (* ------------------------------------------------------------------------ *)
 
-let set_finfo tbl loc cc = 
+let set_finfo tbl loc cc =
   let n = new_count tbl in
   Hashtbl.add tbl.finfo n (loc, cc);
   pos_of_int n
-  
-let get_finfo tbl n = 
+
+let get_finfo tbl n =
   try Hashtbl.find tbl.finfo (int_of_pos n)
   with Not_found -> assert false
 
-let cfdef_of_fdef tbl fd = 
+let cfdef_of_fdef tbl fd =
   let fn = cfun_of_fun tbl fd.f_name in
   let f_iinfo = set_finfo tbl fd.f_loc fd.f_cc in
-  let f_params = 
+  let f_params =
     List.map (fun x -> cvari_of_vari tbl (L.mk_loc L._dummy x)) fd.f_args in
   let f_body = cstmt_of_stmt tbl fd.f_body [] in
   let f_res = List.map (cvari_of_vari tbl) fd.f_ret in
-  fn, { C.f_iinfo = f_iinfo; 
-        C.f_params = f_params; 
-        C.f_body   = f_body; 
+  fn, { C.f_iinfo = f_iinfo;
+        C.f_params = f_params;
+        C.f_body   = f_body;
         C.f_res    = f_res }
-             
-  
-let fdef_of_cfdef tbl (fn, fd) = 
+
+
+let fdef_of_cfdef tbl (fn, fd) =
   let f_loc, f_cc = get_finfo tbl fd.C.f_iinfo in
   { f_loc;
     f_cc;
     f_name = fun_of_cfun tbl fn;
     f_args = List.map (fun v -> L.unloc (vari_of_cvari tbl v)) fd.C.f_params;
     f_body = stmt_of_cstmt tbl fd.C.f_body;
-    f_ret  = List.map (vari_of_cvari tbl) fd.C.f_res; 
+    f_ret  = List.map (vari_of_cvari tbl) fd.C.f_res;
   }
 
 let cprog_of_prog p =
   let tbl = empty_tbl () in
   tbl, List.map (cfdef_of_fdef tbl) p
 
-let prog_of_cprog tbl p = 
+let prog_of_cprog tbl p =
   List.map (fdef_of_cfdef tbl) p
--- compiler2/src/conv.mli
+++ compiler2/src/conv.mli
@@ -1,5 +1,5 @@
 open Prog
-type 'info coq_tbl 
+type 'info coq_tbl
 
 val cprog_of_prog : 'info prog -> 'info coq_tbl * Expr.prog
 
