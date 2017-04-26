7b7908e4d524495da33189ec078a7b64fce3ba8d
Vincent Laporte <Vincent.Laporte@gmail.com>
2017-04-26 10:13:07 +0000

    Register allocation: collect conflicting variables

--- compiler2/src/regalloc.ml
+++ compiler2/src/regalloc.ml
@@ -1,3 +1,4 @@
+open Utils
 open Prog
 
 let fill_in_missing_names (f: 'info func) : 'info func =
@@ -26,3 +27,55 @@ let fill_in_missing_names (f: 'info func) : 'info func =
   and fill_stmt s = List.map fill_instr s in
   let f_body = fill_stmt f.f_body in
   { f with f_body }
+
+module C =
+struct
+  type t = V.t * V.t
+
+  let norm ((u, v) as x : t) : t =
+    if V.compare u v < 0 then (v, u) else x
+
+  let compare x x' =
+    let (u, v) = norm x in
+    let (u', v') = norm x' in
+    match V.compare u u' with
+    | 0 -> V.compare v v'
+    | n -> n
+
+end
+
+module Sc = Set.Make (C)
+
+let conflicts_in (i: Sv.t) (k: C.t -> 'a -> 'a) : 'a -> 'a =
+  let e = Sv.elements i |> List.sort V.compare in
+  let rec loop a =
+    function
+    | [] -> a
+    | x :: xs ->
+      let rec inner a =
+        function
+        | [] -> a
+        | y :: ys -> inner (k (C.norm (x, y)) a) ys
+      in
+      loop (inner a xs) xs
+  in
+  fun a -> loop a e
+
+let collect_conflicts (f: (Sv.t * Sv.t) func) : Sc.t =
+  let add (c: Sc.t) ((i, _): (Sv.t * Sv.t)) : Sc.t =
+    conflicts_in i Sc.add c in
+  let rec collect_instr_r c =
+    function
+    | Cblock s
+    | Cfor (_, _, s)
+    | Cwhile (_, s)
+      -> collect_stmt c s
+    | Cassgn _
+    | Copn _
+    | Ccall _
+      -> c
+    | Cif (_, s1, s2)
+      -> collect_stmt (collect_stmt c s1) s2
+  and collect_instr c { i_desc ; i_info } = collect_instr_r (add c i_info) i_desc
+  and collect_stmt c s = List.fold_left collect_instr c s in
+  collect_stmt Sc.empty f.f_body
