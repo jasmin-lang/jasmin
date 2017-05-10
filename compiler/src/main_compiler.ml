e1e8a06391642c66d6a781355bcdf04359f7dd88
Vincent Laporte <Vincent.Laporte@gmail.com>
2017-05-10 08:10:59 +0000

    whitespace

--- compiler/src/main_compiler.ml
+++ compiler/src/main_compiler.ml
@@ -17,27 +17,27 @@ let set_coqonly s =
   coqonly := true
 
 let poptions = [
-    Compiler.Typing                      
-  ; Compiler.ParamsExpansion             
-  ; Compiler.Inlining                    
-  ; Compiler.RemoveUnusedFunction        
-  ; Compiler.Unrolling                   
-  ; Compiler.AllocInlineAssgn            
-  ; Compiler.DeadCode_AllocInlineAssgn   
-  ; Compiler.ShareStackVariable          
-  ; Compiler.DeadCode_ShareStackVariable 
-  ; Compiler.RegArrayExpansion           
-  ; Compiler.LowerInstruction            
-  ; Compiler.RegAllocation               
-  ; Compiler.DeadCode_RegAllocation      
-  ; Compiler.StackAllocation             
-  ; Compiler.Linearisation                 
-  ; Compiler.Assembly ]    
+    Compiler.Typing
+  ; Compiler.ParamsExpansion
+  ; Compiler.Inlining
+  ; Compiler.RemoveUnusedFunction
+  ; Compiler.Unrolling
+  ; Compiler.AllocInlineAssgn
+  ; Compiler.DeadCode_AllocInlineAssgn
+  ; Compiler.ShareStackVariable
+  ; Compiler.DeadCode_ShareStackVariable
+  ; Compiler.RegArrayExpansion
+  ; Compiler.LowerInstruction
+  ; Compiler.RegAllocation
+  ; Compiler.DeadCode_RegAllocation
+  ; Compiler.StackAllocation
+  ; Compiler.Linearisation
+  ; Compiler.Assembly ]
 
-let set_printing p () = 
+let set_printing p () =
   print_list := p :: !print_list
 
-let set_all_print () = 
+let set_all_print () =
   print_list := poptions
 
 let print_strings = function
@@ -58,7 +58,7 @@ let print_strings = function
   | Compiler.Linearisation               -> "linear"  , "linearisation"
   | Compiler.Assembly                    -> "asm"     , "generation of assembly"
 
-let print_option p = 
+let print_option p =
   let s, msg = print_strings p in
   ("-p"^s, Arg.Unit (set_printing p), "print program after "^msg)
 
@@ -69,8 +69,8 @@ let options = [
     "-coq"     , Arg.Set_string coqfile, "[filename]: generate the corresponding coq file";
     "-coqonly" , Arg.String set_coqonly, "[filename]: generate the corresponding coq file, and exit";
     "-pall"    , Arg.Unit set_all_print, "print program after each compilation steps"
-  ] @  List.map print_option poptions    
-              
+  ] @  List.map print_option poptions
+
 let usage_msg = "Usage : jasminc [option] filename"
 
 let parse () =
@@ -83,11 +83,11 @@ let parse () =
 
 (*--------------------------------------------------------------------- *)
 
-let pp_var_i tbl fmt vi = 
+let pp_var_i tbl fmt vi =
   let vi = Conv.vari_of_cvari tbl vi in
   Printer.pp_var ~debug:true fmt (Prog.L.unloc vi)
 
-let rec pp_comp_err tbl fmt = function 
+let rec pp_comp_err tbl fmt = function
   | Compiler_util.Cerr_varalloc(x,y,msg) ->
     Format.fprintf fmt "Variable allocation %a and %a: %s"
      (pp_var_i tbl) x (pp_var_i tbl) y (Conv.string_of_string0 msg)
@@ -144,34 +144,34 @@ let rec pp_comp_err tbl fmt = function
       (Conv.string_of_string0 s)
 
 and pp_comp_ferr tbl fmt = function
-  | Compiler_util.Ferr_in_body(f1,f2,(_ii, err_msg)) ->  
+  | Compiler_util.Ferr_in_body(f1,f2,(_ii, err_msg)) ->
     let f1 = Conv.fun_of_cfun tbl f1 in
     let f2 = Conv.fun_of_cfun tbl f2 in
       (* let (i_loc, _) = Conv.get_iinfo tbl ii in *)
     Format.fprintf fmt "in functions %s and %s at position s: %a"
       f1.fn_name f2.fn_name (* (Prog.L.tostring i_loc)  *)
-      (pp_comp_err tbl) err_msg 
+      (pp_comp_err tbl) err_msg
   | Compiler_util.Ferr_neqfun(f1,f2) ->
     let f1 = Conv.fun_of_cfun tbl f1 in
     let f2 = Conv.fun_of_cfun tbl f2 in
     Format.fprintf fmt "function %s and %s not equal"
       f1.fn_name f2.fn_name
-  | Compiler_util.Ferr_neqprog  -> 
+  | Compiler_util.Ferr_neqprog  ->
     Format.fprintf fmt "program not equal"
   | Compiler_util.Ferr_loop     ->
-    Format.fprintf fmt "loop iterator to small" 
+    Format.fprintf fmt "loop iterator to small"
   | Compiler_util.Ferr_uniqfun ->
-    Format.fprintf fmt "two function declarations with the same name" 
+    Format.fprintf fmt "two function declarations with the same name"
 
 (* -------------------------------------------------------------------- *)
 
-let eprint step pp_prog p = 
+let eprint step pp_prog p =
   if List.mem step !print_list then
     let (_, msg) = print_strings step in
-    Format.eprintf 
+    Format.eprintf
 "(* -------------------------------------------------------------------- *)@.";
     Format.eprintf "(* After %s *)@.@." msg;
-    Format.eprintf "%a@.@.@." pp_prog p               
+    Format.eprintf "%a@.@.@." pp_prog p
 
 (* -------------------------------------------------------------------- *)
 let main () =
@@ -213,20 +213,20 @@ let main () =
       let fd = trans fd in
       cfdef_of_fdef fd in
 
-    let stk_alloc_fd fn cfd = 
+    let stk_alloc_fd fn cfd =
       let fd = fdef_of_cfdef fn cfd in
       if !debug then Format.eprintf "START stack alloc@." ;
-    
+
       let alloc, sz, fd = Array_expand.stk_alloc_func fd in
-      let alloc = 
+      let alloc =
         let trans (v,i) = Conv.cvar_of_var tbl v, Conv.z_of_int i in
         List.map trans alloc in
       let sz = Conv.z_of_int sz in
       let cfd = cfdef_of_fdef fd in
-      let sfd = { 
+      let sfd = {
         Stack_alloc.sf_iinfo  = cfd.Expr.f_iinfo;
         Stack_alloc.sf_stk_sz = sz;
-        Stack_alloc.sf_stk_id = 
+        Stack_alloc.sf_stk_id =
           Var0.Var.vname (Conv.cvar_of_var tbl Prog.vstack);
         Stack_alloc.sf_params = cfd.Expr.f_params;
         Stack_alloc.sf_body   = cfd.Expr.f_body;
@@ -234,7 +234,7 @@ let main () =
       alloc, sfd
     in
 
-    let pp_cprog fmt cp = 
+    let pp_cprog fmt cp =
       let p = Conv.prog_of_cprog tbl cp in
       Printer.pp_prog ~debug:true fmt p in
 
@@ -245,16 +245,16 @@ let main () =
       Compiler.share_stk_fd = apply "share stk" Varalloc.alloc_stack_fd;
       Compiler.reg_alloc_fd = apply "reg alloc" Regalloc.regalloc;
       Compiler.stk_alloc_fd = stk_alloc_fd;
-      Compiler.print_prog   = (fun s p -> eprint s pp_cprog p; p); 
+      Compiler.print_prog   = (fun s p -> eprint s pp_cprog p; p);
     } in
 
-    let entries = 
+    let entries =
       let ep = List.filter (fun fd -> fd.f_cc = Export) prog in
       List.map (fun fd -> Conv.cfun_of_fun tbl fd.f_name) ep in
 
-    begin match 
+    begin match
       Compiler.compile_prog_to_x86 cparams entries cprog with
-    | Utils0.Error e -> 
+    | Utils0.Error e ->
       Utils.hierror "compilation error %a@.PLEASE REPORT"
          (pp_comp_ferr tbl) e
     | _ -> ()
