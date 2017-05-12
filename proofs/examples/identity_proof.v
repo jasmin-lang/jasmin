af5f621cd7b22955e4d121c26e00df6e015a6cb3
Vincent Laporte <Vincent.Laporte@gmail.com>
2017-05-12 09:59:17 +0000

    Logic example: overcome the missing rule for Ccall or inline

--- proofs/examples/identity_proof.v
+++ proofs/examples/identity_proof.v
@@ -11,6 +11,26 @@ Import Ssem.
 
 Goal
   ∀ v,
+    match get_fundef program identity with
+    | None => False
+    | Some fd =>
+      hoare program
+            (λ s, s.(sevm).[x] = v)
+            (f_body fd)
+            (λ s, s.(sevm).[r] = v)
+    end%vmap.
+
+Import prog_notation.
+
+Proof.
+  move=> v.
+  apply: hoare_by_wp.
+  post_wp.
+  subst. reflexivity.
+Qed.
+
+Goal
+  ∀ v,
   hoare program
     (λ _, True)
     [:: MkI xH (Ccall  DoNotInline [:: Lvar x] identity [:: Pcast (Pconst v)]) ]
