caf57e53beed230fb9e83fc200df8541f9aecc2c
Arthur Blot <arthur.blot78@gmail.com>
2017-05-29 16:20:17 +0200

    Remove logic from the distribution script

--- distrib.sh
+++ distrib.sh
@@ -2,8 +2,11 @@
 rm -Rf jasmin jasmin.tar.gz
 mkdir jasmin
 cp -R compiler proofs LICENSE README.md jasmin
+rm -r jasmin/proofs/logic
 sed -e "s/Inria$/<Anonymized>/" -i jasmin/proofs/*/*.v jasmin/compiler/src/*.ml
 sed -e "s/IMDEA Software Institute/<Anonymized>/" -i jasmin/proofs/*/*.v jasmin/compiler/src/*.ml
 sed -e "s,http://jasmin-lang.github.io/,http://example.com," -i jasmin/compiler/opam
 sed -e "s,https://github.com/jasmin-lang/jasmin/issues,http://example.com," -i jasmin/compiler/opam
+sed -e "/logic/d" -i jasmin/proofs/_CoqProject
+rm -f jasmin/proofs/Makefile.coq
 tar czf jasmin.tar.gz jasmin
