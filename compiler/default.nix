f11b6d06888be0951d9b3ec7cc8f91f5f055a94c
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-01-12 17:16:02 +0000

    compiler: adds a « dist » target
    Runnning « make dist » creates an archive with all OCaml sources
    of the Jasmin compiler: both hand-written code and code extracted
    from Coq files.

---
+++ compiler/default.nix
@@ -0,0 +1,9 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "jasmin-0";
+  src = ./.;
+  buildInputs = [ ]
+  ++ (with ocamlPackages; [ ocaml findlib ocamlbuild batteries menhir zarith ])
+  ;
+}
