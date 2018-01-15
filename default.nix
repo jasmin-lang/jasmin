43da1ad2c05c107347c9b65eae20d4b716d0a822
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-01-15 12:54:04 +0000

    Use Coq 8.7

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   src = ./.;
   buildInputs = [ ]
     ++ (with python3Packages; [ python pyyaml ])
-    ++ (with coqPackages_8_6; [ coq mathcomp ])
+    ++ (with coqPackages_8_7; [ coq mathcomp ])
     ++ (with ocamlPackages; [ ocaml findlib ocamlbuild batteries menhir zarith ])
     ;
 }
