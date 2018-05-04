bf347c466ac0255a9c6ef677461d7f75e63d22ea
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-05-04 06:32:47 +0000

    Update CoqWord

--- coqword.nix
+++ coqword.nix
@@ -2,7 +2,7 @@
 
 let inherit (coqPackages_8_7) coq mathcomp; in
 
-let rev = "834a3cf733f87d43a27376f7cd4d379e9abe7b03"; in
+let rev = "0c882f69087ce3d964434a77916f27686a5b6fe3"; in
 
 stdenv.mkDerivation rec {
   version = "0.0-git-${builtins.substring 0 8 rev}";
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
     owner = "jasmin-lang";
     repo = "coqword";
     inherit rev;
-    sha256 = "0wh596n0xw411hci6pp3fg1qlnd91r49afb7dm8s9fhil4qqnma5";
+    sha256 = "19gyc177yw98s5360s4fv0py4g9z798bd80ybxdkli0vxgh66k7c";
   };
 
   buildInputs = [ coq ];
