59d1d75b117aa6878e1a990b338f40b935743468
Vincent Laporte <Vincent.Laporte@gmail.com>
2018-07-04 14:02:43 +0000

    inline array copy: minor cleaning

--- compiler/src/inline_array_copy.ml
+++ compiler/src/inline_array_copy.ml
@@ -1,14 +1,15 @@
+open Utils
+open Prog
+
 module L = Location
-module E = Expr
-module P = Prog
 
 let is_array_copy x e =
   match x with
-  | P.Lvar z ->
+  | Lvar z ->
     begin match (L.unloc z).v_ty with
-    | P.Arr (ws, n) ->
+    | Arr (ws, n) ->
       begin match e with
-      | P.Pvar y -> Some (z, ws, n, y)
+      | Pvar y -> Some (z, ws, n, y)
       | _ -> None
       end
     | _ -> None
@@ -16,14 +17,14 @@ let is_array_copy x e =
   | _ -> None
 
 let array_copy z ws n y =
-  let i = L.mk_loc (L.loc z) (P.PV.mk "i" P.Inline (Bty Int) (L.loc z)) in
-  P.Cfor(i, (UpTo, Pconst P.B.zero, n), [
-      let i_desc = P.Cassgn (Laset (z, Pvar i), AT_none, P.Bty (P.U ws), Pget (y, Pvar i)) in
+  let i = L.mk_loc (L.loc z) (PV.mk "i" Inline (Bty Int) (L.loc z)) in
+  Cfor(i, (UpTo, Pconst B.zero, n), [
+      let i_desc = Cassgn (Laset (z, Pvar i), AT_none, Bty (U ws), Pget (y, Pvar i)) in
       { i_desc ; i_loc = L.loc z, [] ; i_info = () }
     ])
 
 let rec iac_pstmt is = List.map iac_pinstr is
-and iac_pinstr i = { i with P.i_desc = iac_pinstr_r i.P.i_desc }
+and iac_pinstr i = { i with i_desc = iac_pinstr_r i.i_desc }
 and iac_pinstr_r ir =
   match ir with
   | Cassgn (x, _, _, e) ->
@@ -37,11 +38,11 @@ and iac_pinstr_r ir =
   | (Copn _ | Ccall _) -> ir
 
 let iac_pfunc pf =
-  { pf with P.f_body = iac_pstmt pf.P.f_body }
+  { pf with f_body = iac_pstmt pf.f_body }
 
 let iac_pmod_item =
   function
-  | P.MIfun pf -> P.MIfun (iac_pfunc pf)
+  | MIfun pf -> MIfun (iac_pfunc pf)
   | pmi -> pmi
 
 let doit p = List.map iac_pmod_item p
--- compiler/src/inline_array_copy.mli
+++ compiler/src/inline_array_copy.mli
@@ -1,1 +1,3 @@
+(* Transforms array copies, of the form “x = y”,
+   into loops of the form “for i = 0 to n { x[i] = y[i]; }”. *)
 val doit : unit Prog.pprog -> unit Prog.pprog
