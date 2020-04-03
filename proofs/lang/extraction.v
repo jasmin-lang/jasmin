106ba031c286b0321e25503f54bfa04c81af785e
Vincent Laporte <Vincent.Laporte@inria.fr>
2020-04-03 18:52:33 +0200

    Extraction to OCaml: avoid 8-tuples of booleans

--- proofs/lang/extraction.v
+++ proofs/lang/extraction.v
@@ -31,6 +31,9 @@ Require ExtrOcamlString.
 Extraction Inline ssrbool.is_left.
 Extraction Inline ssrbool.predT ssrbool.pred_of_argType.
 
+Extract Constant strings.ascii_beq => "Char.equal".
+Extract Constant strings.ascii_cmp => "(fun x y -> let c = Char.compare x y in if c = 0 then Datatypes.Eq else if c < 0 then Datatypes.Lt else Datatypes.Gt)".
+
 Cd  "lang/ocaml".
 
 Extraction Blacklist String List Nat Utils Var Array.
