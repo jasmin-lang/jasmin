90665b56f443bc2d3f3f857fddd23ba95ace5530
Vincent Laporte <Vincent.Laporte@inria.fr>
2020-04-09 15:42:54 +0200

    default.nix: make it easy to use OCaml with frame pointers

--- default.nix
+++ default.nix
@@ -1,15 +1,29 @@
-with import <nixpkgs> {};
+{ pkgs ? import <nixpkgs> {}
+, enableFramePointers ? false
+}:
+
+with pkgs;
 
 let coqPackages = coqPackages_8_9; in
 
 let coqword = callPackage ./coqword.nix { inherit coqPackages; }; in
+
+let oP =
+  if enableFramePointers
+  then ocamlPackages.overrideScope' (self: super: {
+    ocaml = super.ocaml.overrideAttrs (o: {
+      configureFlags = o.configureFlags ++ [ "--enable-frame-pointers" ];
+    });
+  })
+  else ocamlPackages
+; in
 
 stdenv.mkDerivation {
   name = "jasmin-0";
   src = ./.;
   buildInputs = [ coqPackages.coq coqword ]
     ++ (with python3Packages; [ python pyyaml ])
-    ++ (with ocamlPackages; [ ocaml findlib ocamlbuild
+    ++ (with oP; [ ocaml findlib ocamlbuild
         (batteries.overrideAttrs (o: { doCheck = false; }))
          menhir merlin zarith mpfr camlidl apron ppl])
     ;
