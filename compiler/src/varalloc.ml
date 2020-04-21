4ee4636285879fd65a77eec96c78eb8e9f725f89
Vincent Laporte <Vincent.Laporte@inria.fr>
2020-04-21 16:08:04 +0200

    Varalloc: minor refactoring

--- compiler/src/varalloc.ml
+++ compiler/src/varalloc.ml
@@ -6,6 +6,22 @@ module G = IntervalGraphColoring
 
 type liverange = G.graph Mint.t
 
+(* --------------------------------------------------- *)
+let pp_var = Printer.pp_var ~debug: true
+
+let pp_range fmt (x, (lo, hi)) =
+  Format.fprintf fmt "%a: [%d; %d]" pp_var x lo hi
+
+let pp_ranges fmt (sz, g) =
+  Format.fprintf fmt "%d ↦ %a" sz (pp_list "@ " pp_range) (Mv.bindings g)
+
+let pp_color fmt (x, i) =
+  Format.fprintf fmt "%a: %a" pp_var x pp_var i
+
+let pp_coloring fmt (sz, c) =
+  Format.fprintf fmt "%d ↦ %a" sz (pp_list "@ " pp_color) (Mv.bindings c)
+
+(* --------------------------------------------------- *)
 let incr_liverange r x d : liverange =
   let s = size_of x.v_ty in
   let g = Mint.find_default Mv.empty s r in
@@ -40,7 +56,7 @@ let preprocess_liveset (s: Sv.t) : Sv.t * Sv.t =
           else Sv.add r s, Sv.add x all
         in
         if is_stk_ptr_kind x.v_kind
-        then let p = get_stack_pointer x in Sv.add p s, Sv.add x all
+        then Sv.add (get_stack_pointer x) s, Sv.add x all
         else s, all
       else
         if is_stack_kind x.v_kind
@@ -74,29 +90,17 @@ and live_ranges_instr (d, acc) { i_desc ; i_info = before, after } =
 and live_ranges_stmt d_acc s =
   List.fold_left live_ranges_instr d_acc s
 
-in 
-live_ranges_stmt d c, stack_pointers
+in live_ranges_stmt d c, stack_pointers
 
-let subst_of_coloring c s =
-  let r = Hashtbl.create 17 in
-  Mv.fold (fun x i s ->
-      match Hashtbl.find r i with
-      | exception Not_found -> Hashtbl.add r i x; s
-      | y -> Mv.add x y s
-    ) c s
-
-let subst_of_colorings (c: G.coloring Mint.t) : Subst.vsubst =
-  Mint.fold (fun _sz c s ->
-      subst_of_coloring c s
-    ) c Mv.empty
-
+(* --------------------------------------------------- *)
 let check_class ptr_classes args x s =
   let s = Sv.add x s in
   if not (Sv.disjoint args s) && not (Sv.mem x ptr_classes) then
     hierror "cannot put a reg ptr argument into the local stack"
 
+(* --------------------------------------------------- *)
 let alloc_stack_fd get_fun fd =
-  Format.eprintf "alloc_stack %s@." fd.f_name.fn_name;
+  if !Glob_options.debug then Format.eprintf "alloc_stack %s@." fd.f_name.fn_name;
   let result = fd in
   let alias =
     let get_cc fn =
@@ -111,30 +115,19 @@ let alloc_stack_fd get_fun fd =
   Mv.iter (check_class ptr_classes ptr_args) classes;
   let fd = Live.live_fd false fd in
   let (_, (ranges, all_stack_vars)), stack_pointers = live_ranges_stmt alias ptr_classes (0, (Mint.empty, Sv.empty)) fd.f_body in
-  let pp_range fmt (x, (lo, hi)) = Format.fprintf fmt "%a: [%d; %d]" (Printer.pp_var ~debug:true) x lo hi in
-  let pp_ranges fmt (sz, g) =
-    Format.fprintf fmt "%d ↦ %a" sz (pp_list "@ " pp_range) (Mv.bindings g);
-  in
   Format.eprintf "Ranges: %a@." (pp_list "@ " pp_ranges) (Mint.bindings ranges);
-  let pp_color fmt (x, i) =
-    Format.fprintf fmt "%a: %a" (Printer.pp_var ~debug:true) x (Printer.pp_var ~debug:true) i
-  in
-  let pp_coloring fmt (sz, c) =
-    Format.fprintf fmt "%d ↦ %a" sz (pp_list "@ " pp_color) (Mv.bindings c);
-  in
   let coloring = Mint.mapi G.solve ranges in
   Format.eprintf "Colors: %a@." (pp_list "@ " pp_coloring) (Mint.bindings coloring);
-  Format.eprintf "All stack vars: %a@." (pp_list "@ " (Printer.pp_var ~debug:true)) (Sv.elements all_stack_vars);
+  Format.eprintf "All stack vars: %a@." (pp_list "@ " pp_var) (Sv.elements all_stack_vars);
   Format.eprintf "alias: %a@." Alias.pp_alias alias;
 
-  let pp_var = Printer.pp_var ~debug: true in
   let slots = ref Sv.empty in
   let stk = ref Mv.empty in
   let add_slot slot = slots := Sv.add slot !slots in
   let add_stack x slot range = stk := Mv.add x (`Stack (slot,range)) !stk in
   let add_stack_ptr x slot = stk := Mv.add x (`StackPtr slot) !stk in
   let add_glob x slot range = stk := Mv.add x (`Glob (slot,range)) !stk in
-  let dovar v = 
+  let dovar v =
     match v.v_kind with
     | Stack Direct ->
       if is_ty_arr v.v_ty then
@@ -143,46 +136,40 @@ let alloc_stack_fd get_fun fd =
         else
           begin
             let sz = size_of c.in_var.v_ty in
-            let slot = 
-              try Mv.find c.in_var (Mint.find sz coloring) 
-              with Not_found -> 
+            let slot =
+              try Mv.find c.in_var (Mint.find sz coloring)
+              with Not_found ->
                 hierror "%a %a" pp_var v pp_var c.in_var in
             add_slot slot;
             add_stack v slot c.range
           end
       else
         let sz = size_of v.v_ty in
-        let slot = 
-          try Mv.find v (Mint.find sz coloring) 
+        let slot =
+          try Mv.find v (Mint.find sz coloring)
           with Not_found -> assert false in
         add_slot slot;
         add_stack v slot (0, sz)
 
     | Stack (Pointer _) ->
-      let xp = 
+      let xp =
         try Hashtbl.find stack_pointers v
         with Not_found -> assert false in
       let sz = size_of xp.v_ty in
-      let slot = 
-        try Mv.find xp (Mint.find sz coloring) 
+      let slot =
+        try Mv.find xp (Mint.find sz coloring)
         with Not_found -> assert false in
       add_slot slot;
-      add_stack_ptr v slot 
+      add_stack_ptr v slot
     | _ -> () in
   Sv.iter dovar all_stack_vars;
 
   Format.eprintf "slots = %a@." (pp_list "@ " pp_var) (Sv.elements !slots);
-  let pp_stk fmt (x, stkk) = 
+  let pp_stk fmt (x, stkk) =
     match stkk with
     | `Glob  (y, r) -> Format.fprintf fmt "%a -> %%G%a" pp_var x pp_range (y,r)
     | `Stack (y, r) -> Format.fprintf fmt "%a -> %a" pp_var x pp_range (y,r)
     | `StackPtr y -> Format.fprintf fmt "%a |-> %a" pp_var x pp_var y in
   Format.eprintf "Stk : @[<v>%a@]@." (pp_list "@ " pp_stk) (Mv.bindings !stk);
-    
-  let subst = subst_of_colorings coloring in
-  (*Subst.vsubst_func subst result *)
 
-  let pp_pair fmt (k, v) = Format.fprintf fmt "%a ↦ %a" pp_var k pp_var v in
-  Format.eprintf "Stack allocation: %a@." (pp_list ",@ " pp_pair) (Mv.bindings subst);
   result
-
