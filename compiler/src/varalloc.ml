bf477dbecd2eddfafd0946d7b15bc0d308996fab
Vincent Laporte <Vincent.Laporte@inria.fr>
2020-04-21 13:15:55 +0200

    WIP: big rewrite of varalloc

--- compiler/src/varalloc.ml
+++ compiler/src/varalloc.ml
@@ -1,185 +1,134 @@
 open Utils
 open Prog
-open Subst
-open Liveness
 
-(* ---------------------------------------------------------------------- *)
+module Live = Liveness
+module G = IntervalGraphColoring
 
-(* remove dependency of a stack array variable
-   before its first initialisation *)
+type liverange = G.graph Mint.t
 
-let init_lval init x =
-  match x with
-  | Lnone _        -> init
-  | Lvar x         -> Sv.add (L.unloc x) init
-  | Lmem _         -> init
-  | Laset(_, _, x, _) -> Sv.add (L.unloc x) init
-  | Lasub(_,_,_,x,_)  -> Sv.add (L.unloc x) init
+let incr_liverange r x d : liverange =
+  let s = size_of x.v_ty in
+  let g = Mint.find_default Mv.empty s r in
+  let i =
+    match Mv.find x g with
+    | (lo, hi) -> assert (hi <= d); (lo, d)
+    | exception Not_found -> (d, d)
+  in
+  let g = Mv.add x i g in
+  Mint.add s g r
 
-let init_lvals init xs = List.fold_left init_lval init xs
+let live_ranges_stmt (alias: Alias.alias) (ptr_classes: Sv.t) =
 
-let rec rm_uninitialized_i init i =
-  let (s1, s2) = i.i_info in
-  let init',i_desc =
-    match i.i_desc with
-    | Cassgn(x, _, _, _) ->
-      init_lval init x, i.i_desc
-    | Copn(xs, _, _, _) | Ccall(_, xs, _, _) ->
-      init_lvals init xs, i.i_desc
-    | Cif(e, c1, c2) ->
-      let init1, c1 = rm_uninitialized_c init c1 in
-      let init2, c2 = rm_uninitialized_c init c2 in
-      Sv.union init1 init2, Cif(e, c1, c2)
-    | Cfor(x, d, c) ->
-      let init', _ = rm_uninitialized_c init c in
-      let init'', c = rm_uninitialized_c init' c in
-      assert (Sv.equal init' init'');
-      init', Cfor(x, d, c)
-    | Cwhile(a, c1, e, c2) ->
-      let init1, _ = rm_uninitialized_c init  c1 in
-      let init2, _ = rm_uninitialized_c init1 c2 in
-      let init1', c1 = rm_uninitialized_c init2  c1 in
-      let init2', c2 = rm_uninitialized_c init1' c2 in
-      assert (Sv.equal init2 init2');
-      init2', Cwhile(a, c1, e, c2) in
-  init', {i with i_desc;
-          i_info = Sv.inter s1 init, Sv.inter s2 init' }
+let stack_pointers = Hashtbl.create 117 in
 
-and rm_uninitialized_c init c =
-  let init', r =
-    List.fold_left (fun (init,r) i ->
-      let init', i = rm_uninitialized_i init i in
-      init', i::r) (init, []) c in
-  init', List.rev r
+let get_stack_pointer x =
+  try Hashtbl.find stack_pointers x
+  with Not_found ->
+    let r = V.mk x.v_name (Stack Direct) u64 x.v_dloc in
+    Hashtbl.add stack_pointers x r;
+    r
+in
 
-let live_init_fd fd =
-  let fd = live_fd true fd in
-  let init = Sv.of_list fd.f_args in
-  let _, f_body = rm_uninitialized_c init fd.f_body in
-  { fd with f_body } 
+let preprocess_liveset (s: Sv.t) : Sv.t * Sv.t =
+  Sv.fold (fun x (s, all) ->
+      if is_ty_arr x.v_ty
+      then
+        let s, all =
+          let r = Alias.((normalize_var alias x).in_var) in
+          if Sv.mem r ptr_classes
+          then s, all
+          else Sv.add r s, Sv.add x all
+        in
+        if is_stk_ptr_kind x.v_kind
+        then let p = get_stack_pointer x in Sv.add p s, Sv.add p all
+        else s, all
+      else
+        if is_stack_kind x.v_kind
+        then Sv.add x s, Sv.add x all
+        else s, all
+    ) s (Sv.empty, Sv.empty)
+in
 
-type pointsto = Sv.t Mv.t
+let process_live_info d (acc, stack) (i: Sv.t) =
+  let process_live_var x acc =
+    incr_liverange acc x d
+  in
+  let i, all = preprocess_liveset i in
+  let acc = Sv.fold process_live_var i acc in
+  (acc, Sv.union stack all)
+in
 
-let expr_pointsto pt =
+let rec live_ranges_instr_r d_acc =
   function
-  | Pvar x | Psub (_,_,_,x,_) ->
-     let x = L.unloc x.gv in
-     if is_ptr x.v_kind
-     then Mv.find_default Sv.empty x pt
-     else Sv.singleton x
-  | Parr_init _ -> Sv.empty
-  | e -> hierror "expr_pointsto: %a" (Printer.pp_expr ~debug:true) e
-
-let pointsto_merge pt1 pt2 =
-  Mv.merge (fun _x o1 o2 ->
-      let s1 = odfl Sv.empty o1 in
-      let s2 = odfl Sv.empty o2 in
-      Some (Sv.union s1 s2)
-    ) pt1 pt2
-
-let merge_aliases_merge (cf1, pt1) (cf2, pt2) =
-  (var_classes_merge cf1 cf2, pointsto_merge pt1 pt2)
-
-(* p ≤ merge p q *)
-let pointsto_incl pt1 pt2 =
-  Mv.for_all (fun x s1 ->
-      let s2 = Mv.find_default Sv.empty x pt2 in
-      Sv.subset s1 s2
-    ) pt1
-
-let merge_aliases_incl (cf1, pt1) (cf2, pt2) =
-  var_classes_incl cf1 cf2 && pointsto_incl pt1 pt2
-
-let set_same_all i cf x s =
-  Sv.fold (fun y cf ->
-      try set_same cf x y
-      with SetSameConflict ->
-        Format.eprintf "Varalloc: cannot merge conflicting variables %a and %a@.%a %a@."
-           (Printer.pp_var ~debug: true) x
-           (Printer.pp_var ~debug: true) y
-           Printer.pp_iloc i.i_loc
-           (Printer.pp_instr ~debug:true) i;
-        cf
-    ) s cf
-
-let merge_aliases_assgn i ((cf, pt) as acc) lv e =
-  match lv with
-  | Lvar x when is_ptr (L.unloc x).v_kind ->
-     (cf, Mv.add (L.unloc x) (expr_pointsto pt e) pt)
-  | Lvar x when is_stack_array x && is_var e ->
-     (set_same_all i cf (L.unloc x) (expr_pointsto pt e), pt)
-  | _ -> acc
-
-let rec merge_aliases_instr get_fun acc i =
-  match i.i_desc with
-  | Cassgn (x, _, _, e) -> merge_aliases_assgn i acc x e
-  | Ccall (_, xs, fn, es) ->
-     let ra =
-       let f = get_fun fn in
-       match f.f_cc with
-       | Subroutine { returned_params } -> returned_params
-       | (Internal | Export) -> assert false
-     in
-     List.fold_left2 (fun acc oi x ->
-         match oi with
-         | None -> acc
-         | Some n -> merge_aliases_assgn i acc x (List.nth es n))
-       acc ra xs
-  | Copn _ -> acc
-  | Cfor _ -> assert false
-  | Cif (_, s1, s2) ->
-     let r1 = merge_aliases_stmt get_fun acc s1 in
-     let r2 = merge_aliases_stmt get_fun acc s2 in
-     merge_aliases_merge r1 r2
+  | (Cassgn _ | Copn _ | Ccall _) -> d_acc
+  | Cif (_, s1, s2)
   | Cwhile (_, s1, _, s2) ->
-     (* s1 ; while e do { s2 ; s1 } *)
-     let rec loop acc =
-       let r1 = merge_aliases_stmt get_fun acc s1 in
-       let r2 = merge_aliases_stmt get_fun r1 s2 in
-       if merge_aliases_incl r2 acc
-       then r1
-       else loop (merge_aliases_merge acc r2)
-     in loop acc
-  and merge_aliases_stmt get_fun acc s =
-    List.fold_left (merge_aliases_instr get_fun) acc s
+     let d_acc = live_ranges_stmt d_acc s1 in
+     let d_acc = live_ranges_stmt d_acc s2 in
+     d_acc
+  | Cfor _ -> assert false
+and live_ranges_instr (d, acc) { i_desc ; i_info = before, after } =
+  let acc = process_live_info d acc before in
+  let d, acc = live_ranges_instr_r (d + 1, acc) i_desc in
+  d + 1, process_live_info d acc after
+and live_ranges_stmt d_acc s =
+  List.fold_left live_ranges_instr d_acc s
 
-let merge_aliases get_fun (cf: Liveness.var_classes) (fd: _ Prog.func) : Liveness.var_classes =
-  let pt : pointsto = Mv.empty in
-  merge_aliases_stmt get_fun (cf, pt) fd.f_body
-  |> fst
+in live_ranges_stmt
+
+let subst_of_coloring c s =
+  let r = Hashtbl.create 17 in
+  Mv.fold (fun x i s ->
+      match Hashtbl.find r i with
+      | exception Not_found -> Hashtbl.add r i x; s
+      | y -> Mv.add x y s
+    ) c s
+
+let subst_of_colorings (c: G.coloring Mint.t) : Subst.vsubst =
+  Mint.fold (fun _sz c s ->
+      subst_of_coloring c s
+    ) c Mv.empty
+
+let check_class ptr_classes args x s =
+  let s = Sv.add x s in
+  if not (Sv.disjoint args s)
+  then
+    if not (Sv.mem x ptr_classes)
+    then hierror "cannot put a reg ptr argument into the local stack"
 
 let alloc_stack_fd get_fun fd =
-  (* collect all stack variables occuring in fd *)
-  let vars = Sv.filter is_stack_var (vars_fc fd) in
-  let vars = Sv.elements vars in
-  (* liveness analysis *)
-  let fd' = live_init_fd fd in
-  let cf = conflicts fd' in
-
-(*  Format.eprintf "liveness done@."; *)
-(*  let pp_info fmt (c1, c2) =
-    let pp_set fmt c = 
-      Format.fprintf fmt "{%a}" 
-        (Printer.pp_list ", " (Printer.pp_var ~debug:true)) (Sv.elements c) in
-    Format.fprintf fmt "%a%a" pp_set c1 pp_set c2 in
-  Format.eprintf "%a" (Printer.pp_ifunc ~debug:true pp_info) fd';
-  let pp_var =  Printer.pp_var ~debug:true in
-  Mv.iter (fun x s ->
-      Format.eprintf "%a -> %a@."
-        pp_var x (Printer.pp_list ", " pp_var) (Sv.elements s)) cf;
-  Format.eprintf "dependency done@."; *)
-
-  (* allocated variables *)
-  let cfm = init_classes cf in
-  let cfm = merge_aliases get_fun cfm fd in
-  let cfm = ref cfm in
-  let alloc x =
-    let cx = get_conflict !cfm x in
-    let test y = x.v_ty = y.v_ty && x.v_kind = y.v_kind && not (Sv.mem y cx) && 
-                   try ignore(set_same !cfm x y); true
-                   with SetSameConflict -> false in
-    let x' = List.find test vars in
-    cfm := set_same !cfm x x' in
-  List.iter alloc vars;
-  vsubst_func (normalize_repr !cfm) fd
+  let result = fd in
+  let alias =
+    let get_cc fn =
+      match (get_fun fn).f_cc with
+      | Subroutine si -> si
+      | (Export | Internal) -> assert false
+    in
+    Alias.analyze_fd get_cc fd in
+  let classes = Alias.classes alias in
+  let ptr_args = List.fold_left (fun s x -> if is_reg_ptr_kind x.v_kind then Sv.add x s else s) Sv.empty fd.f_args in
+  let ptr_classes = Mv.fold (fun x s acc -> if Sv.for_all (fun y -> is_ptr y.v_kind) (Sv.add x s) then Sv.add x acc else acc) classes Sv.empty in
+  Mv.iter (check_class ptr_classes ptr_args) classes;
+  let fd = Live.live_fd false fd in
+  let _, (ranges, all_stack_vars) = live_ranges_stmt alias ptr_classes (0, (Mint.empty, Sv.empty)) fd.f_body in
+  let pp_range fmt (x, (lo, hi)) = Format.fprintf fmt "%a: [%d; %d]" (Printer.pp_var ~debug:true) x lo hi in
+  let pp_ranges fmt (sz, g) =
+    Format.fprintf fmt "%d ↦ %a" sz (pp_list "@ " pp_range) (Mv.bindings g);
+  in
+  Format.eprintf "Ranges: %a@." (pp_list "@ " pp_ranges) (Mint.bindings ranges);
+  let pp_color fmt (x, i) =
+    Format.fprintf fmt "%a: %d" (Printer.pp_var ~debug:true) x i
+  in
+  let pp_coloring fmt (sz, c) =
+    Format.fprintf fmt "%d ↦ %a" sz (pp_list "@ " pp_color) (Mv.bindings c);
+  in
+  let coloring = Mint.map G.solve ranges in
+  Format.eprintf "Colors: %a@." (pp_list "@ " pp_coloring) (Mint.bindings coloring);
+  Format.eprintf "All stack vars: %a@." (pp_list "@ " (Printer.pp_var ~debug:true)) (Sv.elements all_stack_vars);
+  let subst = subst_of_colorings coloring in
+  (*Subst.vsubst_func subst result *)
+  let pp_var = Printer.pp_var ~debug: true in
+  let pp_pair fmt (k, v) = Format.fprintf fmt "%a ↦ %a" pp_var k pp_var v in
+  Format.eprintf "Stack allocation: %a@." (pp_list ",@ " pp_pair) (Mv.bindings subst);
+  result
 
