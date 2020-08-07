77791ee24cb4dd931e267c71cb0a0797aa448691
swarnpriya <swarn.099016@gmail.com>
2020-08-07 11:35:48 -0400

    wip

--- proofs/compiler/unrolling_proof.v
+++ proofs/compiler/unrolling_proof.v
@@ -39,83 +39,99 @@ Local Open Scope seq_scope.
 
 Section PROOF.
 
-  Variable p : prog.
+  Variable p p': prog.
+  Variable Fs: seq (funname * seq leak_i_tr).
   Notation gd := (p_globs p).
 
-  Let p' := unroll_prog p.
+  Hypothesis (p'_def : p' = (unroll_prog p).1).
 
-  Let Pi_r (s:estate) (i:instr_r) (s':estate) :=
-    forall ii, sem p' s (unroll_i (MkI ii i)) s'.
+  Hypothesis (Fs_def : Fs = (unroll_prog p).2).
+  
+  Let Pi_r (s:estate) (i:instr_r) (li: leak_i) (s':estate) :=
+    forall ii, sem p' s (unroll_i (MkI ii i)).1 (leak_I (leak_Fun Fs) li (unroll_i (MkI ii i)).2) s'.
 
-  Let Pi (s:estate) (i:instr) (s':estate) :=
-    sem p' s (unroll_i i) s'.
+  Let Pi (s:estate) (i:instr) (li: leak_i) (s':estate) :=
+    sem p' s (unroll_i i).1 (leak_I (leak_Fun Fs) li (unroll_i i).2)  s'.
 
-  Let Pc (s:estate) (c:cmd) (s':estate) :=
-    sem p' s (unroll_cmd unroll_i c) s'.
+  Let Pc (s:estate) (c:cmd) (lc: leak_c) (s':estate) :=
+    sem p' s (unroll_cmd unroll_i c).1 (leak_Is (leak_I (leak_Fun Fs)) (unroll_cmd unroll_i c).2 lc) s'.
 
-  Let Pfor (i:var_i) vs s c s' :=
-    sem_for p' i vs s (unroll_cmd unroll_i c) s'
+  Let Pfor (i:var_i) vs s c lf s' :=
+    sem_for p' i vs s (unroll_cmd unroll_i c).1 (leak_Iss (leak_I (leak_Fun Fs))  (unroll_cmd unroll_i c).2 lf) s'
     /\ forall ii, sem p' s
-      (flatten (map (fun n => assgn ii i (Pconst n) :: (unroll_cmd unroll_i c)) vs)) s'.
+                      (flatten (map (fun n => assgn ii i (Pconst n) :: (unroll_cmd unroll_i c).1) vs))
+                      (flatten (leak_Iss (leak_I (leak_Fun Fs))  (unroll_cmd unroll_i c).2 lf)) s'.
 
-  Let Pfun m1 fn vargs m2 vres :=
-    sem_call p' m1 fn vargs m2 vres.
+  Let Pfun m1 fn vargs lf m2 vres :=
+    sem_call p' m1 fn vargs (lf.1, (leak_Is (leak_I (leak_Fun Fs)) (leak_Fun Fs lf.1) lf.2)) m2 vres.
 
   Local Lemma Hskip : sem_Ind_nil Pc.
   Proof. exact: Eskip. Qed.
 
   Local Lemma Hcons : sem_Ind_cons p Pc Pi.
   Proof.
-    move=> s1 s2 s3 i c Hsi Hi Hsc Hc.
+    move=> s1 s2 s3 i c li lc Hsi Hi Hsc Hc.
     exact: (sem_app Hi Hc).
   Qed.
 
   Local Lemma HmkI : sem_Ind_mkI p Pi_r Pi.
-  Proof. move=> ii i s1 s2 _ Hi; exact: Hi. Qed.
+  Proof. move=> ii i s1 s2 li _ Hi ; exact: Hi. Qed.
 
   Local Lemma Hassgn : sem_Ind_assgn p Pi_r.
   Proof.
-    move=> s1 s2 x tag ty e v v' hv hv' hw ii.
-    by apply: sem_seq1; apply: EmkI; apply: Eassgn; eauto.
+    move=> s1 s2 x tag ty e v v' le lw hv  hv' hw ii /=.
+    apply: sem_seq1; apply: EmkI; apply: Eassgn; eauto.
+    replace (p_globs p') with gd. auto. by rewrite p'_def.
+    replace (p_globs p') with gd. auto. by rewrite p'_def.
   Qed.
 
   Local Lemma Hopn : sem_Ind_opn p Pi_r.
   Proof.
-    move=> s1 s2 t o xs es Hw ii.
-    by apply: sem_seq1; apply: EmkI; apply: Eopn.
+    move=> s1 s2 t o xs es lo Hw ii.
+    apply: sem_seq1; apply: EmkI; apply: Eopn.
+    replace (p_globs p') with gd. auto. by rewrite p'_def.
   Qed.
 
   Local Lemma Hif_true : sem_Ind_if_true p Pc Pi_r.
   Proof.
-    move=> s1 s2 e c1 c2 Hb Hsc Hc ii.
-    by apply: sem_seq1; apply: EmkI; apply: Eif_true.
+    move=> s1 s2 e c1 c2 le lc Hb Hsc Hc ii /=.
+    apply: sem_seq1; apply: EmkI; apply: Eif_true.
+    replace (p_globs p') with gd. auto. by rewrite p'_def.
+    rewrite /Pc in Hc. auto.
   Qed.
 
   Local Lemma Hif_false : sem_Ind_if_false p Pc Pi_r.
   Proof.
-    move=> s1 s2 e c1 c2 Hb Hsc Hc ii.
-    by apply: sem_seq1; apply: EmkI; apply: Eif_false.
+    move=> s1 s2 e c1 c2 le lc Hb Hsc Hc ii /=.
+    apply: sem_seq1; apply: EmkI; apply: Eif_false.
+    replace (p_globs p') with gd. auto. by rewrite p'_def.
+    rewrite /Pc in Hc. auto.
   Qed.
 
   Local Lemma Hwhile_true : sem_Ind_while_true p Pc Pi_r.
   Proof.
-    move=> s1 s2 s3 s4 a c e c' Hsc Hc Hb Hsc' Hc' Hsi Hi ii.
+    move=> s1 s2 s3 s4 a c e c' lc le lc' li Hsc Hc Hb Hsc' Hc' Hsi Hi ii /=.
     apply: sem_seq1; apply: EmkI; apply: Ewhile_true=> //; eauto=> /=.
-    by move: Hi=> /(_ ii) /semE [?] [/sem_IE Hi /semE ->].
+    replace (p_globs p') with gd. auto. by rewrite p'_def.
+    rewrite /Pi_r in Hi. rewrite /Pc in Hc'.
+    move: Hi. move=> /(_ ii) /= /semE [] s [] li' [] lc'' [] /= /sem_IE Hi /semE [] -> -> Hl /=.
+    by rewrite Hl /=.
   Qed.
 
   Local Lemma Hwhile_false : sem_Ind_while_false p Pc Pi_r.
   Proof.
-   move=> s1 s2 a c e c' Hsc Hc Hb ii.
-   by apply: sem_seq1; apply: EmkI; apply: Ewhile_false.
+   move=> s1 s2 a c e c' lc le Hsc Hc Hb ii /=.
+   apply: sem_seq1; apply: EmkI; apply: Ewhile_false.
+   by rewrite /Pc in Hc. replace (p_globs p') with gd. auto. by rewrite p'_def.
   Qed.
 
   Local Lemma Hfor : sem_Ind_for p Pi_r Pfor.
   Proof.
-    move=> s1 s2 i d lo hi c vlo vhi Hlo Hhi Hc [Hfor Hfor'] ii /=.
+    move=> s1 s2 i r wr c lr lf Hwr Hc [Hfor Hfor'] ii /=.
+    case: (r). move=> [d lo] hi.
     case Hlo': (is_const lo)=> [nlo|].
     + case Hhi': (is_const hi)=> [nhi|].
-      + have ->: nlo = vlo.
+      (*+ have ->: nlo = lo.
           rewrite /is_const /= in Hlo'.
           by case: lo Hlo Hlo'=> //= z [] -> [] ->.
         have ->: nhi = vhi.
@@ -123,8 +139,8 @@ Section PROOF.
           by case: hi Hhi Hhi'=> //= z [] -> [] ->.
         exact: Hfor'.
       apply: sem_seq1; apply: EmkI; apply: Efor; [apply: Hlo|apply: Hhi|apply: Hfor].
-    apply: sem_seq1; apply: EmkI; apply: Efor; [apply: Hlo|apply: Hhi|apply: Hfor].
-  Qed.
+    apply: sem_seq1; apply: EmkI; apply: Efor; [apply: Hlo|apply: Hhi|apply: Hfor].*)
+  Admitted.
 
   Local Lemma Hfor_nil : sem_Ind_for_nil Pfor.
   Proof.
@@ -142,38 +158,48 @@ Section PROOF.
 
   Local Lemma Hfor_cons : sem_Ind_for_cons p Pc Pfor.
   Proof.
-    move=> s1 s1' s2 s3 i w ws c Hw Hsc Hc Hsfor [Hfor Hfor']; split=> [|ii].
+    move=> s1 s1' s2 s3 i w ws c lc lf Hw Hsc Hc Hsfor [Hfor Hfor']; split=> [|ii].
     apply: EForOne; [exact: Hw|exact: Hc|exact: Hfor].
-    move: Hfor'=> /(_ ii) Hfor'.
-    apply: Eseq.
+    move: Hfor'=> /(_ ii) Hfor'. rewrite /assgn /=. 
+    (*apply: Eseq.
     + apply: EmkI; apply: Eassgn. reflexivity. by rewrite (write_var_Z Hw). exact Hw.
     apply: sem_app.
     exact: Hc.
     exact: Hfor'.
-  Qed.
+  Qed.*)
+    Admitted. 
 
   Local Lemma Hcall : sem_Ind_call p Pi_r Pfun.
   Proof.
-    move=> s1 m2 s2 ii xs fn args vargs vs Hexpr Hcall Hfun Hw ii'.
-    apply: sem_seq1; apply: EmkI; apply: Ecall; [exact: Hexpr|exact: Hfun|exact: Hw].
+    move=> s1 m2 s2 ii xs fn args vargs vs lf lw Hexpr Hcall Hfun Hw ii' /=.
+    case hlf: (lf) => [fn' fd']. apply: sem_seq1; apply: EmkI; apply: Ecall.
+    replace (p_globs p') with gd. auto. by rewrite p'_def. auto.
+    rewrite /Pfun in Hfun. rewrite /= in Hfun. rewrite hlf in Hfun. apply Hfun.
+    replace (p_globs p') with gd. apply Hw. by rewrite p'_def.
   Qed.
 
   Local Lemma Hproc : sem_Ind_proc p Pc Pfun.
   Proof.
-    move => m1 m2 fn f vargs vargs' s1 vm2 vres vres'.
+    move => m1 m2 fn f vargs vargs' s1 vm2 vres vres' lc /=.
     case: f=> fi ftyi fparams fc ftyo fres /= Hget Htyi Hw _ Hc Hres Htyo.
-    apply: EcallRun.
-    + by rewrite get_map_prog Hget.
-    + exact: Htyi.
-    + exact: Hw.
-    + exact: Hc.
-    + exact: Hres.
-    exact: Htyo.
-  Qed.
+    apply EcallRun with {|
+           f_iinfo := fi;
+           f_tyin := ftyi;
+           f_params := fparams;
+           f_body := fc;
+           f_tyout := ftyo;
+           f_res := fres |} vargs s1 vm2 vres. subst.
+    + (*rewrite get_map_prog Hget.*) admit.
+    + apply Htyi.
+    + apply Hw.
+    + rewrite /Pc /= in Hc. rewrite /= in Hc. admit.
+    + apply Hres.
+    apply Htyo.
+   Admitted.
 
-  Lemma unroll_callP f mem mem' va vr:
-    sem_call p  mem f va mem' vr ->
-    sem_call p' mem f va mem' vr.
+  Lemma unroll_callP f mem mem' va vr lf:
+    sem_call p  mem f va lf mem' vr ->
+    sem_call p' mem f va (lf.1, (leak_Is (leak_I (leak_Fun Fs)) (leak_Fun Fs lf.1) lf.2)) mem' vr.
   Proof.
     apply (@sem_call_Ind p Pc Pi_r Pi Pfor Pfun Hskip Hcons HmkI Hassgn Hopn
              Hif_true Hif_false Hwhile_true Hwhile_false Hfor Hfor_nil Hfor_cons Hcall Hproc).
