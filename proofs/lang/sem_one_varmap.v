cd934a693e72943f8599224137101f29e3efcbc5
Vincent Laporte <Vincent.Laporte@inria.fr>
2020-08-18 13:13:24 +0200

    sem-one-varmap: Induction principle

--- proofs/lang/sem_one_varmap.v
+++ proofs/lang/sem_one_varmap.v
@@ -181,4 +181,138 @@ Proof.
   by exists f, m1, s2'.
 Qed.
 
+(*---------------------------------------------------*)
+(* Induction principle *)
+Section SEM_IND.
+  Variables
+    (Pc   : estate → cmd → estate → Prop)
+    (Pi : estate → instr → estate → Prop)
+    (Pi_r : instr_info → estate → instr_r → estate → Prop)
+    (Pfun : instr_info → estate → funname → estate → Prop).
+
+  Definition sem_Ind_nil : Prop :=
+    ∀ s : estate, Pc s [::] s.
+
+  Definition sem_Ind_cons : Prop :=
+    ∀ (s1 s2 s3 : estate) (i : instr) (c : cmd),
+      sem_I s1 i s2 → Pi s1 i s2 → sem s2 c s3 → Pc s2 c s3 → Pc s1 (i :: c) s3.
+
+  Hypotheses
+    (Hnil: sem_Ind_nil)
+    (Hcons: sem_Ind_cons)
+  .
+
+  Definition sem_Ind_mkI : Prop :=
+    ∀ (ii : instr_info) (i : instr_r) (s1 s2 : estate),
+      sem_i ii (kill_extra_register ii s1) i s2 → Pi_r ii (kill_extra_register ii s1) i s2 → Pi s1 (MkI ii i) s2.
+
+  Hypothesis HmkI : sem_Ind_mkI.
+
+  Definition sem_Ind_assgn : Prop :=
+    ∀ (ii: instr_info) (s1 s2 : estate) (x : lval) (tag : assgn_tag) ty (e : pexpr) v v',
+      sem_pexpr gd s1 e = ok v →
+      truncate_val ty v = ok v' →
+      write_lval gd x v' s1 = ok s2 →
+      Pi_r ii s1 (Cassgn x tag ty e) s2.
+
+  Definition sem_Ind_opn : Prop :=
+    ∀ (ii: instr_info) (s1 s2 : estate) t (o : sopn) (xs : lvals) (es : pexprs),
+      sem_sopn gd o s1 xs es = ok s2 →
+      Pi_r ii s1 (Copn xs t o es) s2.
+
+  Definition sem_Ind_if_true : Prop :=
+    ∀ (ii: instr_info) (s1 s2 : estate) (e : pexpr) (c1 c2 : cmd),
+    sem_pexpr gd s1 e = ok (Vbool true) →
+    sem s1 c1 s2 → Pc s1 c1 s2 → Pi_r ii s1 (Cif e c1 c2) s2.
+
+  Definition sem_Ind_if_false : Prop :=
+    ∀ (ii: instr_info) (s1 s2 : estate) (e : pexpr) (c1 c2 : cmd),
+    sem_pexpr gd s1 e = ok (Vbool false) →
+    sem s1 c2 s2 → Pc s1 c2 s2 → Pi_r ii s1 (Cif e c1 c2) s2.
+
+  Definition sem_Ind_while_true : Prop :=
+    ∀ (ii: instr_info) (s1 s2 s3 s4 : estate) a (c : cmd) (e : pexpr) (c' : cmd),
+    sem s1 c s2 → Pc s1 c s2 →
+    sem_pexpr gd s2 e = ok (Vbool true) →
+    sem s2 c' s3 → Pc s2 c' s3 →
+    sem_i ii s3 (Cwhile a c e c') s4 → Pi_r ii s3 (Cwhile a c e c') s4 → Pi_r ii s1 (Cwhile a c e c') s4.
+
+  Definition sem_Ind_while_false : Prop :=
+    ∀ (ii: instr_info) (s1 s2 : estate) a (c : cmd) (e : pexpr) (c' : cmd),
+    sem s1 c s2 -> Pc s1 c s2 →
+    sem_pexpr gd s2 e = ok (Vbool false) →
+    Pi_r ii s1 (Cwhile a c e c') s2.
+
+  Hypotheses
+    (Hasgn: sem_Ind_assgn)
+    (Hopn: sem_Ind_opn)
+    (Hif_true: sem_Ind_if_true)
+    (Hif_false: sem_Ind_if_false)
+    (Hwhile_true: sem_Ind_while_true)
+    (Hwhile_false: sem_Ind_while_false)
+  .
+
+  Definition sem_Ind_call : Prop :=
+    ∀ (ii: instr_info) (s1 s2: estate) ini res fn args xargs xres,
+      mapM get_pvar args = ok xargs →
+      mapM get_lvar res = ok xres →
+      sem_call ii s1 fn s2 →
+      Pfun ii s1 fn s2 →
+      Pi_r ii s1 (Ccall ini res fn args) s2.
+
+  Definition sem_Ind_proc : Prop :=
+    ∀ (ii: instr_info) (s1 s2: estate) (fn: funname) fd m1 s2',
+      get_fundef (p_funcs p) fn = Some fd →
+      (if fd.(f_extra).(sf_return_address) is RAstack _ then extra_free_registers ii != None else true) →
+      alloc_stack s1.(emem) fd.(f_extra).(sf_align) fd.(f_extra).(sf_stk_sz) = ok m1 →
+      sem {| emem := m1 ; evm := set_RSP m1 (if fd.(f_extra).(sf_return_address) is RAreg x then s1.(evm).[x <- undef_error] else s1.(evm)) |} fd.(f_body) s2' →
+      Pc {| emem := m1 ; evm := set_RSP m1 (if fd.(f_extra).(sf_return_address) is RAreg x then s1.(evm).[x <- undef_error] else s1.(evm)) |} fd.(f_body) s2' →
+      let m2 := free_stack s2'.(emem) fd.(f_extra).(sf_stk_sz) in
+      s2 = {| emem := m2 ; evm := set_RSP m2 s2'.(evm) |}  →
+      Pfun ii s1 fn s2.
+
+  Hypotheses
+    (Hcall: sem_Ind_call)
+    (Hproc: sem_Ind_proc)
+  .
+
+  Fixpoint sem_Ind (s1 : estate) (c : cmd) (s2 : estate) (s: sem s1 c s2) {struct s} :
+    Pc s1 c s2 :=
+    match s in sem s1 c s2 return Pc s1 c s2 with
+    | Eskip s0 => Hnil s0
+    | @Eseq s1 s2 s3 i c s0 s4 =>
+        @Hcons s1 s2 s3 i c s0 (@sem_I_Ind s1 i s2 s0) s4 (@sem_Ind s2 c s3 s4)
+    end
+
+  with sem_i_Ind (ii: instr_info) (e : estate) (i : instr_r) (e0 : estate) (s : sem_i ii e i e0) {struct s} :
+    Pi_r ii e i e0 :=
+    match s in sem_i ii s1 i s2 return Pi_r ii s1 i s2 with
+    | @Eassgn ii s1 s2 x tag ty e1 v v' h1 h2 h3 => @Hasgn ii s1 s2 x tag ty e1 v v' h1 h2 h3
+    | @Eopn ii s1 s2 t o xs es e1 => @Hopn ii s1 s2 t o xs es e1
+    | @Eif_true ii s1 s2 e1 c1 c2 e2 s0 =>
+      @Hif_true ii s1 s2 e1 c1 c2 e2 s0 (@sem_Ind s1 c1 s2 s0)
+    | @Eif_false ii s1 s2 e1 c1 c2 e2 s0 =>
+      @Hif_false ii s1 s2 e1 c1 c2 e2 s0 (@sem_Ind s1 c2 s2 s0)
+    | @Ewhile_true ii s1 s2 s3 s4 a c e1 c' s0 e2 s5 s6 =>
+      @Hwhile_true ii s1 s2 s3 s4 a c e1 c' s0 (@sem_Ind s1 c s2 s0) e2 s5 (@sem_Ind s2 c' s3 s5) s6
+          (@sem_i_Ind ii s3 (Cwhile a c e1 c') s4 s6)
+    | @Ewhile_false ii s1 s2 a c e1 c' s0 e2 =>
+      @Hwhile_false ii s1 s2 a c e1 c' s0 (@sem_Ind s1 c s2 s0) e2
+    | @Ecall ii s1 s2 ini res fn args xargs xres hargs hres exec =>
+      @Hcall ii s1 s2 ini res fn args xargs xres hargs hres exec (@sem_call_Ind ii s1 fn s2 exec)
+    end
+
+  with sem_I_Ind (s1 : estate) (i : instr) (s2 : estate) (s : sem_I s1 i s2) {struct s} : Pi s1 i s2 :=
+    match s in sem_I e1 i0 e2 return Pi e1 i0 e2 with
+    | @EmkI ii i s1 s2 exec => @HmkI ii i s1 s2 exec (@sem_i_Ind ii _ i s2 exec)
+    end
+
+  with sem_call_Ind (ii: instr_info) (s1: estate) (fn: funname) (s2: estate) (s: sem_call ii s1 fn s2) {struct s} : Pfun ii s1 fn s2 :=
+    match s with
+    | @EcallRun ii s1 s2 fn fd m1 s2' ok_fd ok_ra ok_m1 exec ok_s2 =>
+      @Hproc ii s1 s2 fn fd m1 s2' ok_fd ok_ra ok_m1 exec (@sem_Ind _ _ _ exec) ok_s2
+    end.
+
+End SEM_IND.
+
 End SEM.
