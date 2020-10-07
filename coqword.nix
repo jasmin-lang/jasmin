1fec609f0d0256d7238336e821fc42d1191f6ecb
Vincent Laporte <Vincent.Laporte@inria.fr>
2020-10-07 14:35:22 +0200

    use Coq 8.12 and mathcomp 1.11

--- coqword.nix
+++ coqword.nix
@@ -4,12 +4,12 @@ let inherit (coqPackages) coq; in
 
 let mathcomp =
  (if coqPackages ? mathcomp_
-  then coqPackages.mathcomp_ "1.10.0"
-  else coqPackages.mathcomp.override { version = "1.10.0"; }
+  then coqPackages.mathcomp_ "1.11.0"
+  else coqPackages.mathcomp.override { version = "1.11.0"; }
  ).algebra
 ; in
 
-let rev = "4b83cbe911364a39b4f87ea2451b5a6119cbca06"; in
+let rev = "7c650450e03310ca67bdccb64c95be82116945c7"; in
 
 stdenv.mkDerivation rec {
   version = "0.0-git-${builtins.substring 0 8 rev}";
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
     owner = "jasmin-lang";
     repo = "coqword";
     inherit rev;
-    sha256 = "0235qgjzdjjci1b8h85y16g8akrl0mgdm799lnhf4givx0d90hzv";
+    sha256 = "1b4p67599s6cqbs3r1pd736dq5zivvi3w8wbl4dhcg6mnzbgvkyg";
   };
 
   buildInputs = [ coq ];
