56a92ca5811d02871e88f83b07d0b5d079aafb87
Antoine Séré <antoine.sere@protonmail.com>
2020-11-03 15:30:48 +0100

    Prettified unionfind, completed some proofs

--- proofs/compiler/tunneling.v
+++ proofs/compiler/tunneling.v
@@ -12,31 +12,10 @@ Unset Printing Implicit Defensive.
 
 Local Open Scope seq_scope.
 
-Section WEIRDUNIONFIND.
+Section UnionFind.
 
-  Definition WeirdUnionFind := seq (label * label).
-
-  Definition EmptyWeirdUnionFind : WeirdUnionFind := [::].
-
-  Definition MakeSet (l : label) (uf : WeirdUnionFind) :=
-    if has (fun pl => pl.1 == l) uf
-    then uf
-    else (l,l) :: uf.
-  
-  Definition Find (l : label) (uf : WeirdUnionFind) :=
-    (nth (l,l) uf (find (fun x => x.1 == l) uf)).2.
-
-  Definition Union (lx : label) (ly : label) (uf : WeirdUnionFind) :=
-    let ufx := MakeSet lx uf in
-    let ufxy := MakeSet ly ufx in
-    let fx := Find lx ufxy in
-    let fy := Find ly ufxy in
-    map (fun pl => if pl.2 == fx then (pl.1,fy) else pl) ufxy.
-
-  Lemma EmptyWeirdUnionFind_Find (l : label) : Find l [::] = l.
-  Proof.
-    by rewrite /Find.
-  Qed.
+  (*I would like to replace label by S, but I need to solve the equality stuff...*)
+  Context (S : Set).
 
   (*Equality shenanigans...*)
   Lemma eq_refl (l : label) : l == l.
@@ -48,14 +27,53 @@ Section WEIRDUNIONFIND.
   Lemma eq_label (lx ly : label) : (lx == ly) = true -> lx = ly.
   Admitted.
 
+  Definition unionfind := seq (label * label).
+
+  Definition Empty : unionfind := [::].
+
+  Definition makeset (uf : unionfind) (l : label) :=
+    if has (fun pl => pl.1 == l) uf
+    then uf
+    else (l,l) :: uf.
+  
+  Definition find (uf : unionfind) (l : label) :=
+    (nth (l,l) uf (seq.find (fun x => x.1 == l) uf)).2.
+
+  Definition union (uf : unionfind) (lx ly : label) :=
+    let ufx := makeset uf lx in
+    let ufxy := makeset ufx ly in
+    let fx := find ufxy lx in
+    let fy := find ufxy ly in
+    map (fun pl => if pl.2 == fx then (pl.1,fy) else pl) ufxy.
+
   (*hasNfind does not exist apparently?*)
-  Lemma hasNfind (T : Type) (a : pred T) s : ~~ has a s -> find a s = size s.
+  Lemma hasNfind (T : Type) (a : pred T) s : ~~ has a s -> seq.find a s = size s.
   Proof. by rewrite has_find; case: ltngtP (find_size a s). Qed.
 
-  Lemma MakeSet_Find (lm lf : label) (uf :WeirdUnionFind) : Find lf (MakeSet lm uf) = Find lf uf.
+  Lemma has_makeset (uf : unionfind) (lh lm : label) : has (fun x => x.1 == lh) (makeset uf lm) = (lm == lh) || (has (fun x => x.1 == lh) uf).
   Proof.
-    rewrite /MakeSet /Find.
-    case Hlmuf: (has (λ pl : pos_eqType * label, pl.1 == lm) uf) => //=.
+    rewrite /makeset.
+    case Hlmuf: (has (fun x => x.1 == lm) uf) => //=.
+    case Hlhuf: (has (fun x => x.1 == lh) uf); first by rewrite Bool.orb_true_r.
+    case Hlmlh: (lm == lh) => //.
+    by rewrite (eq_label Hlmlh) Hlhuf in Hlmuf.
+  Qed.
+
+  Lemma find_Empty (l : label) : find [::] l = l.
+  Proof.
+    by rewrite /find.
+  Qed.
+
+  Lemma find_cons (uf : unionfind) (p : label * label) (l : label) : find (p :: uf) l = if (p.1 == l) then p.2 else find uf l.
+  Proof.
+    rewrite /find /nth /=.
+    by case Hll1: (p.1 == l).
+  Qed.
+
+  Lemma find_makeset (uf : unionfind) (lf lm : label) : find (makeset uf lm) lf = find uf lf.
+  Proof.
+    rewrite /makeset /find.
+    case Hlmuf: (has (fun x => x.1 == lm) uf) => //=. 
     case Hlmlf: (lm == lf) => //=.
     move: Hlmuf.
     rewrite (eq_label Hlmlf).
@@ -67,21 +85,43 @@ Section WEIRDUNIONFIND.
   Qed.
 
   (*The only important thing about this UnionFind is that when using Union x y the representative of y stays the same.*)
-  Lemma Union_Find (l lx ly : label) (uf : WeirdUnionFind) : Find l (Union lx ly uf) = if (Find l uf == Find lx uf) then Find ly uf else Find l uf.
+  Lemma find_union (uf : unionfind) (l lx ly : label) : find (union uf lx ly) l = if (find uf l == find uf lx) then find uf ly else find uf l.
   Proof.
-    induction uf.
-    + rewrite !EmptyWeirdUnionFind_Find /Union /MakeSet /= /Find /=.
-      case Hlxly: (lx == ly) => /=.
-      - rewrite Hlxly !eq_refl /= (eq_sym l lx).
-        case Hlxl: (lx == l) => //=.
-        by rewrite (eq_label Hlxly).
-      rewrite !eq_refl /= (eq_sym ly lx) Hlxly /= (eq_sym ly lx) Hlxly eq_refl /= (eq_sym l lx).
-      case Hlxl: (lx == l); case Hlyl: (ly == l) => //=.
-      by rewrite (eq_label Hlyl).
-    by admit.
+    rewrite /union.
+    rewrite -(find_makeset uf l lx) -(find_makeset uf lx lx) -(find_makeset uf ly lx).
+    rewrite -(find_makeset (makeset uf lx) l ly) -(find_makeset (makeset uf lx) lx ly) -(find_makeset (makeset uf lx) ly ly).
+    pose ufxy := makeset (makeset uf lx) ly.
+    rewrite -/ufxy.
+    have: has (fun x => x.1 == ly) ufxy.
+    + by rewrite /ufxy has_makeset eq_refl Bool.orb_true_l.
+    have: has (fun x => x.1 == lx) ufxy.
+    + by rewrite /ufxy !has_makeset eq_refl Bool.orb_true_l Bool.orb_true_r.
+    induction ufxy as [|huf tuf IHtuf].
+    + by rewrite has_nil.
+    rewrite //= !find_cons.
+    case Hhufl: (huf.1 == l); case Hhuflx: (huf.1 == lx); case Hhufly: (huf.1 == ly).
+    + by rewrite eq_refl Hhufl.
+    + by rewrite eq_refl Hhufl.
+    + by case Huffind: (huf.2 == find tuf lx) => /=; rewrite Hhufl.
+    + by case Huffind: (huf.2 == find tuf lx) => /=; rewrite Hhufl.
+    + have Htuf: [seq (if pl.2 == huf.2 then (pl.1, huf.2) else pl) | pl <- tuf] = tuf.
+      - clear IHtuf.
+        induction tuf as [|htuf ttuf IHttuf] => //=.
+        rewrite IHttuf.
+        case Hhtufhuf: (htuf.2 == huf.2) => //.
+        by rewrite -(eq_label Hhtufhuf) -surjective_pairing.
+      rewrite eq_refl /= Hhufl Htuf => _ _.
+      case Hfindtuf: (find tuf l == huf.2) => //.
+      by rewrite (eq_label Hfindtuf).
+    + by admit.
+    + by admit.
+    rewrite /= => Hhaslx Hhasly; rewrite IHtuf => //.
+    case Hhuffindtuf: (huf.2 == find tuf lx) => /=.
+    + by rewrite Hhufl.
+    by rewrite Hhufl.
   Admitted.
 
-End WEIRDUNIONFIND.
+End UnionFind.
 
 Section PairFoldLeft.
 
@@ -91,62 +131,43 @@ Section PairFoldLeft.
 
 End PairFoldLeft.
 
-Section TUNNELING.
+Section Tunneling.
 
   Context (fn : funname).
 
   Definition Linstr_align := (MkLI xH Lalign).
 
-  Definition tunnel_draw (uf : WeirdUnionFind) (c c' : linstr) :=
+  Definition tunnel_chart (uf : unionfind) (c c' : linstr) :=
     match c, c' with
       | MkLI _ li, MkLI _ li' =>
         match li, li' with
-          | Llabel l, Lgoto (fn',l') => if fn == fn' then uf else Union l l' uf
+          | Llabel l, Lgoto (fn',l') => if fn == fn' then uf else union uf l l'
           | _, _ => uf
         end
     end.
 
-  Definition tunnel_plan (uf : WeirdUnionFind) := pairfoldl tunnel_draw uf Linstr_align.
+  Definition tunnel_plan (uf : unionfind) := pairfoldl tunnel_chart uf Linstr_align.
 
-  Definition tunnel_bore (uf : WeirdUnionFind) (c c' : linstr) :=
+  Definition tunnel_bore (uf : unionfind) (c c' : linstr) :=
     match c, c' with
       | MkLI _ li, MkLI _ li' =>
         match li, li' with
-          | Llabel l, Lgoto (fn',l') => if fn == fn' then Lgoto (fn', Find l' uf) else Lgoto (fn',l')
-          | _, Lcond pe l' => Lcond pe (Find l' uf)
+          | Llabel l, Lgoto (fn',l') => if fn == fn' then Lgoto (fn', find uf l') else Lgoto (fn',l')
+          | _, Lcond pe l' => Lcond pe (find uf l')
           | _, _ => li'
         end
     end.
 
   Definition tunnel (lc : lcmd) :=
-    let uf := tunnel_plan EmptyWeirdUnionFind lc in
+    let uf := tunnel_plan Empty lc in
     pairmap (tunnel_bore uf) Linstr_align lc.
 
-  (*
-  Definition tunnel_build_uf (lc : lcmd) (uf : WeirdUnionFind) :=
-    let uf_Lgoto uf' c :=
-      match c with
-        | MkLI l li =>
-          match li with
-            | Lgoto (fn',l') => if fn == fn' then uf' else Union l l' (MakeSet l (MakeSet l' uf'))
-            | _ => uf'
-          end
-      end
-    in
-    foldl uf_Lgoto uf lc.
+End Tunneling.
 
-  Definition tunnel (lc : lcmd) :=
-    let uf := tunnel_build_uf lc [::] in
-    let bore c :=
-      let: MkLI l li := c in
-        (l,
-        match li with
-          | Lgoto (fn',l') => if fn == fn' then Lgoto (fn', Find l' uf) else Lgoto (fn',l')
-          | Lcond pe l' => Lcond pe (Find l' uf)
-          | _ => li
-        end)
-    in
-    map bore lc.
-  *)
+Section TunnelingProof.
 
-End TUNNELING.
+  Lemma lsem_tunneling 
+
+End TunnelingProof.
+
+
