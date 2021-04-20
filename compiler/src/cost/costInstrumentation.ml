73926279e86ef8eea3f9512fc55410c4f68c09fe
Vincent Laporte <Vincent.Laporte@inria.fr>
2021-04-20 10:55:52 +0200

    Cost: instrumentation of calls

--- compiler/src/cost/costInstrumentation.ml
+++ compiler/src/cost/costInstrumentation.ml
@@ -54,40 +54,80 @@ let add_count_instruction fn p f s =
   Also returns the set of fresh variables introduced.
 Maintains the current path
 *)
-let rec instrument_stmt fn p f s =
-  let _p, f, acc = List.fold_left (instrument_instr fn) (p, f, []) s in
+let instrument_stmt context fn p f s =
+
+let rec instrument_stmt p f s =
+  let _p, f, acc = List.fold_left instrument_instr (p, f, []) s in
   f, List.rev acc
-and instrument_instr fn (p, f, acc) i =
-  let f, i_desc = instrument_instr_r fn p f i.i_desc in
+and instrument_instr (p, f, acc) i =
+  let f, i_desc = instrument_instr_r p f i.i_desc in
   next p, f, { i with i_desc } :: acc
-and instrument_instr_r fn p f i =
+and instrument_instr_r p f i =
   match i with
-  | (Cassgn _ | Copn _ | Ccall _) -> f, i
+  | (Cassgn _ | Copn _) -> f, i
+  | Ccall (ii, xs, fn, es) ->
+     let xs, es, f =
+       List.fold_right (fun x (xs, es, f) ->
+           let x = V.clone x in
+           let xi = inloc x in
+           Lvar xi :: xs,
+           Pvar xi :: es,
+           Sv.add x f
+         )
+         (Mf.find fn context)
+         (xs, es, f)
+     in f, Ccall (ii, xs, fn, es)
   | Cif (c, th, el) ->
-     let f, th = instrument_stmt fn (branch_left p) f th in
+     let f, th = instrument_stmt (branch_left p) f th in
      let f, th = add_count_instruction fn p f th in
-     let f, el = instrument_stmt fn (branch_right p) f el in
+     let f, el = instrument_stmt (branch_right p) f el in
      f, Cif (c, th, el)
   | Cfor (x, r, s) ->
-     let f, s = instrument_stmt fn (down p) f s in
+     let f, s = instrument_stmt (down p) f s in
      let f, s = add_count_instruction fn p f s in
      f, Cfor (x, r, s)
   | Cwhile (a, s1, c, s2) ->
-     let f, s1 = instrument_stmt fn (branch_right p) f s1 in
-     let f, s2 = instrument_stmt fn (branch_left p) f s2 in
+     let f, s1 = instrument_stmt (branch_right p) f s1 in
+     let f, s2 = instrument_stmt (branch_left p) f s2 in
      let f, s2 = add_count_instruction fn p f s2 in
      f, Cwhile (a, s1, c, s2)
 
-let instrument_body fn s =
-  let f, s = instrument_stmt fn empty_path Sv.empty s in
-  f, Sv.fold add_reset_instruction f s
+in instrument_stmt p f s
 
-let instrument_fd fd =
-  let f, f_body = instrument_body fd.f_name.fn_name fd.f_body in
-  f |> Sv.elements |> List.rev_map (fun x -> fd.f_name.fn_name, x),
-  { fd with f_body }
+let instrument_body context fn s =
+  instrument_stmt context fn empty_path Sv.empty s
+
+let instrument_fd context fd =
+  let f, f_body = instrument_body context fd.f_name.fn_name fd.f_body in
+  match fd.f_cc with
+  | Export ->
+     let f_body = Sv.fold add_reset_instruction f f_body in
+     f |> Sv.elements |> List.rev_map (fun x -> fd.f_name.fn_name, x),
+     context,
+     { fd with f_body }
+  | Internal ->
+     let f_tyin, f_args, f_tyout, f_ret, extra =
+       Sv.fold (fun x (tyin, args, tyout, ret, extra) ->
+           tint :: tyin,
+           x :: args,
+           tint :: tyout,
+           inloc x :: ret,
+           x :: extra
+         ) f (fd.f_tyin, fd.f_args, fd.f_tyout, fd.f_ret, [])
+     in
+     [],
+     Mf.add fd.f_name extra context,
+     { fd with f_tyin; f_args; f_tyout; f_ret; f_body }
 
 let instrument (gd, fds) =
-  let fs, fds = fds |> List.map instrument_fd |> List.split in
-  List.fold_left List.rev_append [] fs,
+  let fs, fds, _context =
+    List.fold_right (fun fd (fs, fds, context) ->
+        let f, context, fd = instrument_fd context fd in
+        List.rev_append f fs,
+        fd :: fds,
+        context)
+      fds
+      ([], [], Mf.empty)
+  in
+  fs,
   (gd, fds)
