e0969fb5f441b6c63c8f1f8251b68aadcdeab3ad
Vincent Laporte <Vincent.Laporte@inria.fr>
2021-08-04 17:07:27 +0200

    update coqword

--- coqword.nix
+++ coqword.nix
@@ -9,7 +9,7 @@ let mathcomp =
  ).algebra
 ; in
 
-let rev = "7c650450e03310ca67bdccb64c95be82116945c7"; in
+let rev = "131bee8a1c14a67a4925534e455ea0b870ee0615"; in
 
 stdenv.mkDerivation rec {
   version = "0.0-git-${builtins.substring 0 8 rev}";
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
     owner = "jasmin-lang";
     repo = "coqword";
     inherit rev;
-    sha256 = "1b4p67599s6cqbs3r1pd736dq5zivvi3w8wbl4dhcg6mnzbgvkyg";
+    sha256 = "196w51biq9wagbr0dkjd2v2z16nf6sz0yy5r4q1mgnpdld0rs9m1";
   };
 
   buildInputs = [ coq ];
