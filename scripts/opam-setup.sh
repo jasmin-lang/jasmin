3fbf3b4fdfbc3f99ca46f12aced6175be390fad4
Jean-Christophe Léchenet <jean-christophe.lechenet@inria.fr>
2022-05-04 15:27:06 +0200

    Hacky fix to opam problem in CI
    We patch the script used by opam to sandbox its subcommands
    (build/install) to make $OPAMROOT available in the sandbox. It was not
    the case before because of the unusual path of the opam installation (in
    /builds). The proper fix is to update to opam.2.2 when it is released
    and revert this commit.
    
    (cherry picked from commit 12628a78c3a3be5f1cd53fe61f901bcb8ba183aa)

--- scripts/opam-setup.sh
+++ scripts/opam-setup.sh
@@ -3,6 +3,21 @@
 set -e
 
 opam init --no-setup --compiler=4.12.1
+# If $OPAMROOT is not in a standard location, it may happen that the sandbox
+# hides it during the building of packages, which for some packages results in
+# a build failure. There is a variable OPAM_USER_PATH_RO that is provided to
+# make more directories available inside the sandbox, but the opam nix package
+# overrides its value using "makeWrapper --set". This temporary and hacky fix
+# consists in patching the sandbox script used by opam to add $OPAMROOT to
+# OPAM_USER_PATH_RO after it was overwritten by makeWrapper. In the
+# (hopefully near) future, opam 2.2 will make (nearly) the whole filesystem
+# available in read-only mode, thus eliminating the need for OPAM_USER_PATH_RO
+# and this hack.
+if patch -N -r - "$OPAMROOT/opam-init/hooks/sandbox.sh" scripts/fix_path_ro.patch >/dev/null 2>&1 ; then
+  echo "$OPAMROOT/opam-init/hooks/sandbox.sh patched successfully"
+else
+  echo "no need to patch $OPAMROOT/opam-init/hooks/sandbox.sh"
+fi
 if [ $1 ]
 then
   opam repo add coq-released https://coq.inria.fr/opam/released
