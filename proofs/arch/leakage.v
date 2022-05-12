cbe2f174be5e4cd1ba79fc1558b82186822f5926
Vincent Laporte <Vincent.Laporte@inria.fr>
2022-05-12 15:51:30 +0200

    minor cleaning

--- proofs/arch/leakage.v
+++ proofs/arch/leakage.v
@@ -1,32 +1,5 @@
-(* ** License
- * -----------------------------------------------------------------------
- * Copyright 2016--2017 IMDEA Software Institute
- * Copyright 2016--2017 Inria
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * ----------------------------------------------------------------------- *)
 From mathcomp Require Import all_ssreflect all_algebra.
-From CoqWord Require Import ssrZ.
-Require Import Psatz xseq.
-Require Export array. 
-Require Import expr label. 
+Require Import memory_model label.
 Import Utf8.
 
 Set Implicit Arguments.
@@ -43,7 +16,7 @@ Context {pd: PointerData}.
 
 (** Leakage for assembly-level **)
 
-Inductive leak_asm : Type :=
+Variant leak_asm : Type :=
   | Ljump  of seq pointer & remote_label  
   | Ljumpc of bool (* the value of the test *) 
   | Lop    of seq pointer.
@@ -55,10 +28,9 @@ End Leak_Asm.
 Section Directive_Asm.
 
 (** Directive for assembly-level **)
-Inductive directive_asm : Type :=
+Variant directive_asm : Type :=
   | Dstep  : directive_asm
   | Dforce : directive_asm (* force directive : 
                               in case test evaluates to true, force always                                                  resembles taking else branch *).
 
 End Directive_Asm.
- 
