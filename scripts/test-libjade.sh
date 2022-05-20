70c88f2ee007b1ae84e5c0076fc5343eac3c1361
Vincent Laporte <Vincent.Laporte@inria.fr>
2022-05-20 12:58:12 +0200

    CI: Use EXCLUDE variable from libjade Makefile

--- scripts/test-libjade.sh
+++ scripts/test-libjade.sh
@@ -9,17 +9,15 @@ DIR=$NAME-$BRANCH
 OUT=results
 
 # Exclude primitives that are known not to build
-EXCLUDED=crypto_kem
+export EXCLUDE="./crypto_kem/kyber/kyber512/amd64/avx2/ ./crypto_kem/kyber/kyber768/amd64/avx2/"
 
 curl -v -o $FILE https://codeload.github.com/formosa-crypto/$NAME/tar.gz/refs/heads/$BRANCH
 tar xvf $FILE
 
-for d in $EXCLUDED
-do
-	rm -rf $DIR/src/$d
-done
-
 make -C $DIR/src CI=1 JASMIN=$PWD/compiler/jasminc.native
+res=$?
 
 mkdir -p $OUT
 (cd $OUT && tar xvf ../$DIR/src/check.tar.gz)
+
+exit $res
