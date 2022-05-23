8159e08beda3c74b2d76e499fd3e0c547206db95
tiago <tfaoliveira@gmail.com>
2022-05-23 16:52:30 +0200

    scripts: ci: updating EXCLUDE variable in test-libjade.sh to be synced with libjade main

--- scripts/test-libjade.sh
+++ scripts/test-libjade.sh
@@ -9,12 +9,12 @@ DIR=$NAME-$BRANCH
 OUT=results
 
 # Exclude primitives that are known not to build
-export EXCLUDE="./crypto_kem/kyber/kyber512/amd64/avx2/ ./crypto_kem/kyber/kyber768/amd64/avx2/"
+export EXCLUDE="crypto_kem/kyber/kyber512/amd64/avx2/ crypto_kem/kyber/kyber768/amd64/avx2/"
 
 curl -v -o $FILE https://codeload.github.com/formosa-crypto/$NAME/tar.gz/refs/heads/$BRANCH
 tar xvf $FILE
 
-make -C $DIR/src CI=1 JASMIN=$PWD/compiler/jasminc.native
+make -C $DIR/src CI=1 #JASMIN=$PWD/compiler/jasminc.native
 res=$?
 
 mkdir -p $OUT
