53bd69bac1bd0db9e8b6408ed54655b79fae1a69
Vincent Laporte <Vincent.Laporte@inria.fr>
2022-06-17 09:13:44 +0200

    CT checker: return signatures of checked functions instead of unit

--- compiler/src/ct_checker_forward.mli
+++ compiler/src/ct_checker_forward.mli
@@ -1,3 +1,10 @@
 open Prog
 
-val ty_prog : infer:bool -> ('info, 'asm) prog -> Name.t list -> unit
+type signature
+(** Security type of a function *)
+
+val pp_signature : Format.formatter -> funname * signature -> unit
+(** Human-readable form of a signature *)
+
+val ty_prog :
+  infer:bool -> ('info, 'asm) prog -> Name.t list -> (funname * signature) list
