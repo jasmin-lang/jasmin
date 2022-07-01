e07ff4f4f079c899fef2524db15c156ea46804af
Vincent Laporte <Vincent.Laporte@inria.fr>
2022-07-01 09:10:13 +0100

    Linear: linstr need not be an eqType

--- proofs/lang/linear.v
+++ proofs/lang/linear.v
@@ -62,47 +62,4 @@ Record lprog :=
     lp_globs : seq u8;
     lp_funcs : seq (funname * lfundef) }.
 
-Definition eqb_r i1 i2 :=
-  match i1, i2 with
-  | Lopn lv1 o1 e1, Lopn lv2 o2 e2 => (lv1 == lv2) && (o1 == o2) && (e1 == e2)
-  | Lsyscall o1, Lsyscall o2 => o1 == o2
-  | Lalign, Lalign => true
-  | Llabel l1, Llabel l2 => l1 == l2
-  | Lgoto l1, Lgoto l2 => l1 == l2
-  | Ligoto e1, Ligoto e2 => e1 == e2
-  | LstoreLabel x1 lbl1, LstoreLabel x2 lbl2 => (x1 == x2) && (lbl1 == lbl2)
-  | Lcond e1 l1, Lcond e2 l2 => (e1 == e2) && (l1 == l2)
-  | _, _ => false
-  end.
-
-Lemma eqb_r_axiom : Equality.axiom eqb_r.
-Proof.
-  case => [lv1 o1 e1|o1||l1|l1|e1|lv1 l1|e1 l1] [lv2 o2 e2|o2||l2|l2|e2|lv2 l2|e2 l2] //=;try by constructor.
-  + apply (@equivP (((lv1 == lv2) && (o1 == o2)) /\ e1 == e2 ));first by apply andP.
-    by split => [ [] /andP [] /eqP -> /eqP -> /eqP -> //| [] -> -> ->];rewrite !eqxx.
-  1-4: by apply: (equivP eqP); split; congruence.
-  + apply: (equivP andP); split.
-    * by case=> /eqP <- /eqP <-.
-    by case => <- <-; rewrite !eqxx.
-  apply (@equivP ((e1 == e2) /\ (l1 == l2)));first by apply andP.
-  by split => [ [] /eqP -> /eqP -> //| [] -> ->];rewrite !eqxx.
-Qed.
-
-Definition linstr_r_eqMixin := Equality.Mixin eqb_r_axiom.
-Canonical  linstr_r_eqType  := Eval hnf in EqType linstr_r linstr_r_eqMixin.
-
-Definition eqb i1 i2 :=
-  (li_ii i1 == li_ii i2) && (eqb_r (li_i i1) (li_i i2)).
-
-Lemma eqb_axiom : Equality.axiom eqb.
-Proof.
-  case=> [ii1 i1] [ii2 i2];rewrite /eqb /=.
-  apply (@equivP ((ii1 == ii2) /\ eqb_r i1 i2));first by apply andP.
-  split => [[]/eqP -> /eqb_r_axiom -> // | [] -> ->];rewrite eqxx;split => //.
-  by apply /eqb_r_axiom.
-Qed.
-
-Definition linstr_eqMixin := Equality.Mixin eqb_axiom.
-Canonical  linstr_eqType  := Eval hnf in EqType linstr linstr_eqMixin.
-
 End ASM_OP.
