23643d33b2ca0ce79ed94212d7983783720d0ed1
Vincent Laporte <Vincent.Laporte@inria.fr>
2022-08-01 13:05:05 +0200

    Printer: fix printing of opn tags

--- compiler/src/printer.ml
+++ compiler/src/printer.ml
@@ -218,9 +218,10 @@ let rec pp_gi pp_info pp_len pp_opn pp_var fmt i =
       | Sopn.Oasm (Arch_extra.BaseOp(Some ws, _)) -> Format.fprintf fmt "(%du)" (int_of_ws ws)
       | _ -> () in
 
-    F.fprintf fmt "@[<hov 2>%a %s=@ %a#%a(%a);@]"
-      (pp_glvs pp_len pp_var) x (pp_tag t) pp_cast o pp_opn o
+    F.fprintf fmt "@[<hov 2>%a =@ %a#%a(%a); /* %s */@]"
+      (pp_glvs pp_len pp_var) x pp_cast o pp_opn o
       (pp_ges pp_len pp_var) e
+      (pp_tag t)
 
   | Csyscall(x, o, e) ->
       F.fprintf fmt "@[<hov 2>%a =@ %s(%a);@]"
