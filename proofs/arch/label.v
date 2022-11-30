e6c781c196241b45eacb742406eab7d302067e5c
Vincent Laporte <Vincent.Laporte@inria.fr>
2022-11-30 21:04:23 +0100

    label: suggest a consistent axiom

--- proofs/arch/label.v
+++ proofs/arch/label.v
@@ -20,6 +20,41 @@ Definition remote_label := (funname * label)%type.
 Section WITH_POINTER_DATA.
 Context {pd: PointerData}.
 
+Section  SPEC.
+  Context
+    (enc: seq remote_label → remote_label → option pointer)
+    (dec: seq remote_label → pointer → option remote_label).
+
+  Definition decode_encode_label_t : Prop :=
+    ∀ dom lbl,
+      (Z.of_nat (size dom) < wbase Uptr)%Z →
+      lbl \in dom →
+      obind (dec dom) (enc dom lbl) = Some lbl.
+
+End  SPEC.
+
+Section CONSISTENCY.
+  Lemma decode_encode_label_consistent :
+    ∃ enc dec, decode_encode_label_t enc dec.
+  Proof.
+    exists (λ dom lbl,
+             let r := find (pred1 lbl) dom in
+             if r < size dom
+             then Some (wrepr Uptr (Z.of_nat r))
+             else None).
+    exists (λ dom p, oseq.onth dom (Z.to_nat (wunsigned p))).
+    move => dom lbl small_dom.
+    rewrite -has_pred1 => /dup[] => lbl_in_dom.
+    rewrite has_find => /= /dup[] /ltP found -> /=.
+    rewrite wunsigned_repr_small; last first.
+    - move: (find _ _) (size _) small_dom found => n m; Lia.lia.
+    rewrite Nat2Z.id oseq.onth_nth.
+    rewrite (nth_map lbl); last exact/ltP.
+    by have /eqP -> := nth_find lbl lbl_in_dom.
+  Qed.
+
+End CONSISTENCY.
+
 Parameter encode_label : seq remote_label → remote_label → option pointer.
 Parameter decode_label : seq remote_label → pointer → option remote_label.
 Axiom decode_encode_label : ∀ dom lbl, lbl \in dom → obind (decode_label dom) (encode_label dom lbl) = Some lbl.
