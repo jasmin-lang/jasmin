8b3bab1bc5546970b55fd9c3b1d07c5c3d16af5f
Vincent Laporte <Vincent.Laporte@inria.fr>
2023-01-12 15:27:10 +0100

    Turn a reachable “assert false” into a proper error
    (cherry picked from commit 1c09c7b5b42ce207daa09d5f07999dd838d8d524)
    (cherry picked from commit 4dda68154960f90174c3e9da3f54762a37a81113)

--- compiler/src/alias.ml
+++ compiler/src/alias.ml
@@ -157,7 +157,8 @@ let slice_of_pexpr a =
   | Parr_init _ -> None
   | Pvar x -> Some (normalize_gvar a x)
   | Psub (aa, ws, len, x, i) -> Some (normalize_asub a aa ws len x i)
-  | (Pconst _ | Pbool _ | Pget _ | Pload _ | Papp1 _ | Papp2 _ | PappN _ | Pif _) -> assert false
+  | (Pconst _ | Pbool _ | Pget _ | Pload _ | Papp1 _ | Papp2 _ | PappN _ ) -> assert false
+  | Pif _ -> hierror_no_loc "conditional move of (ptr) arrays is not supported yet"
 
 let slice_of_lval a =
   function
