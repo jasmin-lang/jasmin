c848d359b285a925a0f43fef064645ae76718f5f
Vincent Laporte <Vincent.Laporte@inria.fr>
2023-02-17 10:16:59 +0100

    insert-copy-and-fix-length: remove legacy comment

--- compiler/src/insert_copy_and_fix_length.ml
+++ compiler/src/insert_copy_and_fix_length.ml
@@ -1,11 +1,3 @@
-(* This step has two purposes: 
-   1/ Fix the size information (n) in Ocopy(ws, n). 
-      For the moment pretyping add a dummy value for n, it is fixed here.
-   2/ Replace x = y with #copy, when x and y are arrays and at least one of them
-      is a reg array. This #copy will be transformed into a loop later.
-      This is optional: !Glob_options.introduce_array_copy 
-*)
-
 open Utils
 open Prog
 module L = Location
