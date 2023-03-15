0a48aa66ed39cf2c8bf33ab6987690c191c62fe2
Vincent Laporte <Vincent.Laporte@inria.fr>
2023-03-15 06:12:33 +0100

    CI: update nixpkgs (feat. OCaml 4.14.1)

--- scripts/nixpkgs.nix
+++ scripts/nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/6a0d2701705c3cf6f42c15aa92b7885f1f8a477f.tar.gz";
-  sha256 = "sha256:0dk8wx7rn6311rf1hanwda6xrgb0rbk4a1wf1zavjfcyxk2687lg";
+  url = "https://github.com/NixOS/nixpkgs/archive/67f26c1cfc5d5783628231e776a81c1ade623e0b.tar.gz";
+  sha256 = "sha256:1spvf68a2yjxd13x7q1n3nppnmp5vf0m1gl9lp3n58bcs3frcnb3";
 })
