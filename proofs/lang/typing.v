5200d3e120f86318ae42d8e9a68747f6d9d9bdd4
swarnpriya <swarn.099016@gmail.com>
2023-05-10 16:07:42 +0200

    wip for soundness of type system

--- proofs/lang/typing.v
+++ proofs/lang/typing.v
@@ -26,67 +26,65 @@ end.
 
 Section TypeE.
 
-Variable ty_expr : wsize -> pexpr -> exec stype.
+Variable ty_pexpr : wsize -> pexpr -> exec stype.
 
 (* Pget aa ws x e: Checks x is of type array and e(index) is of type int 
    Returns word of size ws *)
 Definition ty_get_set (pd : wsize) (ws : wsize) (x : gvar) (e : pexpr) : exec stype := 
 let tx := x.(gv).(v_var).(vtype) in 
-Let te := ty_expr pd e in
+Let te := ty_pexpr pd e in
 Let ta := check_array (Pvar x) tx in 
 Let ti := check_int e te in
 ok (sword ws).
 
 (* Psub aa ws len x e: Checks x is of type array and e(index) is of type int
-   Returns array type of length len *)
+   Returns array type of length len *) 
 Definition ty_get_set_sub (pd : wsize) (ws : wsize) (len : positive) (x : gvar) (e : pexpr) : exec stype := 
 let tx := x.(gv).(v_var).(vtype) in 
-Let te := ty_expr pd e in 
+Let te := ty_pexpr pd e in 
 Let ta := check_array (Pvar x) tx in 
 Let ti := check_int e te in 
-ok (sarr len).
+ok (sarr (Z.to_pos (wsize_size ws) * len)).
 
 Definition ty_load_store (pd : wsize) (ws : wsize) (x : var_i) (e : pexpr) : exec stype :=
 let tx := x.(v_var).(vtype) in 
-Let te := ty_expr pd e in 
+Let te := ty_pexpr pd e in 
 Let tp := check_ptr (Pvar {| gv := x; gs := Slocal |}) pd tx in 
 Let tp' := check_ptr e pd te in 
 ok (sword ws).
 
 Definition check_expr (pd : wsize) (e : pexpr) (ty : stype) : exec stype :=
-Let te := ty_expr pd e in 
+Let te := ty_pexpr pd e in 
 check_type e te ty.
 
 Definition check_pexprs (pd : wsize) (es : pexprs) (tys : seq stype) : exec (seq stype) := 
-if (size tys == size es) 
-then mapM2 ErrType (check_expr pd) es tys 
-else type_error.
+mapM2 ErrType (check_expr pd) es tys. 
 
 End TypeE.
 
 (*------------- Typing rules for expressions ---------------*) 
-Fixpoint ty_expr (pd : wsize) (e : pexpr) : exec stype := 
+Fixpoint ty_pexpr (pd : wsize) (e : pexpr) : exec stype := 
 match e with 
 | Pconst _ => ok sint
 | Pbool _ => ok sbool
 | Parr_init len => ok (sarr len)
 | Pvar x => ok (x.(gv).(v_var).(vtype))
-| Pget aa ws x e => ty_get_set ty_expr pd ws x e
-| Psub aa ws len x e => ty_get_set_sub ty_expr pd ws len x e
-| Pload ws x e => ty_load_store ty_expr pd ws x e 
+| Pget aa ws x e => ty_get_set ty_pexpr pd ws x e
+| Psub aa ws len x e => ty_get_set_sub ty_pexpr pd ws len x e
+| Pload ws x e => ty_load_store ty_pexpr pd ws x e 
 | Papp1 op e => let (tin, tout) := type_of_op1 op in 
-                Let ce := check_expr ty_expr pd e tin in 
+                Let ce := check_expr ty_pexpr pd e tin in 
                 ok tout
 | Papp2 op e1 e2 => let top := type_of_op2 op in 
-                    Let ce1 := check_expr ty_expr pd e1 top.1.1 in
-                    Let ce2 := check_expr ty_expr pd e2 top.1.2 in 
+                    Let ce1 := check_expr ty_pexpr pd e1 top.1.1 in
+                    Let ce2 := check_expr ty_pexpr pd e2 top.1.2 in 
                     ok top.2
 | PappN op es => let top := type_of_opN op in 
-                 Let ces := check_pexprs ty_expr pd es top.1 in 
+                 Let ces := check_pexprs ty_pexpr pd es top.1 in 
                  ok top.2
-| Pif ty b et ef => Let cb := check_expr ty_expr pd b sbool in
-                    Let ct := check_expr ty_expr pd et ty in 
-                    Let cf := check_expr ty_expr pd ef ty in 
+| Pif ty b et ef => Let cb := check_expr ty_pexpr pd b sbool in
+                    Let ct := check_expr ty_pexpr pd et ty in 
+                    Let cf := check_expr ty_pexpr pd ef ty in 
                     ok ty
 end.
 
@@ -110,9 +108,9 @@ Definition ty_lval (pd : wsize) (l : lval) : exec stype :=
 match l with 
 | Lnone _ ty => ok ty
 | Lvar x => ok x.(v_var).(vtype)
-| Lmem ws x e => ty_load_store ty_expr pd ws x e
-| Laset aa ws x e => ty_get_set ty_expr pd ws {| gv := x; gs := Slocal |} e
-| Lasub aa ws len x e => ty_get_set_sub ty_expr pd ws len {| gv := x; gs := Slocal |} e
+| Lmem ws x e => ty_load_store ty_pexpr pd ws x e
+| Laset aa ws x e => ty_get_set ty_pexpr pd ws {| gv := x; gs := Slocal |} e
+| Lasub aa ws len x e => ty_get_set_sub ty_pexpr pd ws len {| gv := x; gs := Slocal |} e
 end.
 
 (*-------------- Typing rules for instr --------------------*)
---
+++ proofs/lang/typing_proof.v
@@ -0,0 +1,74 @@
+From mathcomp Require Import all_ssreflect all_algebra.
+From mathcomp Require Import word_ssrZ.
+Require Export array type expr gen_map low_memory warray_ sem_type sem_op_typed values typing sem.
+Require Export
+  flag_combination
+  sem_params.
+
+Section TYPING_PEXPR.
+
+Context
+  {asm_op syscall_state : Type}
+  {ep : EstateParams syscall_state}
+  {spp : SemPexprParams}
+  (gd : glob_decls).
+
+Theorem sem_pexpr_type : forall pd ty e s v,
+ty_pexpr pd e = ok ty ->
+sem_pexpr gd s e = ok v ->
+type_of_val v = ty.
+Proof.
+move=> pd ty e s v hty hsem. case: e hty hsem.
+(* Pconst *)
++ rewrite /ty_pexpr /sem_pexpr /type_of_val. by move=> z [<-] [<-]. 
+(* Pbool *)
++ rewrite /ty_pexpr /sem_pexpr /type_of_val. by move=> b [<-] [<-].
+(* Parr_init *)
++ rewrite /ty_pexpr /sem_pexpr /type_of_val. by move=> a [<-] [<-].  
+(* Pvar *)
++ rewrite /ty_pexpr /sem_pexpr. move=> g [] <- hg. by have := type_of_get_gvar hg.
+(* Pget *)
++ move=> a w g p /=. rewrite /ty_get_set /=. t_xrbindP=> tp htp ta htc ti hti <- /=.
+  apply on_arr_gvarP. move=> n t harr hget. by t_xrbindP=> z zv hp hi z1 hz1 <- /=.
+(* Psub *)
++ move=> a w p g p' /=. rewrite /ty_get_set_sub /=. t_xrbindP=> tp htp ta htc ti hti /= heq; subst.
+  apply on_arr_gvarP. move=> n t harr hget. 
+  t_xrbindP=> z zv he hi /= z1 hg hv; subst. by have heq' := to_intI hi; subst; rewrite /=. 
+(* Pload *)
++ move=> w vi e /=. rewrite /ty_load_store /=. t_xrbindP=> pt het vit. rewrite /check_ptr /= /check_type.
+  case: ifP => //=. case: (vtype vi)=> //= wsz hpd [] heq; subst. case: pt het=> //= wsz' het zt. 
+  case: ifP=> //= hpd' [] heq; subst. move=> heq; subst. by move=> zw viv hg hp zw' viv' he hp' wz hr <- /=. 
+(* Papp1 *)
++ move=> op e /=. rewrite /check_expr /= /check_type /=. case: op=> //=.
+  + t_xrbindP=> w et et' het. case: ifP=> //= /eqP heq [] heq' hty; subst.
+    move=> zv he hop. by have [x hov -> /=] := sem_sop1I hop.
+  + t_xrbindP=> w zt zt' /= het. case: zt' het=> //= w' het. case: ifP=> //= hcmp [] heq hty'; subst.
+    move=> z he hop. by have [x hov -> /=] := sem_sop1I hop.
+  + t_xrbindP=> w w' zt zt' het. case: zt' het=> //= w'' het. case: ifP=> //= hcmp [] heq hty; subst.
+    move=> z he hop. by have [x hov -> /=] := sem_sop1I hop.
+  + t_xrbindP=> w w' zt zt' het. case: zt' het=> //= w'' het. case: ifP=> //= hcmp [] heq hty; subst.
+    move=> z he hop. by have [x hov -> /=] := sem_sop1I hop.
+  + t_xrbindP=> zt zt' het. case: ifP=> //= /eqP heq [] heq' hty; subst. move=> zv he hop.
+    by have [x hov -> /=] := sem_sop1I hop.
+  + t_xrbindP=> w zt zt' het. case: zt' het=> //= w' het. case: ifP=> //= hcmp [] heq hty; subst.
+    move=> zv he hop. by have [x hov -> /=] := sem_sop1I hop. 
+  move=> ok. case: ok=> //=.
+  + t_xrbindP=> zt zt' /= hty. case: ifP=> //= /eqP heq [] heq' heq''; subst.
+    move=> zv he hop. by have [x hov -> /=] := sem_sop1I hop.
+  move=> w. t_xrbindP=> te te' hty. case: te' hty=> //= wsz hty. case: ifP=> //= hcmp [] ht ht'; subst.
+  move=> zv he hop. by have [x hov -> /=] := sem_sop1I hop.
+(* Papp2 *)
++ move=> op2 e1 e2 /=. rewrite /check_expr /= /check_type. t_xrbindP=> et1 et1' hty1. 
+  case: ifP=> //= hsub [] heq et2 et2' hty2; subst. case: ifP=> //= hsub' [] heq' hty'; subst.
+  move=> ev1 /= he1 ev2 he2 hop. have [w1 [w2 [w3 [/= hv hv' hop' hveq]]]] := sem_sop2I hop; subst.
+  by apply type_of_to_val.
+(* PappN *)
++ admit.
+(* Pif *)
+move=> sty b e1 e2 /=. rewrite /check_expr /= /check_type /=. 
+t_xrbindP=> bty bty' htb. case: ifP=> //= /eqP heq; subst. move=> [] heq'; subst.
+move=> e1t e1t' hte1. case: ifP=> //= hsub [] heq; subst. move=> e2t e2t' hte2.
+case: ifP=> //= hsub' [] heq; subst. move=> hteq; subst. move=> bv bv' hb hbt e1v e1v' he1 ht e2v e2v' he2 ht' <-.
+case: ifP=> //= _. by have := truncate_val_has_type ht. by have := truncate_val_has_type ht'.
+Admitted.
+ 
