ace848a9085cf50c8117c2c6d1545edfdc50b785
Jean-Christophe Léchenet <jean-christophe.lechenet@inria.fr>
2023-05-22 18:43:34 +0200

    Cleaning in parseio
    In particular, use Menhir's traditional API. We use ocamllex anyway.

--- compiler/src/parseio.ml
+++ compiler/src/parseio.ml
@@ -1,31 +1,14 @@
 (* -------------------------------------------------------------------- *)
 open Utils
 
-module P = Parser
-module L = Lexing
-
 (* -------------------------------------------------------------------- *)
-let lexbuf_from_channel = fun name channel ->
-  let lexbuf = Lexing.from_function channel in
-    lexbuf.Lexing.lex_curr_p <- {
-        Lexing.pos_fname = name;
-        Lexing.pos_lnum  = 1;
-        Lexing.pos_bol   = 0;
-        Lexing.pos_cnum  = 0
-      };
-    lexbuf
-
-(* -------------------------------------------------------------------- *)
-let parserfun_entry =
-    MenhirLib.Convert.Simplified.traditional2revised P.module_
-
-(* -------------------------------------------------------------------- *)
-let lexer (lexbuf : L.lexbuf) =
-  let token = Lexer.main lexbuf in
-  (token, L.lexeme_start_p lexbuf, L.lexeme_end_p lexbuf)
+let lexbuf_from_function = fun name f ->
+  let lexbuf = Lexing.from_function f in
+  Lexing.set_filename lexbuf name;
+  lexbuf
 
 (* -------------------------------------------------------------------- *)
 let parse_program ?(name = "") (inc : IO.input) =
-  let lexbuf = lexbuf_from_channel name @@ fun buf n ->
+  let lexbuf = lexbuf_from_function name @@ fun buf n ->
   try IO.input inc buf 0 n with IO.No_more_input -> 0 in
-  parserfun_entry (fun () -> lexer lexbuf)
+  Parser.module_ Lexer.main lexbuf
