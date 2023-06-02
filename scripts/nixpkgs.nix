8eb81ad331baab13663933f1955060d83c31adfc
Vincent Laporte <Vincent.Laporte@inria.fr>
2023-06-02 11:22:21 +0200

    CI: update nix & nixpkgs

--- scripts/nixpkgs.nix
+++ scripts/nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/67f26c1cfc5d5783628231e776a81c1ade623e0b.tar.gz";
-  sha256 = "sha256:1spvf68a2yjxd13x7q1n3nppnmp5vf0m1gl9lp3n58bcs3frcnb3";
+  url = "https://github.com/NixOS/nixpkgs/archive/3a70dd92993182f8e514700ccf5b1ae9fc8a3b8d.tar.gz";
+  sha256 = "sha256:0npyj597wwrp7xyqwrkds537vs9i5gazhmaxv8wc8z0sjr1450fb";
 })
