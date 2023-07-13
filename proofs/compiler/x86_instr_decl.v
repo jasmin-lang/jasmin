e946b2254a4c6c3fe7bce3f0d77ca51376d16e6d
basavesh <basavesh.shivakumar@gmail.com>
2023-07-13 15:52:36 +0200

    Fix instruction printing of MULX
    (cherry picked from commit d47656030f729074151472a6cff67f51591c1991)
    (cherry picked from commit e3a1a9ad98be591180c63b698855c0cb99391610)

--- proofs/compiler/x86_instr_decl.v
+++ proofs/compiler/x86_instr_decl.v
@@ -1300,7 +1300,7 @@ Definition Ox86_MULX_instr :=
    ((fun (sz:wsize) =>
      mk_instr (pp_sz name sz) (w2_ty sz sz) (w2_ty sz sz)
          [::R RDX; E 2] [:: E 0; E 1] (reg_msb_flag sz)
-         (@x86_MULX sz) check_mulx 3 sz [::] (pp_iname name sz)),
+         (@x86_MULX sz) check_mulx 3 sz [::] (pp_iname "mulx" sz)),
     (name, PrimP U64 MULX)).
 
 Definition check_neg (_:wsize) := [::[::rm false]].
