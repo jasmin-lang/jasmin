a2cd9da2e20b233dec4e42c7262355354871add6
basavesh <basavesh.shivakumar@gmail.com>
2023-07-17 00:29:53 +0200

    wip... print the C H/W results too

--- compiler/entry/libfoo/foo.c
+++ compiler/entry/libfoo/foo.c
@@ -25,5 +25,22 @@ extern void set_execute_get(struct asm_state *);
 
 void set_execute_get_wrapper(struct asm_state *state) {
     set_execute_get(state);
-    printf("the flag is %ld\n", state->rflags);
+    printf("Post Execution\n");
+    printf("rax = %ld\n", state->rax);
+    printf("rcx = %ld\n", state->rcx);
+    printf("rdx = %ld\n", state->rdx);
+    printf("rbx = %ld\n", state->rbx);
+    printf("rsp = %ld\n", state->rsp);
+    printf("rbp = %ld\n", state->rbp);
+    printf("rsi = %ld\n", state->rsi);
+    printf("rdi = %ld\n", state->rdi);
+    printf("r8 = %ld\n", state->r8);
+    printf("r9 = %ld\n", state->r9);
+    printf("r10 = %ld\n", state->r10);
+    printf("r11 = %ld\n", state->r11);
+    printf("r12 = %ld\n", state->r12);
+    printf("r13 = %ld\n", state->r13);
+    printf("r14 = %ld\n", state->r14);
+    printf("r15 = %ld\n", state->r15);
+    printf("rflags = %ld\n", state->rflags);
 }
