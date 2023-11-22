910bd19c5cfb80d02328cd3f7d0542b2f9f3e71f
Vincent Laporte <Vincent.Laporte@inria.fr>
2023-11-22 10:05:22 +0100

    CI/opam: fix constraints & un-pin coq-mathcomp-word
    (cherry picked from commit c9785d9bd4996122ceb5074ed10d8c3bedbd2f99)
    (cherry picked from commit 438fcd0210d31d7b1341e18d198ccdea0dd7c5ef)

--- scripts/opam-setup.sh
+++ scripts/opam-setup.sh
@@ -6,7 +6,6 @@ opam init --disable-sandboxing --no-setup --compiler=4.14.1
 if [ 1 -le $# ]
 then
   opam repo add coq-released https://coq.inria.fr/opam/released
-  opam pin --yes --no-action add coq-mathcomp-word https://github.com/jasmin-lang/coqword.git
   opam pin --yes --no-depexts --no-action add .
 else
   opam pin --yes --no-depexts --no-action add compiler
