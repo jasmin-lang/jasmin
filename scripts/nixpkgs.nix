12478484eaf6b7c6400b38ca316beb27dceacd5b
Vincent Laporte <Vincent.Laporte@inria.fr>
2023-12-07 11:07:03 +0100

    CI: update nixpkgs & nix
    (cherry picked from commit ac3e4532ae2d7ae65b842e71876d75ff1edeaf4d)
    (cherry picked from commit 196f76b6e8b6b2d10eef8298e37f770005e00b81)

--- scripts/nixpkgs.nix
+++ scripts/nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/3a70dd92993182f8e514700ccf5b1ae9fc8a3b8d.tar.gz";
-  sha256 = "sha256:0npyj597wwrp7xyqwrkds537vs9i5gazhmaxv8wc8z0sjr1450fb";
+  url = "https://github.com/NixOS/nixpkgs/archive/933d7dc155096e7575d207be6fb7792bc9f34f6d.tar.gz";
+  sha256 = "sha256:1gcqpm7v42wfmq0wrl4dym9kg4y7n4f5wsgvisq52zr90vjvylwx";
 })
