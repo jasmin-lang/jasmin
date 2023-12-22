79381fca4cabdcf9d15a7f29b2d4c7551a6053c6
Vincent Laporte <Vincent.Laporte@inria.fr>
2023-12-22 13:53:28 +0100

    Current version is 2023.06.2

--- compiler/src/glob_options.ml
+++ compiler/src/glob_options.ml
@@ -1,6 +1,6 @@
 open Utils
 (*--------------------------------------------------------------------- *)
-let version_string = "Jasmin Compiler @VERSION@"
+let version_string = "Jasmin Compiler 2023.06.2"
 (*--------------------------------------------------------------------- *)
 let outfile = ref ""
 let latexfile = ref ""
