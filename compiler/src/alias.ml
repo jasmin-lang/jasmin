244d42d2a8c37d7607abf7dd5e92eddd2c339340
Aaron Kaiser <a_kaiser@posteo.de>
2024-04-04 09:41:48 +0200

    fix(syscalls): Tell the compiler which input references are returned by the syscall again

--- compiler/src/alias.ml
+++ compiler/src/alias.ml
@@ -178,10 +178,10 @@ let assign_arr params a x e =
 let syscall_cc (o : 'a Syscall_t.syscall_t) =
   match o with
   | Syscall_t.RandomBytes _ -> [Some 0]
-  | Syscall_t.Open _ -> [None] (** FIXME: I don't know what to put here *)
-  | Syscall_t.Close -> [None] (** FIXME: I don't know what to put here *)
-  | Syscall_t.Write _ -> [None] (** FIXME: I don't know what to put here *)
-  | Syscall_t.Read _ -> [None] (** FIXME: I don't know what to put here *)
+  | Syscall_t.Open _ -> [None]
+  | Syscall_t.Close -> [None]
+  | Syscall_t.Write _ -> [Some 0]
+  | Syscall_t.Read _ -> [Some 0]
 
 let link_array_return params a xs es cc =
   List.fold_left2 (fun a x ->
