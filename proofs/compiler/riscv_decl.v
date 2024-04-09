6d84586da8537fc09040d07e611cf38f3f616726
Jean-Christophe Léchenet <jean-christophe.lechenet@inria.fr>
2024-04-09 19:15:32 +0200

    Fixes program crash
    It looks like gp may be used to access globals. I don't know for tp, but
    to be safe I added both of them into the list of callee-saved registers.

--- proofs/compiler/riscv_decl.v
+++ proofs/compiler/riscv_decl.v
@@ -181,9 +181,11 @@ Instance riscv_decl : arch_decl register register_ext xregister rflag condt :=
   ; ad_fcp := riscv_fcp
   }.
 
+  (* It looks like the program crashes if X3 (global pointer) is not preserved.
+     To be on the safe side, X3 and X4 (thread pointer) are marked as callee-saved. *)
   Definition riscv_linux_call_conv : calling_convention :=
   {| callee_saved :=
-      map ARReg [:: X2; X8; X9; X18; X19; X20; X21; X22; X23; X24; X25; X26; X27 ]
+      map ARReg [:: X2; X3; X4; X8; X9; X18; X19; X20; X21; X22; X23; X24; X25; X26; X27 ]
    ; callee_saved_not_bool := erefl true
    ; call_reg_args  := [:: X10; X11; X12; X13; X14; X15; X16; X17 ]
    ; call_xreg_args := [::]
