38b9401fc47796ac2807abe1c2054ec3b1d9afe8
Vincent Laporte <Vincent.Laporte@inria.fr>
2024-04-30 11:22:05 +0200

    CI: update nixpkgs (feat. why3 1.7.2)

--- scripts/nixpkgs.nix
+++ scripts/nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/219951b495fc2eac67b1456824cc1ec1fd2ee659.tar.gz";
-  sha256 = "sha256:065jy7qivlbdqmbvd7r9h97b23f21axmc4r7sqmq2h0j82rmymxv";
+  url = "https://github.com/NixOS/nixpkgs/archive/576ecd43d3b864966b4423a853412d6177775e8b.tar.gz";
+  sha256 = "sha256:05gr8w09w7fqfi6zflx7df1n0hhsf8f4p9siikk3wb7k7by2d9sr";
 })
