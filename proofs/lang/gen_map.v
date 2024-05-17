f202ec4d82df333c036180555a12fe1835bee8da
Enrico Tassi <Enrico.Tassi@Inria.fr>
2024-05-17 15:27:22 +0200

    do not use SSR have in extracted code

--- proofs/lang/gen_map.v
+++ proofs/lang/gen_map.v
@@ -428,8 +428,8 @@ Module Mmake (K':CmpType) <: MAP.
     reflect (m.[kv.1] = Some kv.2) (kv \in elements m).
   Proof.
     rewrite /elements;move=> T kv m.
-    have : InA (Map.eq_key_elt (elt:=T)) (kv.1, kv.2) (Map.elements m) <->
-           (kv \in Map.elements m).
+    assert(InA (Map.eq_key_elt (elt:=T)) (kv.1, kv.2) (Map.elements m) <->
+           (kv \in Map.elements m)) as step.
     + elim: (Map.elements m) => [|x xs Hrec].
       + by rewrite in_nil; split => // /InAE.
       rewrite in_cons; split=> [| /orP [/eqP|]].
@@ -440,6 +440,7 @@ Module Mmake (K':CmpType) <: MAP.
         by move/Hrec ->; exact: orbT.
       + move => ->; constructor;case x;reflexivity.
       by move => H; apply /InA_cons_tl /Hrec.
+    revert step.
     case: (boolP (kv \in Map.elements m)) => Hin [Hf Ht];constructor.
     + move: (Ht (erefl _)).
       by move=> /(Facts.elements_mapsto_iff m kv.1 kv.2) /Facts.find_mapsto_iff.
