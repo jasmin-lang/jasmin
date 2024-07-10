bb21d5a09d6801334fdc40d8a0a7ac74e5025a84
Vincent Laporte <Vincent.Laporte@inria.fr>
2024-07-10 13:20:32 +0200

    tests: continue after an error

--- compiler/tests/sct-checker/accept.ml
+++ compiler/tests/sct-checker/accept.ml
@@ -4,16 +4,18 @@ open Sct_checker_forward
 
 let path = "success"
 
-let load_and_check name =
+let load_and_check n name =
   Format.printf "File %s:@." name;
   let p = load_file (Filename.concat path name) in
   match ty_prog Arch.is_ct_sopn p [] with
   | exception Utils.HiError e ->
       Format.eprintf "%a@." Utils.pp_hierror e;
-      exit 1
-  | r -> List.iter (Format.printf "%a@." pp_funty) r
+      n + 1
+  | r ->
+      List.iter (Format.printf "%a@." pp_funty) r;
+      n
 
 let () =
   let cases = Sys.readdir path in
   Array.sort String.compare cases;
-  Array.iter load_and_check cases
+  Array.fold_left load_and_check 0 cases |> exit
