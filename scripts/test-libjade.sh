7d07d92e101639c01de6b171264d80250c25ad4d
Vincent Laporte <Vincent.Laporte@inria.fr>
2024-08-16 10:32:17 +0200

    CI: fix libjade jobs

--- scripts/test-libjade.sh
+++ scripts/test-libjade.sh
@@ -2,14 +2,14 @@
 
 REPO=formosa-crypto
 NAME=libjade
-BRANCH=main
+BRANCH=0534bc5226dca9aa603abd590272637d13b63692
 
 FILE="$NAME.tar.gz"
-ROOT="$NAME-$BRANCH"
+ROOT=$(echo -n $NAME-$BRANCH | tr / -)
 
 [ 1 -le $# ] || exit 127
 
-DIR="$ROOT/$1"
+DIR="libjade/$1"
 
 MAKELINE="-C $DIR CI=1 JASMIN=$PWD/compiler/jasminc"
 
@@ -18,7 +18,9 @@ export EXCLUDE=""
 
 echo "Info: $MAKELINE (EXCLUDE=$EXCLUDE)"
 
-curl -v -o $FILE https://codeload.github.com/$REPO/$NAME/tar.gz/refs/heads/$BRANCH
+curl -v -o $FILE https://codeload.github.com/$REPO/$NAME/tar.gz/$BRANCH
 tar xvf $FILE
+rm -rf libjade/
+mv $ROOT libjade
 
 make $MAKELINE
