c32f84299ebfb9320af4852a8d744d08fccf85b9
Alexandre BOURBEILLON <alexandrebourbeillon@gmail.com>
2024-09-05 16:03:19 +0200

    removing unnecessary parenthesis

--- compiler/src/pretyping.ml
+++ compiler/src/pretyping.ml
@@ -2152,7 +2152,7 @@ let rec tt_item arch_info (env : 'asm Env.env) pt : 'asm Env.env =
   | S.PFundef pf -> tt_fundef arch_info env (L.loc pt) pf
   | S.PGlobal pg -> tt_global arch_info.pd env (L.loc pt) pg
   | S.Pexec   pf ->
-    Env.Exec.push (L.loc pt) (fst (tt_fun env pf.pex_name)).P.f_name (List.map (fun (x,y)-> (Z.of_string x),(Z.of_string y)) pf.pex_mem) env
+    Env.Exec.push (L.loc pt) (fst (tt_fun env pf.pex_name)).P.f_name (List.map (fun (x,y)-> Z.of_string x,Z.of_string y) pf.pex_mem) env
   | S.Prequire (from, fs) ->
     List.fold_left (tt_file_loc arch_info from) env fs
   | S.PNamespace (ns, items) ->
