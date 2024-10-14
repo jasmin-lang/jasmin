7d3697a32d9162056af0ff7e1a50e6e43992fbba
Tiago Oliveira <tfaoliveira@gmail.com>
2024-10-14 15:30:29 +0100

    DOIT: update is_doit_asm_op in x86_arch_full.ml: IMUL* to true. (#927)

--- compiler/src/x86_arch_full.ml
+++ compiler/src/x86_arch_full.ml
@@ -73,8 +73,8 @@ module X86_core = struct
     | DIV _ -> false (* Not DOIT *)
     | IDIV _ -> false (* Not DOIT *)
     | IMUL _ -> true
-    | IMULr _ -> false (* Not DOIT *)
-    | IMULri _ -> false (* Not DOIT *)
+    | IMULr _ -> true
+    | IMULri _ -> true
     | INC _ -> true
     | LEA _ -> true
     | LFENCE -> false (* Not DOIT *)
