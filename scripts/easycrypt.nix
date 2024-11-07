379462c2a854c613158c47dc31b1a06e6be4f6ba
Vincent Laporte <Vincent.Laporte@inria.fr>
2024-11-07 20:44:21 +0100

    CI: apply bug fix to EasyCrypt

--- scripts/easycrypt.nix
+++ scripts/easycrypt.nix
@@ -2,6 +2,8 @@
 , lib
 , stdenv
 , fetchFromGitHub
+, applyPatches
+, fetchpatch
 , ocamlPackages
 , python3
 , why3
@@ -19,11 +21,17 @@ with {
   "release" = rec {
     version = "2024.09";
     rev = "r${version}";
-    src = fetchFromGitHub {
-      owner = "easycrypt";
-      repo = "easycrypt";
-      inherit rev;
-      hash = "sha256-ZGYklG1eXfytRKzFvRSB6jFrOCm1gjyG8W78eMve5Ng=";
+    src = applyPatches {
+      src = fetchFromGitHub {
+        owner = "easycrypt";
+        repo = "easycrypt";
+        inherit rev;
+        hash = "sha256-ZGYklG1eXfytRKzFvRSB6jFrOCm1gjyG8W78eMve5Ng=";
+      };
+      patches = fetchpatch {
+        url = "https://github.com/EasyCrypt/easycrypt/commit/c8595b5fbb99b215f765b670ce206c235b326133.patch";
+        hash = "sha256-DpCpDzoFW/BZu5doJwM/4iSbkZ085qESUZAdqxRVK3U=";
+      };
     };
   };
 
