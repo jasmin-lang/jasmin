0fd7a3f21110f501f749219e2c809c45075f07cc
Paolo Torrini <ptorrini@sta-04020061.inria.fr>
2024-11-15 20:25:02 +0100

    updated it_lang.v

--- proofs/itrees/it_lang.v
+++ proofs/itrees/it_lang.v
@@ -59,21 +59,27 @@ From ITree Require Import Rutt RuttFacts.
 
 From ITree Require Import EqAxiom.
 
-From Jasmin Require Import expr it_lib psem_defs psem.
+From Jasmin Require Import expr it_lib psem_defs psem oseq.
 
 Import Monads.
 Import MonadNotation.
 Local Open Scope monad_scope.
+Local Open Scope option_scope.
 
 Obligation Tactic := done || idtac.
 
 (**** ERROR SEMANTICS *******************************************)
 Section Errors.
 
+
+  
 (* type of errors (this might becom richer) *)
-Variant ErrType : Type := Err : ErrType. 
+  (* Variant ErrType : Type := Err : ErrType. *)
+Notation ErrType1 := (error).
+Notation Err := (ErrType).
+
 (* error events *)
-Definition ErrState : Type -> Type := exceptE ErrType.
+Definition ErrState : Type -> Type := exceptE ErrType1.
 
 (* failT (itree E) R = itree E (option R) *)
 Definition handle_Err {E} : ErrState ~> failT (itree E) :=
@@ -127,6 +133,12 @@ Definition err_st_opt {E: Type -> Type}
                 | Some v => Ret (st, v)
                 | None => throw Err end.                
 
+Definition err_result {E: Type -> Type} 
+  `{ErrState -< E} T : result T ~> itree E :=
+  fun _ t => match t with
+             | Ok v => Ret v
+             | Error _ => throw Err end.                
+
 End Errors.
 
 (*********************************************************************)
@@ -143,7 +155,7 @@ Inductive instr_r :=
 *)
 
 Section Lang.
-Context `{asmop:asmOp}.
+Context (asm_op: Type) (asmop: asmOp asm_op).
 
 (** Events *)
 
@@ -152,7 +164,6 @@ locations associated to the function (concretely, stackframe
 locations) *)
 Variant FunE : Type -> Type :=
   | FunCode (fn : funname) : FunE cmd
-  | FunArgs (fn: funname) : FunE lvals
   | FunDests (fn: funname) : FunE lvals.                               
 
 (* neutral state events *)
@@ -162,7 +173,7 @@ Variant StE : Type -> Type :=
   | WriteVar (x: var_i) (z: Z) : StE unit                             
 (* evaluates the expressions es and copy the values to la (local
 addresses in the stack frame) *)
-  | InitState (es: pexprs) (la: lvals) : StE unit
+  | InitState (fn: funname) (es: pexprs) : StE unit
 (* copy the results in ld (local addesses in the stack frame) to gd
 (global ones) *)  
   | SetDests (ld gd: lvals) : StE unit.  
@@ -202,12 +213,18 @@ Context (Eff : Type -> Type)
 
 Definition denote_fcall (xs: lvals) (fn: funname) (es: pexprs) :
   itree (CState +' Eff) unit :=
-  la <- trigger (FunArgs fn) ;;
-  trigger (InitState es la) ;; 
+  trigger (InitState fn es) ;; 
   c <- trigger (FunCode fn) ;;   
   trigger_inl1 (LCode c) ;; 
   ld <- trigger (FunDests fn) ;; 
   trigger (SetDests ld xs).
+
+Fixpoint denote_for (i: var_i) (c: itree (CState +' Eff) unit) (ls: list Z) :
+  itree (CState +' Eff) unit :=
+    match ls with
+    | nil => ret tt
+    | (w :: ws) => trigger (WriteVar i w) ;; c ;; denote_for i c ws
+    end.
 
 Fixpoint denote_instr (i : instr_r) : itree (CState +' Eff) unit := 
   let R := cmd_map_r denote_instr in
@@ -220,18 +237,10 @@ Fixpoint denote_instr (i : instr_r) : itree (CState +' Eff) unit :=
       b <- trigger (EvalCond e) ;;
       if b then R c1 else R c2 
 
-  | Cfor i rng c =>
-      match rng with
-      | (d, lo, hi) =>
-          ITree.iter (fun _ =>
-            ITree.bind (trigger (EvalBound lo))
-            (fun vlo => ITree.bind (trigger (EvalBound hi))
-            (fun vhi =>                     
-            match (wrange d vlo vhi) with
-            | nil => ret (inr tt)
-            | (w :: ws) => trigger (WriteVar i w) ;; R c ;; ret (inl tt)
-            end))) tt
-      end
+  | Cfor i (d, lo, hi) c => 
+          vlo <- trigger (EvalBound lo) ;;
+          vhi <- trigger (EvalBound hi) ;;
+          denote_for i (R c) (wrange d vlo vhi)
         
   | Cwhile a c1 e c2 => 
       ITree.iter (fun _ =>
@@ -259,8 +268,83 @@ Definition denote_cmd1 (i: instr) : itree Eff unit :=
 
 End With_MREC.
 
+
+
+(*** FUN-READER SEMANTICS ********************************************)
+Section WSW.
+Context {wsw: WithSubWord}.
+ 
+Section FunEvents.
+  
+Context
+  (dc: DirectCall)
+  (syscall_state : Type)
+  (ep : EstateParams syscall_state)
+  (spp : SemPexprParams)
+  (sip : SemInstrParams asm_op syscall_state)
+  (pT : progT)
+  (scP : semCallParams)
+  (pr : prog)
+  (ev : extra_val_t).
+
+Definition FunDef : Type := _fundef extra_fun_t.
+
+Definition get_FunDef (fn: funname) : option FunDef :=
+  get_fundef (p_funcs pr) fn.
+
+Definition get_FunCode (fn: funname) : option cmd := 
+  opt_lift (@f_body asm_op asmop extra_fun_t) (get_FunDef fn).
+
+Definition get_FunDests (fn: funname) : option lvals :=
+  get_FunDef fn >>o= fun f => Some (map Lvar (f.(f_res))). 
+
+Definition handle_FunE {E: Type -> Type}
+  `{ErrState -< E} : FunE ~> itree E :=
+  fun _ e =>
+    match e with
+    | FunCode fn => err_opt _ (get_FunCode fn)
+    | FunDests fn => err_opt _ (get_FunDests fn) end.   
+
+
+(*******************************************************************)
+
+Variant StackE : Type -> Type :=
+  | GetTopState : StackE estate
+  | PushState (st: estate) : StackE unit. 
+
+Definition eval_Args (args: pexprs) (st: estate) :
+  result error (seq value) := 
+  sem_pexprs (~~direct_call) (p_globs pr) st args.
+
+Definition truncate_Args (f: FunDef) (vargs: seq value) :=
+  mapM2 ErrType dc_truncate_val f.(f_tyin) vargs.
+
+Definition mk_InitState E `{StackE -< E} `{ErrState -< E}
+  (f: FunDef) (args: pexprs) : itree E unit :=
+  ITree.bind (trigger GetTopState)
+      (fun st0 =>
+         let scs1 := st0.(escs) in
+         let m1 := st0.(emem) in 
+         vargs' <- err_result _ _ (eval_Args args st0) ;;
+         vargs <- err_result _ _ (truncate_Args f vargs') ;;
+         ret tt). 
+
+(*
+Definition mk_SetDests E `{StackE -< E} `{ErrState -< E}
+  (f: FunDef) (ld gd: lvals) : itree E unit :=
+  ITree.bind (trigger GetTopState)
+    (fun st0 =>
+(**)
+    get_var_is (~~ direct_call) s2.(evm) f.(f_res) = ok vres ->
+    mapM2 ErrType dc_truncate_val f.(f_tyout) vres = ok vres' ->
+    scs2 = s2.(escs) ->
+    m2 = finalize f.(f_extra) s2.(emem)  ->
+(**)
+*)       
+
+End FunEvents.
+
+End WSW.
+
 End Lang.
-
-About write_var.
-
-
+(** END *)
