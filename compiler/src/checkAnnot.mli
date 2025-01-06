2a652eab3400439bca0b2b6d76fb5ba01e0b3ed3
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-01-06 09:36:59 +0100

    Do not check for inline instr in main-compiler

--- compiler/src/checkAnnot.mli
+++ compiler/src/checkAnnot.mli
@@ -1,5 +1,2 @@
 val check_stack_size : (Expr.stk_fun_extra * _ Prog.func) list -> unit
 (** Check the stacksize, stackallocsize & stackalign annotations, if any *)
-
-val check_no_inline_instr : _ Prog.prog -> unit
-(** Check that no “inline”-annotated instruction remain. *)
