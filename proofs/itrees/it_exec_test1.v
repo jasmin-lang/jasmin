8fdd619316c9d2285475d788d0bfc594874674f0
Paolo Torrini <ptorrini@sta-04020061.inria.fr>
2025-01-16 19:56:48 +0100

    updated it_exec_test1.v; the universe problem seems to be with Basics.CategoryKleisli.v and particularly with 4 instances it contains

--- proofs/itrees/it_exec_test1.v
+++ proofs/itrees/it_exec_test1.v
@@ -1,9 +1,75 @@
 From ITree Require Import
-  Monad
-  (* 1) not importing Events.Map suffices to compile *)
-  Events.Map. 
+  Monad.
+  (* 1) not importing the following suffices to compile *)
+(*  Basics.CategoryKleisli.  *)
 
 From Jasmin Require Import sem_type utils.  
+
+(************)
+
+From Coq Require Import
+     Morphisms.
+
+From ExtLib Require Import
+     Structures.Monad.
+
+From ITree Require Import
+     Basics.Basics
+     Basics.CategoryOps
+     Basics.Function
+     Basics.Monad.
+
+Implicit Types m : Type -> Type.
+Implicit Types a b c : Type.
+
+Definition Kleisli m a b : Type := a -> m b.
+
+(* SAZ: We need to show how these are intended to be used. *)
+(** A trick to allow rewriting in pointful contexts. *)
+Definition Kleisli_arrow {m a b} : (a -> m b) -> Kleisli m a b := fun f => f.
+Definition Kleisli_apply {m a b} : Kleisli m a b -> (a -> m b) := fun f => f.
+
+Definition pure {m} `{Monad m} {a b} (f : a -> b) : Kleisli m a b :=
+  fun x => ret (f x).
+
+Section Instances.
+  Context {m : Type -> Type}.
+  Context `{Monad m}.
+  Context `{Eq1 m}.
+
+  #[global] Instance Eq2_Kleisli : Eq2 (Kleisli m) :=
+    fun _ _ => pointwise_relation _ eq1.
+
+  #[global] Instance Cat_Kleisli : Cat (Kleisli m) :=
+    fun _ _ _ u v x =>
+      bind (u x) (fun y => v y).
+
+  Definition map {a b c} (g:b -> c) (ab : Kleisli m a b) : Kleisli m a c :=
+     cat ab (pure g).
+  
+  #[global] Instance Initial_Kleisli : Initial (Kleisli m) void :=
+    fun _ v => match v : void with end.
+
+  #[global] Instance Id_Kleisli : Id_ (Kleisli m) :=
+    fun _ => pure id.
+
+(* each of these instances is critical, and cause failure *)
+  
+(*  #[global] Instance Case_Kleisli : Case (Kleisli m) sum :=
+    fun _ _ _ l r => case_sum _ _ _ l r.
+
+  #[global] Instance Inl_Kleisli : Inl (Kleisli m) sum :=
+    fun _ _ => pure inl.
+
+  #[global] Instance Inr_Kleisli : Inr (Kleisli m) sum :=
+    fun _ _ => pure inr.
+
+  #[global] Instance Iter_Kleisli `{MonadIter m} : Iter (Kleisli m) sum :=
+    fun a b => Basics.iter.
+*)
+End Instances.
+
+(*************************)
 
 (* 2) defining Eq1 (either as a class or not) locally suffices to
 compile *)
@@ -75,7 +141,7 @@ Definition exec_rel {X: Type} (R : relation X) :
    relation (exec X) := @result_rel error X R (fun x y => True). 
 
 (*** NOTE. FAILS when:
-1) Events.Map is loaded, 
+1) CategoryKleisli is loaded, 
 2) Eq1 is NOT defined locally, but comes from Monad, and 
 3) result is NOT defined locally, but comes from utils *)
 Definition foo1 E : Eq1 (@execT (itree E)) :=
