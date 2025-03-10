fc41048c9b758a4458cd9f508d5a9271af97d09d
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-03-10 08:11:23 +0100

    Disable some warnings

--- compiler/src/glob_options.ml
+++ compiler/src/glob_options.ml
@@ -207,6 +207,7 @@ let options = [
     "-wduplicatevar", Arg.Unit (add_warning DuplicateVar), " Print warning when two variables share the same name";
     "-wunusedvar", Arg.Unit (add_warning UnusedVar), " Print warning when a variable is not used";
     "-noinsertarraycopy", Arg.Clear introduce_array_copy, " Do not automatically insert array copy";
+    "-wall", Arg.Unit (set_all_warnings), " Enable all warnings";
     "-nowarning", Arg.Unit (nowarning), " Do no print warnings";
     "-color", Arg.Symbol (["auto"; "always"; "never"], set_color), " Print messages with color";
     "-help-intrinsics", Arg.Set help_intrinsics, " List the set of intrinsic operators (and exit)";
