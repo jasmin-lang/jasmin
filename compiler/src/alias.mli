eb3d69912a36289da6091b14965d801d230458d5
Jean-Christophe Léchenet <jean-christophe.lechenet@inria.fr>
2025-04-04 12:11:10 +0200

    Better OCaml code
    Co-authored-by: Benjamin Grégoire <Benjamin.Gregoire@inria.fr>

--- compiler/src/alias.mli
+++ compiler/src/alias.mli
@@ -1,10 +1,13 @@
 open Prog
 
-type kind =
-  | Range of (int * int)
-  | Align of Wsize.wsize
+type sub_slice_kind =
+  | Exact
+    (* the range is exact *)
+  | Sub of Wsize.wsize
+    (* the precise offset is not known, we remember that it is a subpart
+       and its alignment *)
 
-type slice = { in_var : var ; scope : E.v_scope; kind : kind }
+type slice = { in_var : var ; scope : E.v_scope ; range : int * int; kind : sub_slice_kind }
 
 type alias = slice Mv.t
 
