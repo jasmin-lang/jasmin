842c1776094cdf38446993e8030f093ac7496247
Jean-Christophe Léchenet <jean-christophe.lechenet@inria.fr>
2025-04-09 14:37:14 +0200

    cleaning of psem_core.v

--- proofs/lang/psem_core.v
+++ proofs/lang/psem_core.v
@@ -1,30 +1,11 @@
-(* * Jasmin semantics with “partial values”. *)
+(* * Core definitions moved out of psem.v *)
 
 (* ** Imports and settings *)
-From mathcomp Require Import ssreflect ssrfun ssrbool eqtype ssralg.
-Require Import xseq.
-Require Export array type expr gen_map low_memory warray_ sem_type sem_op_typed values varmap low_memory syscall_sem psem_defs. 
-Require Export
-  flag_combination
-  sem_params.
-Import Utf8.
-
-Local Open Scope Z_scope.
-Local Open Scope seq_scope.
-Open Scope vm_scope.
-
-(***** Contains definition that have been moved out of psem.v *)
+From mathcomp Require Import ssreflect ssralg.
+Require Export expr sem_type syscall psem_defs sem_params.
 
 Section WSW.
 Context {wsw:WithSubWord}.
-
-(*
-Notation "'Let' ( n , t ) ':=' wdb ',' s '.[' v ']' 'in' body" :=
-  (@on_arr_var _ (get_var wdb s.(evm) v) (fun n (t:WArray.array n) => body)) (at level 25, s at level 0).
-
-Notation "'Let' ( n , t ) ':=' wdb ',' gd ',' s '.[' v ']' 'in' body" :=
-  (@on_arr_var _ (get_gvar wdb gd s.(evm) v) (fun n (t:WArray.array n) => body)) (at level 25, gd at level 0, s at level 0).
-*)
 
 Class semCallParams
   {syscall_state : Type}
@@ -59,23 +40,9 @@ Class DirectCall := {
 Definition indirect_c : DirectCall := {| direct_call := false |}.
 Definition direct_c : DirectCall := {| direct_call := true |}.
 
-Section SEM0.
-Context
-  {dc:DirectCall}
-  {asm_op syscall_state : Type}
-  {ep : EstateParams syscall_state}
-  {spp : SemPexprParams}
-  {sip : SemInstrParams asm_op syscall_state}
-  {pT : progT}
-  {scP : semCallParams}
-  (P : prog)
-  (ev : extra_val_t).
-
-Definition dc_truncate_val t v :=
+Definition dc_truncate_val {dc:DirectCall} t v :=
   if direct_call then ok v
   else truncate_val t v.
-
-End SEM0.
 
 
 Section SEM_CALL_PARAMS.
@@ -83,7 +50,6 @@ Section SEM_CALL_PARAMS.
 Context
   {asm_op syscall_state : Type}
   {ep : EstateParams syscall_state}
-  {spp : SemPexprParams}
   {sip : SemInstrParams asm_op syscall_state}.
 
 (* ** Semantic without stack
@@ -124,4 +90,3 @@ Instance sCP_stack : semCallParams (pT := progStack) :=
 End SEM_CALL_PARAMS.
 
 End WSW.
-
