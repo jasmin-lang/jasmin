3029fc10ce81aa67df044b79aaf34a5c67f9008e
Benjamin Gregoire <Benjamin.Gregoire@inria.fr>
2025-04-09 09:04:27 +0200

    add a sedscript file

--- compiler/scripts/replace-new-memory-syntax.sh
+++ compiler/scripts/replace-new-memory-syntax.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 for f in $(find . -name "*.jazz" -o -name "*.jinc")
 do
-  sed -i 's/( *\(u[0-9]*\) *) *\[ *#aligned/[#aligned:\1/g' "$f"
-  sed -i 's/( *\(u[0-9]*\) *) *\[ *#unaligned/[#unaligned:\1/g' "$f"
-  sed -i 's/( *\(u[0-9]*\) *) *\[/[:\1 /g' "$f"
-  sed -i 's/\[ *#aligned *\(u[0-9]*\)/[#aligned:\1/g' "$f"
-  sed -i 's/\[ *#unaligned *\(u[0-9]*\)/[#unaligned:\1/g' "$f"
-  sed -i 's/\[ *\(u[0-9]*\)/[:\1/g' "$f"
+  sed -i -f $(dirname $0)/replace-new-memory-syntax-sedscript "$f"
 done
 
