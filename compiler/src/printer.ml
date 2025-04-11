d9b5bfc2b25a97c1bd273ae4741eefec22e78c82
Léo Juguet <leo.juguet@crans.org>
2025-04-11 14:07:17 +0200

    Add option to print argument alignment
    Add option `-print-export-info` to print
    interresting information of export functions.
    For the moment it only prints the alignment of
    the arguments
    
    (cherry picked from commit b17e2af4f5754fc740547a15f97cda0844526e8c)

--- compiler/src/printer.ml
+++ compiler/src/printer.ml
@@ -432,8 +432,9 @@ let pp_sprog ~debug pd asmOp fmt ((funcs, p_extra):('info, 'asm) Prog.sprog) =
   let pp_opn = pp_opn pd asmOp in
   let pp_var = pp_var ~debug in
   let pp_f_extra fmt f_extra =
-    Format.fprintf fmt "(* @[<v>alignment = %s; stack size = %a + %a; max stack size = %a;@ max call depth = %a;@ saved register = @[%a@];@ saved stack = %a;@ return_addr = %a@] *)"
+    Format.fprintf fmt "(* @[<v>alignment = %s; argument alignment = [%a];@ stack size = %a + %a; max stack size = %a;@ max call depth = %a;@ saved register = @[%a@];@ saved stack = %a;@ return_addr = %a@] *)"
       (string_of_ws f_extra.Expr.sf_align)
+      (pp_list ", " pp_wsize) (f_extra.Expr.sf_align_args)
       Z.pp_print (Conv.z_of_cz f_extra.Expr.sf_stk_sz)
       Z.pp_print (Conv.z_of_cz f_extra.Expr.sf_stk_extra_sz)
       Z.pp_print (Conv.z_of_cz f_extra.Expr.sf_stk_max)
