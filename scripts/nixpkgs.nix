633942177dc1a04f10f05161b93e6fe71c668503
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-06-02 14:52:28 +0200

    Update nixpkgs

--- scripts/nixpkgs.nix
+++ scripts/nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/5d736263df906c5da72ab0f372427814de2f52f8.tar.gz";
-  sha256 = "sha256:1cvxfj03xhakyrrz8bh4499vz5d35ay92575irrbmydcxixsrf3w";
+  url = "https://github.com/NixOS/nixpkgs/archive/a59eb7800787c926045d51b70982ae285faa2346.tar.gz";
+  sha256 = "sha256:1dhllxcvq7lkscg4xrvlpbfwa4x8b6mqkx6idcb8m0jnfbccdj5b";
 })
