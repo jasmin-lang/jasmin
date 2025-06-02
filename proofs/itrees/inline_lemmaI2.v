c8ec57de37ae98d1fd14f51e8a9f6762236f8729
ptorrx <ptorrx@gmail.com>
2025-06-02 20:09:10 +0200

    updated inline_lemmaI2.v

--- proofs/itrees/inline_lemmaI2.v
+++ proofs/itrees/inline_lemmaI2.v
@@ -21,9 +21,10 @@ From ITree Require Import
      Interp.Interp
      Interp.InterpFacts
      Interp.Recursion
-     Interp.RecursionFacts.
+     Interp.RecursionFacts
+     Interp.TranslateFacts.
      
-Require Import xrutt xrutt_facts tfam_iso.
+Require Import xrutt xrutt_facts rutt_extras tfam_iso.
 
 From ITree Require Import Rutt RuttFacts.
 
@@ -100,8 +101,12 @@ Qed.
 
 End GEN_MREC.
 
-Definition lassoc_tr E1 E2 E3 := @mfun1 (E1 +' (E2 +' E3)) ((E1 +' E2) +' E3)
-                                        (@FIsoLAssoc E1 E2 E3).
+Definition lassoc_tr E1 E2 E3 : (E1 +' (E2 +' E3)) ~> ((E1 +' E2) +' E3) :=
+  fun T e => match e with
+             | inl1 e1 => inl1 (inl1 e1)
+             | inr1 e23 => match e23 with
+                           | inl1 e2 => inl1 (inr1 e2)
+                           | inr1 e3 => inr1 e3 end end.                  
 
 Definition rassoc_tr E1 E2 E3 := @mfun2 (E1 +' (E2 +' E3)) ((E1 +' E2) +' E3) 
                                         (@FIsoLAssoc E1 E2 E3).
@@ -115,21 +120,10 @@ Context {D1 D2: Type -> Type} (E : Type -> Type).
 (* inline (non-recursive) handler *) 
 Context (ctxI: D1 ~> itree (D2 +' E)).
 
-(* general semantic (recursive) handler, specialized to non-inlined
-   calls *)
+(* general semantic (recursive) handler *)
 Context (ctxR: D2 ~> itree (D2 +' E)).
 
-(* combined handler (corresponds to the actual general semantic
-   handler, which handle both inline and non-inline calls) *)
-(*
-Definition ctxIR': forall E, (D1 +' D2) ~> itree ((D1 +' D2) +' E) :=
-  fun E T d => match d with
-               | inl1 d1 => translate (@lassoc_tr D1 D2 E)
-                                      (translate inr1 (@ctxI E T d1))
-               | inr1 d2 => translate (@lassoc_tr D1 D2 E)
-                                      (translate inr1 (@ctxR E T d2)) end.
-*)
-
+(* function to compbine handlers *)
 Definition joint_handler (h1: D1 ~> itree (D2 +' E))
                          (h2: D2 ~> itree (D2 +' E)) :
                          (D1 +' D2) ~> itree ((D1 +' D2) +' E) :=
@@ -139,29 +133,19 @@ Definition joint_handler (h1: D1 ~> itree (D2 +' E))
              | inr1 d2 => translate (@lassoc_tr D1 D2 E)
                                       (translate inr1 (h2 T d2)) end.
 
+(* combined handler *)
 Notation ctxIR := (joint_handler ctxI ctxR).
 
-(*****)
-
+(* function to extend handlers *)
 Definition ext_handler {E1 E2} (h: E1 ~> itree E2) : (E1 +' E2) ~> itree E2 :=
   fun T e => match e with
              | inl1 e1 => h _ e1
              | inr1 e2 => trigger e2 end.               
 
-(*
-Definition inline_interp' T (t: itree (D1 +' (D2 +' E0)) T) :
-  itree (D2 +' E0) T := interp (ext_handle (@ctxI E0)) t.
-
-Definition inline_interp T (t: itree ((D1 +' D2) +' E0) T) :
-  itree (D2 +' E0) T := inline_interp' (translate (@rassoc_tr D1 D2 E0) t).
-*)
-
+(* non-recursive interpreter for inlining *)
 Definition D1_ext_interp (h: D1 ~> itree (D2 +' E))
   T (t: itree ((D1 +' D2) +' E) T) : itree (D2 +' E) T :=
   interp (ext_handler h) (translate (@rassoc_tr D1 D2 E) t).
-
-
-(****)
 
 (* extended combined handler *)
 Definition ctxIRX : (D1 +' D2) +' E ~> itree ((D1 +' D2) +' E) :=
@@ -169,13 +153,29 @@ Definition ctxIRX : (D1 +' D2) +' E ~> itree ((D1 +' D2) +' E) :=
              | inl1 d => ctxIR d
              | inr1 e' => trigger e' end.                      
 
-Definition lw_la: (D2 +' E) ~> ((D1 +' D2) +' E) :=
+
+(*********************************************************************)
+
+Definition lw_la1: (D2 +' E) ~> ((D1 +' D2) +' E) :=
   fun _ d => match d with
              | inl1 d' => inl1 (inr1 d')
              | inr1 e => inr1 e end.
 
+Lemma lw_la1_test_lemma1 T (e: (D2 +' E) T) : lw_la1 e = lassoc_tr (inr1 e).
+  destruct e; simpl; eauto.
+Qed.  
+
+Lemma lw_la1_test_lemma2 T (d: D2 T) : lw_la1 (inl1 d) = inl1 (inr1 d).
+  unfold lw_la1; auto.
+Qed.  
+
+Definition lw_la T (e: (D2 +' E) T) := @lassoc_tr D1 D2 E _ (inr1 e).
+
+(* extend inessentially the event type (inessential = no events for
+   that type) *)
 Definition free_tr: itree (D2 +' E) ~> itree ((D1 +' D2) +' E) :=
   translate lw_la.
+
 
 (** Inline-specific proofs *)
 
@@ -237,16 +237,6 @@ Proof.
   setoid_rewrite ctxIR_ok1.
   setoid_rewrite interp_translate; reflexivity.
 Qed.
-(*
-  generalize (ctxI d1).
-  ginit; gcofix CIH; intro t1.
-  setoid_rewrite (itree_eta t1).
-  destruct (observe t1) eqn: was_t1.
-  { gstep; red; simpl; reflexivity. }
-  {  gstep; red; simpl; econstructor. gfinal; left. eapply CIH. }
-  { gstep; red; try reflexivity. }
-Qed.  
-*)
 
 Lemma D2_inline_lemma T (d2: D2 T) :
     interp_mrec ctxIR (ctxIRX (inl1 (inr1 d2)))
@@ -270,7 +260,7 @@ Proof.
 Qed.
 
 (* inlining lemma: basically, eutt (sem (inline t)) (sem t) *)
-Lemma OK_inline_lemma T (t: itree ((D1 +' D2) +' E) T) :
+Lemma OK_wide_inline_lemma T (t: itree ((D1 +' D2) +' E) T) :
   eutt eq (interp_mrec ctxIR
              (translate lw_la (@D1_ext_interp ctxI _ t)))
           (interp_mrec ctxIR t).
@@ -317,4 +307,164 @@ Qed.
 End InlineOK.
 
 
+Lemma strong_interp_vis {F0 F R} {f : F0 ~> itree F} U
+  (e: F0 U) (k: U -> itree F0 R) :
+  (interp f (Vis e k)) = (ITree.bind (f _ e) (fun x => Tau (interp f (k x)))).
+Proof.
+  eapply bisimulation_is_eq.
+  eapply interp_vis; eauto.
+Qed.  
+
+
+Section InlineOK1.
+
+(* D1: inline events and recursive events; E0: other events. *)  
+Context {D1: Type -> Type} (E : Type -> Type).
+
+(* inline (non-recursive) handler *) 
+Context (ctxI: D1 ~> itree (D1 +' E)).
+
+(* general semantic (recursive) handler *)
+Context (ctxR: D1 ~> itree (D1 +' E)).
+
+(* combined handler *)
+Notation ctxIR := (joint_handler ctxI ctxR).
+
+Definition forget_f : (D1 +' D1) +' E ~> D1 +' E :=
+  fun T d => match d with
+             | inl1 d1 => match d1 with
+                          | inl1 d11 => inl1 d11
+                          | inr1 d2 => inl1 d2 end                
+             | inr1 e => inr1 e end.               
+
+(* forget some events *)
+Definition forget_tr : itree ((D1 +' D1) +' E) ~> itree (D1 +' E) :=
+  translate forget_f.
+
+(* forgetting an inessential extension gives us back the same *)
+Lemma forget_free_inv_lemma T (t: itree (D1 +' E) T) :
+  eq_itree eq t (forget_tr (free_tr t)).
+Proof.
+  revert t.
+  ginit; gcofix CIH.
+  intros t.
+  unfold free_tr, forget_tr.
+  rewrite (itree_eta t).
+  remember (observe t) as ot.
+  destruct ot.
+  { gstep; red; simpl; econstructor; auto. }
+  { gstep; red; simpl; econstructor. gfinal; left; auto. }
+  { gstep; red. simpl. unfold forget_f, lw_la; simpl. destruct e; simpl.
+    econstructor.
+    intros v; simpl. unfold id.
+    gfinal; left; eauto.
+
+    econstructor.
+    intros v; simpl. unfold id.
+    gfinal; left; eauto.
+  }
+Qed.
+
+(* recursion on D1 is the same as recursion on (D1 +' D1) where the
+   left D1 is inessential *)
+Lemma free_widening_lemma
+  T (t: itree (D1 +' E) T) :
+  eutt eq (interp_mrec ctxR t) (interp_mrec ctxIR (free_tr t)).
+Proof.
+  revert t.
+  ginit; gcofix CIH.
+  intros t.
+  setoid_rewrite (itree_eta t).
+  setoid_rewrite unfold_interp_mrec.
+  simpl.
+  remember (observe t) as ot1.
+  destruct ot1; simpl; intros.
+  { gstep; red; simpl. econstructor; auto. }
+  { gstep; red. simpl.
+    econstructor.
+    gfinal; left.
+    eapply CIH.
+  }
+  { gstep; red. simpl.
+    destruct e eqn: was_e; cbn.
+
+    { eapply Eqit.EqTau.
+      gfinal; left.
+    
+      assert (eq_itree eq (x <- @free_tr D1 D1 E _ (ctxR d);; free_tr (k x))
+              (free_tr (x <- ctxR d;; k x))) as UU.
+      { unfold free_tr.
+        rewrite translate_bind. reflexivity. }
+
+      assert ((x <- @free_tr D1 D1 E _ (ctxR d);; free_tr (k x)) =
+                (free_tr (x <- ctxR d;; k x))) as UU1.
+      { eapply bisimulation_is_eq; eauto. }
+
+      set (ZZ := 
+        (@ITree.bind (sum1 (sum1 D1 D1) E) X T
+           (@translate (sum1 D1 (sum1 D1 E)) (sum1 (sum1 D1 D1) E)
+             (@lassoc_tr D1 D1 E) X
+             (@translate (sum1 D1 E) (sum1 D1 (sum1 D1 E))
+                (@inr1 D1 (sum1 D1 E)) X (@ctxR X d)))
+          (fun x : X => @free_tr D1 D1 E T (k x)))).
+
+      set (ZZ1 := 
+           (x <- @free_tr D1 D1 E _ (ctxR d) ;; free_tr (k x))).
+
+      assert (eq_itree eq ZZ ZZ1) as WW.
+      { subst ZZ ZZ1. unfold free_tr, lw_la. simpl. unfold lassoc_tr.
+        setoid_rewrite <- translate_cmpE. reflexivity.
+      }
+
+      assert (ZZ = ZZ1) as WW1.
+      { eapply  bisimulation_is_eq; eauto. }
+      setoid_rewrite WW1.
+      subst ZZ1.
+
+      setoid_rewrite UU1.
+      eapply CIH.
+    }
+    { econstructor.
+      intros v. unfold id.
+      gstep; red. simpl.
+      econstructor.
+      gfinal; left.
+      eapply CIH.
+    }
+  }  
+Qed.
+
+Lemma OK_strict_inline_lemma_new T (t: itree ((D1 +' D1) +' E) T) :
+  eutt eq (interp_mrec ctxR (@D1_ext_interp D1 D1 E ctxI _ t))
+          (interp_mrec ctxIR t).
+  rewrite free_widening_lemma.
+  unfold D1_ext_interp.
+  repeat (rewrite interp_mrec_as_interp).
+  unfold free_tr.
+  repeat (rewrite interp_translate).
+  rewrite interp_interp.
+  eapply eutt_interp; simpl; try reflexivity.
+  intros T0 e.
+  clear t. clear T.
+  destruct e as [d12 | e] ; simpl; try reflexivity.
+  2: { rewrite interp_trigger. reflexivity. }
+  
+  rewrite mrec_as_interp.
+  destruct d12 as [d1 | d2]; simpl; try reflexivity.
+
+  { rewrite interp_translate.
+    rewrite interp_translate.
+    eapply eutt_interp; simpl; try reflexivity. }
+
+  { repeat (rewrite interp_translate).
+    rewrite interp_trigger; simpl.
+    rewrite mrec_as_interp.
+    unfold resum, ReSum_inr, ReSum_id, cat; simpl; unfold resum, id_; simpl;
+      unfold Id_IFun. 
+    repeat (rewrite interp_translate).
+    eapply eutt_interp; simpl; try reflexivity.
+  }
+Qed.   
+
+End InlineOK1.
 
