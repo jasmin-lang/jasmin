eddd9efdf3c6ffac3e75662201ba50e51d5f0548
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-06-25 07:00:58 +0200

    CI: update nixpkgs & docker image
    (cherry picked from commit 818a973cc4d1c0243ee8af92cee6c3af7789ed41)

--- scripts/nixpkgs.nix
+++ scripts/nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/72783634908feefddf58d34a0352fe2238e9541c.tar.gz";
-  sha256 = "sha256:18mp9lwbh98148rr96cz45mwavb14wx7a7yh01ffdyqj6yfpsngd";
+  url = "https://github.com/NixOS/nixpkgs/archive/dee6c49f2361b678b4f2c868081989ac92279f6a.tar.gz";
+  sha256 = "sha256:11c9b68gbr06s1czvhfnzdmix9fzyjbg76xnrhfgympm4clykn9b";
 })
