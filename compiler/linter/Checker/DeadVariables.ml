4a03f7982c73ed9d926eb05cfc6f7265bae5d3e2
Alexandre BOURBEILLON <alexandrebourbeillon@gmail.com>
2025-07-08 13:47:53 +0200

    correcting issues with this approach

--- compiler/linter/Checker/DeadVariables.ml
+++ compiler/linter/Checker/DeadVariables.ml
@@ -14,28 +14,46 @@ let create_dv_error err_payload loc =
       );
   }
 
+let create_dv_error_instr (variables) loc =
+  let open CompileError in
+  {
+    location = loc;
+    error_strategy = CompileError.Fail;
+    code = "DV-E002";
+    to_text =
+      (
+        fun fmt ->
+          Format.fprintf fmt "Instruction contains at least one unused variables. %a " 
+          (Format.pp_print_list ~pp_sep:(fun fmt () -> Format.fprintf fmt ",") (Printer.pp_var ~debug:false))variables
+        );
+  }
 
 let check_func func =
-  let errors = ref [] in
+  let dv_errors = ref [] in
+  let dv_errors_instr = ref [] in
 
   let check_instr ({ i_desc; i_info; i_loc; _}:('info,'asm) Prog.instr) =
     let domain = Annotation.unwrap i_info in
-    let dead_variables = Sv.diff (Jasmin.Prog.assigns i_desc) domain in
-    if (Sv.is_empty (Sv.diff (Jasmin.Prog.assigns i_desc) dead_variables)) then (* We check if at least one lvalue is alive. If then, we do not create an error*)
+    let assigns = Jasmin.Prog.assigns i_desc in
+    let dead_variables = Sv.diff assigns domain in
+    if (Sv.is_empty (Sv.inter assigns domain)) then (* We check if at least one lvalue is alive. If then, we do not create an error*)
       Sv.iter (fun v ->
         let err_payload= create_dv_error v (i_loc.base_loc) in
-        errors := err_payload :: !errors
-      ) dead_variables
+        dv_errors := err_payload :: !dv_errors
+      ) dead_variables;
+    if (not (Sv.is_empty dead_variables)) then
+      let err_payload = create_dv_error_instr (Sv.elements dead_variables) (i_loc.base_loc) in
+      dv_errors_instr := err_payload :: !dv_errors_instr
   in
 
   iter_instr check_instr func.f_body ;
-  !errors
+  !dv_errors, !dv_errors_instr
 
 let check_prog (_,funcs) =
   List.fold (
-    fun acc fd ->
-      let errors = check_func fd in
-      List.rev_append errors acc
+    fun (ev,ei) fd ->
+      let errors_var, errors_instr = check_func fd in
+      (List.rev_append  errors_var ev), (List.rev_append errors_instr ei)
   )
-  []
+  ([],[])
   funcs
