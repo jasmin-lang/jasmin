0df5567917cdb63b88b758b19bb4646ce77f11c7
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-09-08 13:47:28 +0200

    Linter: refactoring in Reaching Definition analysis
    The “strict” mode is removed.

--- compiler/linter/Analysis/ReachingDefinitions/RDDomain.ml
+++ compiler/linter/Analysis/ReachingDefinitions/RDDomain.ml
@@ -1,27 +1,22 @@
 open Jasmin
+open Utils
 open Prog
-open Format
 
-module SIloc = Iloc.SIloc
+type t = Siloc.t Mv.t
 
-type t = SIloc.t Mv.t
 let empty : t = Mv.empty
-
-let add xs i = Sv.fold (fun x -> Mv.add x (SIloc.singleton i)) xs
-
-let join = Mv.union (fun _ a b -> Some (SIloc.union a b))
+let add xs i = Sv.fold (fun x -> Mv.add x (Siloc.singleton i)) xs
+let join = Mv.union (fun _ a b -> Some (Siloc.union a b))
 
 let included (x : t) (y : t) =
-    Mv.for_all (fun x s1 -> SIloc.subset s1 (Mv.find_default SIloc.empty x y)) x
+  Mv.for_all (fun x s1 -> Siloc.subset s1 (Mv.find_default Siloc.empty x y)) x
 
 let forget x = Mv.remove x
 
 let pp fmt ((_, d) : Location.i_loc * t) =
-    Mv.iter
-      (fun k v ->
-        Format.fprintf fmt "%s : %a@." k.v_name
-          (
-            pp_print_seq ~pp_sep:(fun fmt () -> Format.fprintf fmt ",") Iloc.pp)
-            (SIloc.to_seq v)
-          )
-      d
+  Mv.iter
+    (fun k v ->
+      Format.fprintf fmt "%s : %a@." k.v_name
+        (pp_list ", " Location.pp_iloc)
+        (Siloc.elements v))
+    d
