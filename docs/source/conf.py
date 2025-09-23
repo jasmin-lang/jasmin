8e447bb62a2917385c633260fc17bd8557b21a99
Jean-Christophe Léchenet <jean-christophe.lechenet@inria.fr>
2025-09-23 13:09:47 +0200

    doc: tweak the version switcher (#1287)
    - no duplication between stable and the current stable tag
    - changing version preserves the current path in the doc
    
    (cherry picked from commit e5221a8d28db4f3b470d65d0af8bf6149a0b2042)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,4 +38,4 @@ html_context = {
     "github_version": "main",  # Version
     "conf_py_path": "/docs/source/",  # Path in the checkout to the docs root
 }
-# html_static_path = ['_static']
+html_static_path = ['_static']
