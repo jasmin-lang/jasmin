db77da98efa33268e07413170672e4efa1fdd92c
Vincent Laporte <Vincent.Laporte@inria.fr>
2025-09-29 08:31:43 +0200

    exit Alias.analyze_prog
    (cherry picked from commit 8cfe0a653e4725afbab15a5816c389b3b2584309)

--- compiler/src/alias.mli
+++ compiler/src/alias.mli
@@ -15,8 +15,6 @@ val normalize_var : alias -> var -> slice
 
 val analyze_fd : (funname -> int option list) -> (int, 'a, 'asm) Prog.gfunc -> alias
 
-val analyze_prog : ('info, 'asm) func list -> unit
-
 val classes : alias -> Sv.t Mv.t
 
 val pp_slice : Format.formatter -> slice -> unit
