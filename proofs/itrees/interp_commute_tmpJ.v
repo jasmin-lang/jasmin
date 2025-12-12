0e7f943ed9388146330710ba8ec0fa70a3fafa41
ptorrx <ptorrx@gmail.com>
2025-12-12 17:00:02 +0100

    changes in interp_commute_tmp1.v and interp_commute_tmpJ.v

--- proofs/itrees/interp_commute_tmpJ.v
+++ proofs/itrees/interp_commute_tmpJ.v
@@ -1,7 +1,10 @@
+(* Yannick's proof file *)
+
 Require Import Equality.
 (* Require Import Stdlib.Program.Equality. *)
 From ITree Require Import
-  ITree Eq State Handler RecursionFacts InterpFacts StateFacts KTreeFacts.
+  ITree Eq State Handler RecursionFacts InterpFacts StateFacts KTreeFacts
+  TranslateFacts.
 Import Monads.
 Import ITreeNotations.
 #[local] Open Scope itree_scope.
@@ -46,35 +49,13 @@ Section foo.
   refine (fun T e σ => translate _ (h e σ)).
   intros ? f; destruct f.
   Defined.
-
-  (*
-  set (XX := (fun T1 (e: S T1) σ => translate
-    (fun (T0 : Type) (f : void1 T0) =>
-     match f in (void1 T1) return (F' T1) with
-     end) (h e σ))).
-  *)
-      
-From ITree Require Import TranslateFacts.
-  
-  Print h'.
-  Lemma xxx T (e: S T) s : eutt eq (h' e s) (h' e s).
-    unfold h'.
-    set (XX := translate
-    (fun (T0 : Type) (f : void1 T0) =>
-     match f in (void1 T1) return (F' T1) with
-     end) (h e s)).
-Abort.  
-  
+        
   Definition C : IT (F +' S) ~> SIT F'.
     refine (interp (case_ _ h')).
     refine (fun _ f σ => trigger (call' f σ)).
   Defined.
 
   Definition D : SIT F' ~> SIT void1.
-(*    intros.
-    intro s.
-    specialize (X s).
- *)   
   refine (fun T t σ =>
             ITree.iter
               (fun t => match observe t with
@@ -90,13 +71,6 @@ Abort.
                      | inr1 e => h' e σ'
                      end).
   Defined.
-
-  (*
-Lemma xxx T (t: Σ -> itree F' (Σ * T)) s : eutt eq (D t s) (D t s). 
-  unfold D. simpl.
-  set (XX := fun t0 => _).
-*)  
-
   
   Definition interp2 : IT (F +' S) ~> SIT void1 := fun _ t => D (C t).
   Definition top2 : F ~> SIT void1 := fun _ f => interp1 (ctx f).
@@ -133,24 +107,6 @@ Definition RU1 : forall T, ((Σ * IT (fun H : Type => S H) T + Σ * T) ->
                           x = (inl x1) /\ y = (inl y1) /\
                             RU_L x1 y1.
 
-(*
-Definition Rel_LLift G1 G2 (Rel: G1 -> G2 -> Prop) G3 :
-  (G1 + G3) -> (G2 +  
-
-Context (RU : forall T, ((Σ * IT (fun H : Type => S H) T + Σ * T) -> 
-           (IT F' (Σ * T) + Σ * T) -> Prop)).
-
-Context (RU_L : forall T, ((Σ * IT (fun H : Type => S H) T) -> 
-           (IT F' (Σ * T)) -> Prop)).
-
-Definition RU1 : forall T, ((Σ * IT (fun H : Type => S H) T + Σ * T) -> 
-                            (IT F' (Σ * T) + Σ * T) -> Prop) :=
-  fun T x y => exists (x1: (Σ * IT (fun H : Type => S H) T))
-                      (y1: IT F' (Σ * T)),
-                          x = (inl x1) /\ y = (inl y1) /\
-                               RU_L x1 y1.     
-*)
-
 Theorem diagram_commutes : forall T (t : IT _ T) σ,
       interp1 t σ ≈ interp2 t σ.
 Proof.
@@ -167,115 +123,13 @@ Proof.
     gstep; red.
     reflexivity.
   }
-  { unfold interp1, interp2, A, B, C, D.    
+  { unfold interp1, interp2, A, B, C, D.
+    Fail setoid_rewrite interp_mrec_as_interp.
     setoid_rewrite unfold_interp_mrec; simpl.
-    setoid_rewrite unfold_interp_state at 1. simpl.
-    unfold interp at 1.
-    unfold Basics.iter, MonadIter_stateT0, Basics.iter, MonadIter_itree.
-    setoid_rewrite unfold_iter.
-    guclo eqit_clo_bind.
-    econstructor. simpl.
-    setoid_rewrite bind_ret_l.
-    simpl.
-    pstep; red.
-    econstructor.
-    instantiate (1 := @RU1 T).
-    unfold RU1; simpl.
-    repeat eexists.
-    
-    
-    eapply eutt_iter''.
-    
-    
-(*    unfold interp1, interp2, A, B, C, D. simpl.
-    intros.
-    set (XX := interp (case_ _ _) _).
-    set (YY := interp h _). *)
-    einit.    
-    intros ?.
-    ecofix r.
-    intros t σ.
-    unfold interp1, interp2, A, B, C, D.
-
-    match goal with
-      |- euttG _ _ _ _ _ _ ?t => set t end.
-    rewrite (itree_eta t).
-
-    subst i.
-    match goal with
-      |- euttG _ _ _ _ _ ?t (ITree.iter ?foo _) => set t; set foo end.
-
-(*   Set Printing Implicit. *)
-    
-   cut ( @euttG void1 (Σ * T) (Σ * T) (@eq (Σ * T))
-    (@bot2 (IT void1 (Σ * T)) (fun _ : IT void1 (Σ * T) => IT void1 (Σ * T)))
-    (@bot2 (IT void1 (Σ * T)) (fun _ : IT void1 (Σ * T) => IT void1 (Σ * T)))
-    gL' gH' i
-    (@ITree.iter void1 (Σ * T) (IT F' (Σ * T)) i0
-       (@interp (F +' S) (SIT F')
-          (@Functor_stateT (IT F') Σ (@Functor_itree F'))
-          (@Monad_stateT (IT F') Σ (@Monad_itree F'))
-          (@MonadIter_stateT0 (IT F') Σ (@Monad_itree F')
-             (@MonadIter_itree F'))
-          (@case_ (Type -> Type) IFun sum1 Case_sum1 F S
-             (fun T0 : Type => Σ -> IT F' (Σ * T0))
-             (fun (T0 : Type) (f : F T0) (σ0 : Σ) => trigger (@call' T0 f σ0))
-             h') T {| _observe := observe t |} σ))).
-   intro K.
-   admit.
-
-   subst i.
-   remember (observe t) as ot.
-   dependent induction ot; simpl in *.
-   { setoid_rewrite unfold_interp_mrec; simpl.
-     setoid_rewrite interp_state_ret_Eq; simpl.
-     setoid_rewrite unfold_iter; simpl.
-     setoid_rewrite bind_ret_l.
-     reflexivity.
-   }
-   { setoid_rewrite unfold_interp_mrec; simpl.
-     unfold interp at 1.
-     unfold Basics.iter.
-     unfold MonadIter_stateT0.
-     unfold Basics.iter.
-     unfold MonadIter_itree.
-     set (XX := (fun si : Σ * IT (fun H : Type => S H) T =>
-        Monad.bind
-          (match observe (snd si) with
-           | RetF r => Monad.ret (inr r)
-           | TauF t1 => Monad.ret (inl t1)
-           | @VisF _ _ _ X e k => Functor.fmap (fun x : X => inl (k x)) (h e)
-           end (fst si))
-          (fun si' : Σ * (IT (fun H : Type => S H) T + T) =>
-           Monad.ret
-             match snd si' with
-             | inl i' => inl (fst si', i')
-             | inr r => inr (fst si', r)
-             end))).
-     eapply eutt_iter''.
-
+    Fail setoid_rewrite interp_state_tau.
+    setoid_rewrite unfold_interp_state at 1; simpl.
+  (*  Fail setoid_rewrite unfold_interp_state. *)
      
-     setoid_rewrite interp_state_tau_Eq; simpl.
-     rewrite unfold_iter at 1. simpl.
-     setoid_rewrite iter_tau.
-   
-
-
-       euttG eq bot2 bot2 gL' gH' i
-    (ITree.iter i0
-       (interp
-          (case_ (fun (T0 : Type) (f : F T0) (σ0 : Σ) => trigger (call' f σ0))
-             h') t (* {| _observe := observe t |} *) σ))).
-
-    
-    setoid_rewrite (itree_eta_ t).
-    
-    subst i.
-
-
-
-    remember (observe t) as ot.
-    dependent destruction ot.
-    { simpl; simpl in *.
-      
+(*    unfold interp at 1.
+    setoid_rewrite unfold_iter; simpl. *)
     
