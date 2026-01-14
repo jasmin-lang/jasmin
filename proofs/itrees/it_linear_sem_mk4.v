a36dcc0dd64ef505360529826eb6c291ccc64a93
ptorrx <ptorrx@gmail.com>
2026-01-14 16:37:40 +0100

    updated it_linear_sem_mk4.v, mainly comment

--- proofs/itrees/it_linear_sem_mk4.v
+++ proofs/itrees/it_linear_sem_mk4.v
@@ -60,16 +60,7 @@ Context (fenv: FEnv).
 (* point in the linear code of a function *)
 Definition lcpoint : Type := (funname * nat)%type.
 
-(* the stack *)
-Definition alstate := list lcpoint.
-
-(* not used *)
-Definition lcpoint2remote (l: lcpoint) : remote_label :=
-  match l with (fn, n) => (fn, Pos.of_nat n) end. 
-(* not used *)
-Definition remote2lcpoint (l: remote_label) : lcpoint :=
-  match l with (fn, l) => (fn, Pos.to_nat l) end. 
-    
+(* these events represent the actual linear semantics *)
 Variant LinstrE : Type -> Type :=
   | ELopn        : lexprs -> sopn -> rexprs -> LinstrE unit
   | ELsyscall    : syscall_t -> LinstrE unit
@@ -82,6 +73,9 @@ Variant LinstrE : Type -> Type :=
   | ELstoreLabel : var -> label -> LinstrE unit
   | ELcond       : fexpr -> label -> LinstrE unit.
 
+(* these events are used to provide a dataflow abstraction. however,
+   the abstraction might not be so useful, in which case LinstrE and
+   LFlowE should probably be merged. *)
 Variant LFlowE : Type -> Type :=
   | LFopn        : lexprs -> sopn -> rexprs -> lcpoint ->
                    LFlowE lcpoint
@@ -103,16 +97,6 @@ Definition find_linstr_in_env (lc: lcmd) (pt: nat) : option linstr :=
 
 Definition is_final (lc: lcmd) (pt: nat) : bool :=
   pt =? (length lc).
-
-(* not used *)
-Definition halt_pred (l: lcpoint) : bool :=
-  let fn := fst l in
-  let lbl := snd l in
-  let plc := fenv fn in
-  match plc with
-  | Some lc => is_final lc lbl 
-  | _ => false
-  end.             
 
 Definition incr_label (l: lcpoint) : lcpoint :=
   match l with (fn, pt) => (fn, S pt) end.
@@ -145,10 +129,10 @@ Definition lcp_in_interval (fn: funname) (nS nE: nat) (l1: lcpoint) : bool :=
   match l1 with
   | (fn0, n0) => (fn == fn0) && (nS <= n0) && (n0 < nE) end. 
 
-(* the iterator used in the linear projection semantics of the source
-   code. nS and nE are the start and end points of the linear code
-   interval that contextualize the execution. l1 is the executed
-   linear code point. *)
+(* the 'local' iteration body used in the linear projection semantics
+   of the source code. nS and nE are the start and end points of the
+   linear code interval that contextualize the execution. l1 is the
+   linear code point being executed. *)
 Definition LCntr {E} {XE: ErrEvent -< E}  
   (F: linstr_r -> lcpoint -> itree E lcpoint)
   (fn: funname) (nS nE: nat) (l1: lcpoint) : itree E (lcpoint + lcpoint) :=
@@ -174,19 +158,38 @@ Definition LCntr {E} {XE: ErrEvent -< E}
     end
   end.        
 
-(* iterate XContrK *)
+(* iterate LCntr *)
 Definition LCntrI {E} {XE: ErrEvent -< E}  
   (F: linstr_r -> lcpoint -> itree E lcpoint)
   (fn: funname) (nS nE: nat) (l0: lcpoint) : itree E lcpoint :=
   ITree.iter (@LCntr E XE F fn nS nE) l0.
                  
+(* abstract for the stack *)
+Definition alstate := list lcpoint.
+
+(* not used *)
+Definition lcpoint2remote (l: lcpoint) : remote_label :=
+  match l with (fn, n) => (fn, Pos.of_nat n) end. 
+(* not used *)
+Definition remote2lcpoint (l: remote_label) : lcpoint :=
+  match l with (fn, l) => (fn, Pos.to_nat l) end. 
+(* not used *)
+Definition halt_pred (l: lcpoint) : bool :=
+  let fn := fst l in
+  let lbl := snd l in
+  let plc := fenv fn in
+  match plc with
+  | Some lc => is_final lc lbl 
+  | _ => false
+  end.             
+
 (**************************************************************)
 
 Section LinearSem.
   
 Context {E} {XF : LFlowE -< E} {XI : LinstrE -< E} {XE: ErrEvent -< E}. 
 
-(* one-step semantics of instructions *)
+(* denotational semantics of instructions *)
 Definition isem_linstr (ir : linstr_r) (l: lcpoint) :
   itree E lcpoint :=
   match ir with
@@ -216,11 +219,11 @@ Definition isem_linstr (ir : linstr_r) (l: lcpoint) :
 Definition isem_lcmd_body (lbl: lcpoint) :
   itree E (lcpoint + lcpoint) := GCntr isem_linstr lbl.
 
-(* iterative semantics of a program, from any starting point *)
+(* iterative semantics of a linear program, from any starting point *)
 Definition isem_lcmd (lbl: lcpoint) : itree E lcpoint :=
   ITree.iter isem_lcmd_body lbl.
 
-(* iterative semantics of a function from its entry point *)
+(* iterative semantics of a linear function from its entry point *)
 Definition isem_lfun (fn: funname) : itree E lcpoint :=
   isem_lcmd (fn, 0).
 
@@ -257,7 +260,7 @@ Section HandleLFlow.
 Context {E: Type -> Type} {XL: LinE -< E} {XST: StackE -< E}
     {XE: ErrEvent -< E}. 
 
-(* important one *)
+(* provides the behaviour wrt the stack abstraction *)
 Definition handle_LFlow {T} (e: LFlowE T) : itree E T :=
   match e with 
   | LFopn xs o es l0 => Ret (incr_label l0)
@@ -288,6 +291,7 @@ Section HandleStackA.
 
 Context {E: Type -> Type} {XA: @stateE alstate -< E} {XE: ErrEvent -< E}.
 
+(* we interpret stack events in the stack abstraction (alstate) *)
 Definition handle_StackA {T} (e: StackE T) : itree E T :=
   match e with    
   | Push l => stk <- trigger (@Get alstate) ;;
@@ -309,6 +313,7 @@ Context (store_ra : lcpoint -> lstate -> lstate).
 Context (update_ra : lstate -> lstate).
 Context (load_ra : lstate -> lcpoint).
 
+(* we interpret stack events directly in the linear state *)
 Definition handle_StackC {T} (e: StackE T) : itree E T :=
   match e with    
   | Push l => st <- trigger (@Get lstate) ;;
@@ -323,6 +328,8 @@ End HandleStackC.
 
 Section HandleLinA.
 
+(* Abstract alternative: using the stack abstraction, we must express
+   its agreeement with the linear state *)
 Context {stackAgree : lstate -> alstate -> bool}.
 
 Context {E: Type -> Type} {XA: @stateE alstate -< E} 
@@ -346,6 +353,7 @@ End HandleLinA.
 
 Section HandleLinC.
 
+(* Concrete alternative: we do not use the stack abstraction *)
 Context {E: Type -> Type} 
         {XS: @stateE lstate -< E} {XE: ErrEvent -< E}.
 
@@ -446,7 +454,7 @@ Definition lcond_sem (e: fexpr) (lbl: label)
      eval_jump P (s1.(lfn),lbl) s1
   else ok (lnext_pc s1).
 
-(* same as eval_instr *)
+(* same as eval_instr (in the old semantics) *)
 Definition linstr_isem (i : linstr_r) (s1: lstate) : exec lstate :=
   match i with 
   | Lopn xs o es => lopn_sem xs o es s1
@@ -488,25 +496,25 @@ Section Interp.
 
 Context {stackAgree : lstate -> alstate -> bool}.
 
-(* only cosmetics *)
+(* gives the core of the expected denotational semantics *)
 Definition interp_Linstr E {XE: ErrEvent -< E}
   {XL: LinE -< E} {XLS: stateE lstate -< E}
   {XST: StackE -< E} {A: Type}
   (t : itree (LinstrE +' E) A) : itree E A :=
   interp (ext_handler (fun T x => @handle_Linstr E XLS XE XL T x)) t.
 
-(* only cosmetics *)
+(* gives the dataflow abstraction semantics *)
 Definition interp_LFlow E {XL: LinE -< E} {XST: StackE -< E} {A: Type}
   (t : itree (LFlowE +' E) A) : itree E A :=
   interp (ext_handler (fun T x => @handle_LFlow E XL XST T x)) t.
 
-(* stack events (alstate-based) *)
+(* Abstract alternative: stack events (alstate-based) *)
 Definition interp_StackA E {XE: ErrEvent -< E}
   {XL: LinE -< E} {XLS: stateE alstate -< E} {A: Type}
   (t : itree (StackE +' E) A) : itree E A :=
   interp (ext_handler (fun T x => @handle_StackA E XLS XE T x)) t.
 
-(* alstate-based and lstate-based events *)
+(* Abtract alternative: alstate-based and lstate-based events *)
 Definition interp_LinA E {XE: ErrEvent -< E}
   {XLS: stateE alstate -< E}
   {XS: stateE lstate -< E} {A: Type}
@@ -514,7 +522,7 @@ Definition interp_LinA E {XE: ErrEvent -< E}
   interp (ext_handler
             (fun T x => @handle_LinA stackAgree E XLS XS XE T x)) t.
 
-(* only lstate-based *)
+(* Concrete alternative: only lstate-based *)
 Definition interp_LinC E {XE: ErrEvent -< E}
   {XS: stateE lstate -< E} {A: Type}
   (t : itree (LinE +' E) A) : itree E A :=
@@ -533,7 +541,7 @@ Definition interp_up2alstateL E {XE: ErrEvent -< E}
   (t : itree (LinstrE +' StackE +' LinE +' E) A) : itree E A :=
   interp_LinA (interp_StackA (interp_Linstr t)).
 
-(* stack events (lstate-based) *)
+(* Concrete alternative: stack events (lstate-based) *)
 Definition interp_StackC E {XE: ErrEvent -< E}
   {XL: LinE -< E} {XLS: stateE lstate -< E}
   (sra0 : lcpoint -> lstate -> lstate)
@@ -542,12 +550,6 @@ Definition interp_StackC E {XE: ErrEvent -< E}
   {A: Type}
   (t : itree (StackE +' E) A) : itree E A :=
   interp (ext_handler (fun T x => @handle_StackC E XLS sra0 ura0 lra0 T x)) t.
-
-(*
-Definition alstate2lstate E {XS: stateE lstate -< E} T
-  (t: itree (stateE alstate +' E) T) : itree E T.
-Admitted. 
-*)
 
 Definition interp_up2lstateG E {XE: ErrEvent -< E}
   {XS: stateE lstate -< E} 
@@ -575,7 +577,7 @@ Definition lsem_LCntr E {XE: ErrEvent -< E} {XI : LinstrE -< E}
   (fn: funname) (n0 n1: nat) : itree E (lcpoint + lcpoint) :=
   LCntr (fun i l => interp_LFlow (isem_linstr i l)) fn n0 n1 (fn, n0).
 
-(* iterate isem_LCntr *)
+(* iterate lsem_LCntr *)
 Definition lsem_LCntrI E {XE: ErrEvent -< E} {XI : LinstrE -< E}
   {XL: LinE -< E} {XLS: stateE lstate -< E} {XST: StackE -< E}
   (fn: funname) (n0 n1: nat) : itree E lcpoint :=
@@ -613,27 +615,6 @@ Fixpoint lsem_c E {XE: ErrEvent -< E}
   | i :: cc0 => '(fn1, n1) <- R i (fn, n) ;;
                 if fn == fn1 then lsem_c R fn cc0 n1 else throw err end.
 
-(* basically, the sequential alternative to LCntrI, relying on cc *)
-Fixpoint lsem_cX E {XE: ErrEvent -< E}            
-  (R: instr -> lcpoint -> itree E lcpoint)
-  (fn: funname) (nS nE: nat)
-  (cc: cmd) (l0: lcpoint) : itree E lcpoint :=
-  if lcp_in_interval fn nS nE l0
-  then match cc with
-       | nil => Ret l0
-       | i :: cc0 => l2 <- R i l0 ;;
-                     lsem_cX R fn nS nE cc0 l2 end
-  else throw err.
-
-Definition lsem_c_seq E {XE: ErrEvent -< E}
-  (loc_instr : instr -> lcpoint -> nat)               
-  (R: instr -> lcpoint -> itree E lcpoint)
-  (cc: cmd) (l0 l1: lcpoint) : itree E lcpoint :=
-  match l0 with
-  | (fn0, nS) =>
-    let nE := localize_cmd loc_instr fn0 cc nS in
-    lsem_cX R fn0 nS nE cc l1 end.
-
 (* basically, switches between different ktrees, depending on an
    ordered list of intervals. ls are the (well-ordered) interval
    end-points; ks are the ktrees *)
@@ -661,7 +642,8 @@ Definition ktree_switch E {XE: ErrEvent -< E}
           (fun n => (inr (fn, n))) ls (map RSLift ks) (snd l0)
   else Ret (inr l0).         
       
-(* crucially, by assuming fenv gives the linear code *)
+(* linear semantics of the source code, for the intermediate
+   representation. assuming fenv gives the linear code *)
 Fixpoint lsem_instr E {XE: ErrEvent -< E} {XI : LinstrE -< E}
   {XL: LinE -< E} {XLS: stateE lstate -< E} {XST: StackE -< E} 
   (loc_instr : instr -> lcpoint -> nat)
@@ -719,6 +701,29 @@ Fixpoint lsem_instr E {XE: ErrEvent -< E} {XI : LinstrE -< E}
                                   
  end.
 
+(* basically, the sequential alternative to LCntrI, relying on cc *)
+Fixpoint lsem_cX E {XE: ErrEvent -< E}            
+  (R: instr -> lcpoint -> itree E lcpoint)
+  (fn: funname) (nS nE: nat)
+  (cc: cmd) (l0: lcpoint) : itree E lcpoint :=
+  if lcp_in_interval fn nS nE l0
+  then match cc with
+       | nil => Ret l0
+       | i :: cc0 => l2 <- R i l0 ;;
+                     lsem_cX R fn nS nE cc0 l2 end
+  else Ret l0.
+(*  else throw err. *)
+
+Definition lsem_c_seq E {XE: ErrEvent -< E}
+  (loc_instr : instr -> lcpoint -> nat)               
+  (R: instr -> lcpoint -> itree E lcpoint)
+  (cc: cmd) (l0 l1: lcpoint) : itree E lcpoint :=
+  match l0 with
+  | (fn0, nS) =>
+    let nE := localize_cmd loc_instr fn0 cc nS in
+    lsem_cX R fn0 nS nE cc l1 end.
+
+(* linear semantics of source commands *)
 Definition lsem_cmd E {XE: ErrEvent -< E} {XI : LinstrE -< E}
   {XL: LinE -< E} {XLS: stateE lstate -< E} {XST: StackE -< E}
   (loc_instr : instr -> lcpoint -> nat)
@@ -739,6 +744,7 @@ Definition lsem_cmdI E {XE: ErrEvent -< E} {XI : LinstrE -< E}
      itree (callE (lcpoint * funname) lcpoint +' E) lcpoint :=
   lsem_cmd (fun i '(fn, n) => linear_end_i SP fn i n) cc l0 l1.
 
+(* tmp, just to patch up *)
 Variant StackAllocE : Type -> Type :=
   | Before : lcpoint -> StackAllocE unit
   | After : StackAllocE lcpoint
@@ -753,7 +759,7 @@ Definition interp_StackAlloc E {XE: ErrEvent -< E} {XST: StackE -< E}
   {T} (t: itree (StackAllocE +' E) T) : itree E T :=
   interp (ext_handler (fun T x => @handle_StackAlloc E XE XST T x)) t.
 
-(* l1 is the return address *)
+(* linear semantics of source functions. l1 is the return address *)
 Definition lsem_fun E {XE: ErrEvent -< E} {XI : LinstrE -< E}
   {XL: LinE -< E} {XLS: stateE lstate -< E} {XST: StackE -< E}
   {XTSA: StackAllocE -< E}
@@ -783,32 +789,36 @@ Definition interp_LRec E {XE: ErrEvent -< E} {XI : LinstrE -< E}
   T (t: itree (callE (lcpoint * funname) lcpoint +' E) T) : itree E T :=
   interp_mrec (handle_LRec loc_instr) t.
 
-Lemma LinearSemProj_fun_ok E {XE: ErrEvent -< E} {XI : LinstrE -< E}
-  {XL: LinE -< E} {XLS: stateE lstate -< E} {XST: StackE -< E}
-  {XTSA: StackAllocE -< E} 
+(* relating the linear semantics of source commands (intermediate
+   representation) with the direct semantics of their linear
+   translation *)
+Lemma LinearIntermediateRep_cmd_ok E {XE: ErrEvent -< E}
+  {XI : LinstrE -< E} {XL: LinE -< E} {XLS: stateE lstate -< E}
+  {XST: StackE -< E} {XTSA: StackAllocE -< E}
   (loc_instr : instr -> lcpoint -> nat)
   (l0: lcpoint) (c: cmd) :
-  eutt eq
-       (interp_mrec (@handle_LRec E XE XI XL XLS XST
-                          XTSA loc_instr) (lsem_cmdI c l0 l0))
+  eutt eq (interp_mrec (@handle_LRec E XE XI XL XLS XST XTSA loc_instr) 
+                       (lsem_cmdI c l0 l0))
           (interp_LFlow (isem_lcmd l0)). 
 Admitted. 
 
 Definition interp_LRec1 E {XE: ErrEvent -< E} {XI : LinstrE -< E}
   {XL: LinE -< E} {XLS: stateE lstate -< E} {XST: StackE -< E}
   (loc_instr : instr -> lcpoint -> nat)
-  T (t: itree (callE (lcpoint * funname) lcpoint +'
-                       StackAllocE +' E) T) :
+  T (t: itree (callE (lcpoint * funname) lcpoint +' StackAllocE +' E) T) :
   itree E T :=
   interp_StackAlloc (interp_mrec (handle_LRec loc_instr) t).
 
-Lemma LinearSemProj_fun_ok1 E {XE: ErrEvent -< E} {XI : LinstrE -< E}
-  {XL: LinE -< E} {XLS: stateE lstate -< E} {XST: StackE -< E}
+(* better version of the above lemma, in which we eliminate StackAlloc
+   events on the left *)
+Lemma LinearIntermediateRep_cmd_ok1 E {XE: ErrEvent -< E}
+  {XI : LinstrE -< E} {XL: LinE -< E} {XLS: stateE lstate -< E}
+  {XST: StackE -< E}
   (loc_instr : instr -> lcpoint -> nat)
   (l0: lcpoint) (c: cmd) :
-  eutt eq ((@interp_LRec1 E XE XI XL XLS XST 
-                      loc_instr) _ (lsem_cmdI c l0 l0))
-          (interp_LFlow (isem_lcmd l0)). 
+  eutt eq
+    ((@interp_LRec1 E XE XI XL XLS XST loc_instr) _ (lsem_cmdI c l0 l0))
+    (interp_LFlow (isem_lcmd l0)). 
 Admitted. 
 
 
@@ -860,7 +870,11 @@ Definition PostC T1 T2 (e1: E1 T1) (u1: T1) (e2: E2 T2) (u2: T2) : Prop :=
 
 Definition lc_end := fun i '(fn, n) => linear_end_i SP fn i n.
 
-Lemma linearization_proj_lemma (pd : PointerData) (sp: sprog)
+(* relating the linear semantics of a source function (intermediate
+   representation) with its source semantics (tantative). as it is, we
+   probably need to interpret state events. a better way could be to
+   combine higher-level events together. WIP *)
+Lemma LinearSem_fun_correct (pd : PointerData) (sp: sprog)
   (lin_params: linearization_params)
   (sra0 : lcpoint -> lstate -> lstate)
   (ura0 : lstate -> lstate)
@@ -889,7 +903,7 @@ Proof.
 Admitted. 
 
 (* to be proved using linearization_proj_lemma and
-   LinearSemProj_fun_ok1 *)
+   LinearIntermediateRep_ok and LinearSem_correct *)
 Lemma linearization_lemma (pd : PointerData) (sp: sprog)
   (lin_params: linearization_params)
   (sra0 : lcpoint -> lstate -> lstate)
