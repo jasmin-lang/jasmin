d2bb5b3f067cfb2579c8c908c2705e606e02c24a
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-03-06 14:11:06 +0100

    Exit word_ext
    This lemma breaks abstractions boundaries. The `wunsigned_inj` covers
    all its uses.
    
    This requires an updated version of mathcomp-word.
    
    Update mathcomp-word to v3.3
    
    (cherry picked from commit f814b30e4ab88c8cd854eb764fe4e91139dfb0b8)
    (cherry picked from commit 5e9cc55ba499d2a075d0a303420a3d2d49ae0b61)

--- scripts/mathcomp-word.nix
+++ scripts/mathcomp-word.nix
@@ -2,17 +2,17 @@
 
 let inherit (coqPackages) coq; in
 
-let rev = "1abe5ade5240115aed1e3c140e261f1554af2322"; in
+let rev = "8a677724135da3ef369f154b060c7fd97239ff15"; in
 
 stdenv.mkDerivation rec {
-  version = "3.2-git-${builtins.substring 0 8 rev}";
+  version = "3.3-git-${builtins.substring 0 8 rev}";
   pname = "coq${coq.coq-version}-mathcomp-word";
 
   src = fetchFromGitHub {
     owner = "jasmin-lang";
     repo = "coqword";
     inherit rev;
-    hash = "sha256-s6UD9aYsaRXlxMNxkZGD/yZx1rlDVqJLU1mLfJAg6To=";
+    hash = "sha256-5ZKz2QKOfMRCfppR/3oKzIghEU1pMUB0128OdSP6ptw=";
   };
 
   buildInputs = [ coq ocaml dune_3 ];
