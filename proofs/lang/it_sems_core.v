50227152d8b97663f8391664276060a8ab24bce0
Benjamin Gregoire <Benjamin.Gregoire@inria.fr>
2026-04-14 17:39:33 +0200

    itree based proof of linearization

--- proofs/lang/it_sems_core.v
+++ proofs/lang/it_sems_core.v
@@ -183,6 +183,18 @@ Definition isem_while_loop (c1 : cmd) (e:pexpr) (c2: cmd) (s : estate) :
 
 End SEM_C.
 
+Definition estate0 (fs : fstate) :=
+  Estate fs.(fscs) fs.(fmem) Vm.init.
+
+Definition is_init_state_ok (p:prog) ev (fn:funname) (fs:fstate) :=
+  match get_fundef (p_funcs p) fn with
+  | Some fd =>
+    let sinit := estate0 fs in
+    Let _ := init_state fd.(f_extra) (p_extra p) ev sinit in
+    ok tt
+  | None => Error ErrSemUndef
+  end.
+
 Class sem_Fun (E : Type -> Type) :=
   { sem_fun : prog -> extra_val_t -> instr_info -> funname -> fstate -> itree E fstate }.
 
@@ -196,6 +208,8 @@ Context {E E0} {wE : with_Error E E0} {sem_F : sem_Fun E }.
 
 (* semantics of instructions, abstracting on function calls (through
    sem_fun) *)
+
+
 Fixpoint isem_i_body (p : prog) (ev : extra_val_t) (i : instr) (s : estate) :
     itree E estate :=
   let: (MkI ii i) := i in
@@ -252,9 +266,6 @@ Proof.
   rewrite -/isem_cmd_ isem_cmd_cat; rewrite !bind_bind.
   apply eutt_eq_bind => {}s /= ; rewrite bind_ret_l !bind_ret_r tau_eutt; reflexivity.
 Qed.
-
-Definition estate0 (fs : fstate) :=
-  Estate fs.(fscs) fs.(fmem) Vm.init.
 
 Definition initialize_funcall (p : prog) (ev : extra_val_t) (fd : fundef) (fs : fstate) : exec estate :=
   let sinit := estate0 fs in
@@ -691,7 +702,7 @@ Proof.
     rewrite /isem_pexprs interp_cond_iresult; apply eutt_eq_bind => ?.
     rewrite interp_bind; apply eutt_eq_bind'.
     + by apply interp_cond_iresult.
-    move=> _; rewrite interp_bind; apply eutt_eq_bind'.
+    move=> _. rewrite interp_bind; apply eutt_eq_bind'.
     + rewrite /ctx_cond /cond /Handler.case_ /rec_call /F.
       setoid_rewrite interp_trigger; reflexivity.
     move=> ?; rewrite interp_bind; apply eutt_eq_bind'.
