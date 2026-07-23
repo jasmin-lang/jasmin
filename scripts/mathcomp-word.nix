d79f9b6a1cda3f1b9dfde9c1ba1852276cad9594
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-07-23 14:36:41 +0200

    Update mathcomp-word

--- scripts/mathcomp-word.nix
+++ scripts/mathcomp-word.nix
@@ -1,21 +1,21 @@
-{ stdenv, lib, fetchFromGitHub, coqPackages, ocaml, dune_3 }:
+{ stdenv, lib, fetchFromGitHub, coqPackages, ocaml, dune }:
 
 let inherit (coqPackages) coq; in
 
-let rev = "3ba8484ae779a2178ea3c6a470f102a0dd57a8a9"; in
+let rev = "8a69be8d171dbc6479d0df6371428e2beac6a3cd"; in
 
 stdenv.mkDerivation rec {
-  version = "3.4-git-${builtins.substring 0 8 rev}";
+  version = "3.5-git-${builtins.substring 0 8 rev}";
   pname = "coq${coq.coq-version}-mathcomp-word";
 
   src = fetchFromGitHub {
     owner = "jasmin-lang";
     repo = "coqword";
     inherit rev;
-    hash = "sha256-M2Yw1aPhJlXhmJJhHpuQZnhGDV4YdJhtc2RwJzqAVcI=";
+    hash = "sha256-Wyoa0rj38qxR9lLLC1MN+QCZZgKuXMtW9h5jA1AjoQE=";
   };
 
-  buildInputs = [ coq ocaml dune_3 ];
+  buildInputs = [ coq ocaml dune ];
 
   propagatedBuildInputs = (with coqPackages.mathcomp; [ algebra fingroup ssreflect ])
   ++ [ coqPackages.stdlib ];
