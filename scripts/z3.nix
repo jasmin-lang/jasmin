d7c11a16d5cbb6f0f130de33e9ffa730c7396925
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-09-07 11:24:23 +0200

    CI: use Z3 4.16.0

--- scripts/z3.nix
+++ scripts/z3.nix
@@ -1,11 +1,11 @@
 { z3, fetchFromGitHub }:
 
 z3.overrideAttrs (_: rec {
-  version = "4.13.4";
+  version = "4.16.0";
   src = fetchFromGitHub {
     owner = "Z3Prover";
     repo = "z3";
     tag = "z3-${version}";
-    hash = "sha256-8hWXCr6IuNVKkOegEmWooo5jkdmln9nU7wI8T882BSE=";
+    hash = "sha256-DnhX3kxggnFmyYwXEPBsBA1rh4oor1oIJR5TMJk/jvc=";
   };
 })
