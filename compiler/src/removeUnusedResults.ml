a52ae8b669ab222a2d225533ba5cab4752ad42d0
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-09-15 16:16:41 +0200

    Generalize remove-unused-results
    The analysis no longer expects each function to be paired with some
    extra-data.
    
    The documentation comment has also been rewritten.

--- compiler/src/removeUnusedResults.ml
+++ compiler/src/removeUnusedResults.ml
@@ -12,7 +12,7 @@ let used_results (live: Sv.t) : lvals -> Sint.t =
 
 let analyse funcs =
   let liveness_table : (Sv.t * Sv.t, 'asm) func Hf.t = Hf.create 17 in
-  List.iter (fun (_,f) -> Hf.add liveness_table f.f_name (Liveness.live_fd false f)) funcs;
+  List.iter (fun f -> Hf.add liveness_table f.f_name (Liveness.live_fd false f)) funcs;
   let live_results =
     let live : Sint.t Hf.t = Hf.create 17 in
     let cbf _loc fn xs (_, s) =
@@ -24,7 +24,7 @@ let analyse funcs =
     fun fn -> Hf.find_default live fn Sint.empty
   in
   let live = Hf.create 17 in
-  let add (_,fd) =
+  let add fd =
     let info =
       if FInfo.is_export fd.f_cc then None
       else
