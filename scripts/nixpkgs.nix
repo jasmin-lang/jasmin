08e6f9830899496c7fd5ebd5386270992a8a5c05
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-09-21 14:06:30 +0200

    CI: update nixpkgs (elpi 3.8.0)

--- scripts/nixpkgs.nix
+++ scripts/nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/b845e7606d23cdc24c5c81945f77077acc797a3f.tar.gz";
-  sha256 = "sha256:0d626hvjaf8f98g1cibcas3ya8pryhibz2l13xrhgxm64cxvdw5x";
+  url = "https://github.com/NixOS/nixpkgs/archive/652efddd59a21783edff64959252ec1cfce09041.tar.gz";
+  sha256 = "sha256:11vxn2zqblkxfl6m20x4qd4b90sn7wmnmngi4c6mydhdr2kbhvj2";
 })
